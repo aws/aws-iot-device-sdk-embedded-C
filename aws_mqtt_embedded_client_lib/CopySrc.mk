8c435115494291e963a74f00dde2b43c2a97ddf7
Pani <bhadrinp@amazon.com>
2015-10-09 11:24:58 -0700

    modified and cleaned paho naming to Eclipse Paho

--- aws_mqtt_embedded_client_lib/CopySrc.mk
+++
@@ -1,5 +0,0 @@
-MQTTPacket := $(call copy, $(pwd)/MQTTPacket/src,$(brazil.build.dir)/MQTTPacket/src)
-
-MQTTClient := $(call copy, $(pwd)/MQTTClient/src,$(brazil.build.dir)/MQTTClient/src)
-
-MQTTClientC := $(call copy, $(pwd)/MQTTClient-C/src,$(brazil.build.dir)/MQTTClient-C/src)
