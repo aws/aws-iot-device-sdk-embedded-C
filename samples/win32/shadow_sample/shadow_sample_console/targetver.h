9aa2f3e939ed2da097e521e9a5f6ecc63ac99c73
Jerry ONeal <jerry.oneal@gmail.com>
2016-11-11 16:22:07 -0500

    Minor refactoring

--- samples/win32/shadow_sample/shadow_sample_console/targetver.h
+++ samples/win32/shadow_sample/shadow_sample_console/targetver.h
@@ -1,4 +1,20 @@
-#pragma once
+/*
+* Copyright 2010-2016 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+*
+* Licensed under the Apache License, Version 2.0 (the "License").
+* You may not use this file except in compliance with the License.
+* A copy of the License is located at
+*
+*  http://aws.amazon.com/apache2.0
+*
+* or in the "license" file accompanying this file. This file is distributed
+* on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+* express or implied. See the License for the specific language governing
+* permissions and limitations under the License.
+*/
+
+#ifndef _TARGETVER_H
+#define _TARGETVER_H
 
 // Including SDKDDKVer.h defines the highest available Windows platform.
 
@@ -6,3 +22,6 @@
 // set the _WIN32_WINNT macro to the platform you wish to support before including SDKDDKVer.h.
 
 #include <SDKDDKVer.h>
+
+#endif // _TARGETVER_H
+
