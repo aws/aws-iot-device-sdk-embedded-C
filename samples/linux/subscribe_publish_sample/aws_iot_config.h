52764cc95f0d3625467fbb8332ab635d9f775dd9
Hugues Bouvier <33438817+huguesBouvier@users.noreply.github.com>
2018-05-03 11:38:30 -0700

    remove credentials

--- samples/linux/subscribe_publish_sample/aws_iot_config.h
+++ samples/linux/subscribe_publish_sample/aws_iot_config.h
@@ -23,10 +23,10 @@
 
 // Get from console
 // =================================================
-#define AWS_IOT_MQTT_HOST              "a3qggb6vsdlf5s.iot.us-west-2.amazonaws.com" ///< Customer specific MQTT HOST. The same will be used for Thing Shadow
+#define AWS_IOT_MQTT_HOST              "" ///< Customer specific MQTT HOST. The same will be used for Thing Shadow
 #define AWS_IOT_MQTT_PORT              443 ///< default port for MQTT/S
-#define AWS_IOT_MQTT_CLIENT_ID         "Hugues_device" ///< MQTT client ID should be unique for every device
-#define AWS_IOT_MY_THING_NAME 		   "Hugues_device" ///< Thing Name of the Shadow this device is associated with
+#define AWS_IOT_MQTT_CLIENT_ID         "c-sdk-client-id" ///< MQTT client ID should be unique for every device
+#define AWS_IOT_MY_THING_NAME 		   "AWS-IoT-C-SDK" ///< Thing Name of the Shadow this device is associated with
 #define AWS_IOT_ROOT_CA_FILENAME       "rootCA.crt" ///< Root CA file name
 #define AWS_IOT_CERTIFICATE_FILENAME   "cert.pem" ///< device signed certificate file name
 #define AWS_IOT_PRIVATE_KEY_FILENAME   "privkey.pem" ///< Device private key filename
