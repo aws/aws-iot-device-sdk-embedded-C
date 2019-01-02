f5f1dc7fa92ccb1d3768844e86c31077d5c65a19
Gordon Wang <36049150+gordonwang0@users.noreply.github.com>
2019-01-02 07:11:48 -0800

    Document AwsIotShadow_SetDeltaCallback.

--- lib/include/aws_iot_shadow.h
+++ lib/include/aws_iot_shadow.h
@@ -185,6 +185,8 @@ typedef enum AwsIotShadowError
      * - @ref shadow_function_timedget
      * - @ref shadow_function_timedupdate
      * - @ref shadow_function_wait
+     * - @ref shadow_function_setdeltacallback
+     * - @ref shadow_function_setupdatedcallback
      */
     AWS_IOT_SHADOW_TIMEOUT,
 
@@ -883,8 +885,121 @@ AwsIotShadowError_t AwsIotShadow_Wait( AwsIotShadowReference_t reference,
 /* @[declare_shadow_wait] */
 
 /**
- * @brief Set a callback to be invoked when the Thing Shadow "desired" and "reported"
+ * @brief Set a callback to be invoked when the Thing Shadow `desired` and `reported`
  * states differ.
+ *
+ * A Thing Shadow contains `reported` and `desired` states, meant to represent
+ * the current device status and some desired status, respectively. When the
+ * `reported` and `desired` states differ, the Thing Shadow service generates a
+ * <i>delta document</i> and publishes it to the topic `update/delta`. Devices
+ * with a subscription for this topic will receive the delta document and may act
+ * based on the different `reported` and `desired` states. See [this page]
+ * (https://docs.aws.amazon.com/iot/latest/developerguide/using-device-shadows.html#delta-state)
+ * for more information about using delta documents.
+ *
+ * A <i>delta callback</i> may be invoked whenever a delta document is generated.
+ * Each Thing may have a single delta callback set. This function modifies the delta
+ * callback for a specific Thing depending on the `pDeltaCallback` parameter and
+ * the presence of any existing delta callback:
+ * - When no existing delta callback exists for a specific Thing, a new delta
+ * callback is added.
+ * - If there is an existing delta callback and `pDeltaCallback` is not `NULL`, then
+ * the existing callback function and parameter are replaced with `pDeltaCallback`.
+ * - If there is an existing subscription and `pDeltaCallback` is `NULL`, then the
+ * delta callback is removed.
+ *
+ * This function is always blocking; it may block for up to the default MQTT
+ * timeout. This timeout is set as a parameter to @ref shadow_function_init,
+ * and defaults to @ref AWS_IOT_SHADOW_DEFAULT_MQTT_TIMEOUT_MS if not set. If
+ * this function's underlying MQTT operations fail to complete within this
+ * timeout, then this function returns #AWS_IOT_SHADOW_TIMEOUT.
+ *
+ * @param[in] mqttConnection The MQTT connection to use for the subscription to
+ * `update/delta`.
+ * @param[in] pThingName The subscription to `update/delta` will be added for
+ * this Thing Name.
+ * @param[in] thingNameLength The length of `pThingName`.
+ * @param[in] flags This parameter is for future-compatibility. Currently, flags
+ * are not supported for this function and this parameter is ignored.
+ * @param[in] pDeltaCallback Callback function and to invoke for incoming delta
+ * documents.
+ *
+ * @return One of the following:
+ * - #AWS_IOT_SHADOW_SUCCESS
+ * - #AWS_IOT_SHADOW_BAD_PARAMETER
+ * - #AWS_IOT_SHADOW_NO_MEMORY
+ * - #AWS_IOT_SHADOW_MQTT_ERROR
+ * - #AWS_IOT_SHADOW_TIMEOUT
+ *
+ * @return This function always returns #AWS_IOT_SHADOW_SUCCESS when replacing or
+ * removing existing delta callbacks.
+ *
+ * @see @ref shadow_function_setupdatedcallback for the function to register
+ * callbacks for all Shadow updates.
+ *
+ * <b>Example</b>
+ * @code{c}
+ * #define _THING_NAME "Test_device"
+ * #define _THING_NAME_LENGTH ( sizeof( _THING_NAME ) - 1 )
+ *
+ * AwsIotShadowError_t result = AWS_IOT_SHADOW_STATUS_PENDING;
+ * AwsIotShadowCallbackInfo_t deltaCallback = AWS_IOT_SHADOW_CALLBACK_INFO_INITIALIZER;
+ *
+ * // _deltaCallbackFunction will be invoked when a delta document is received.
+ * deltaCallback.function = _deltaCallbackFunction;
+ *
+ * // Set the delta callback for the Thing "Test_device".
+ * result = AwsIotShadow_SetDeltaCallback( mqttConnection,
+ *                                         _THING_NAME,
+ *                                         _THING_NAME_LENGTH,
+ *                                         0,
+ *                                         &deltaCallback );
+ *
+ * // Check if callback was successfully set.
+ * if( result == AWS_IOT_SHADOW_SUCCESS )
+ * {
+ *     AwsIotShadowDocumentInfo_t updateInfo = AWS_IOT_SHADOW_DOCUMENT_INFO_INITIALIZER;
+ *
+ *     // Set the Thing Name for Shadow update.
+ *     updateInfo.pThingName = _THING_NAME;
+ *     updateInfo.thingNameLength = _THING_NAME_LENGTH;
+ *
+ *     // Set the Shadow document to send. This document has different "reported"
+ *     // and "desired" states. It represents a scenario where a device is currently
+ *     // off, but is being ordered to turn on.
+ *     updateInfo.update.pUpdateDocument =
+ *     "{"
+ *          "\"state\": {"
+ *              "\"reported\": { \"deviceOn\": false },"
+ *              "\"desired\": { \"deviceOn\": true }"
+ *          "}"
+ *     "}";
+ *     updateInfo.update.updateDocumentLength = strlen( updateInfo.update.pUpdateDocument );
+ *
+ *     // Send the Shadow document with different "reported" and desired states.
+ *     result = AwsIotShadow_TimedUpdate( mqttConnection,
+ *                                        &updateInfo,
+ *                                        0,
+ *                                        0,
+ *                                        5000 );
+ *
+ *     // After the update is successfully sent, the function _deltaCallbackFunction
+ *     // will be invoked once the Shadow service generates and sends a delta document.
+ *     // The delta document will contain the different "deviceOn" states, as well as
+ *     // metadata.
+ *
+ *     // Once the delta callback is no longer needed, it may be removed by passing
+ *     // NULL as pDeltaCallback.
+ *     result = AwsIotShadow_SetDeltaCallback( mqttConnection,
+ *                                             _THING_NAME,
+ *                                             _THING_NAME_LENGTH,
+ *                                             0,
+ *                                             NULL );
+ *
+ *     // The return value from removing a delta callback should always be success.
+ *     assert( result == AWS_IOT_SHADOW_SUCCESS );
+ * }
+ * @endcode
  */
 /* @[declare_shadow_setdeltacallback] */
 AwsIotShadowError_t AwsIotShadow_SetDeltaCallback( AwsIotMqttConnection_t mqttConnection,
