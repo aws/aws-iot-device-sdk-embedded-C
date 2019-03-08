11f7d6373b278809fb72822e665e5ef589b78738
Lorenzo Tessiore <lt72@users.noreply.github.com>
2019-03-07 20:13:48 -0800

    Add common tests (lists, taskpool, ...) and defender to code coverage run. (#308)

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -17,6 +17,9 @@ rm -rf *
 cmake .. -DIOT_BUILD_TESTS=1 -DCMAKE_BUILD_TYPE=Debug -DCMAKE_C_FLAGS="-DIOT_TEST_SERVER=\"\\\"$AWS_IOT_ENDPOINT\\\"\" -DIOT_TEST_PORT=443 -DIOT_TEST_ROOT_CA=\"\\\"../credentials/AmazonRootCA1.pem\\\"\" -DIOT_TEST_CLIENT_CERT=\"\\\"../credentials/clientCert.pem\\\"\" -DIOT_TEST_PRIVATE_KEY=\"\\\"../credentials/privateKey.pem\\\"\" -DIOT_TEST_MQTT_CLIENT_IDENTIFIER=\"\\\"$AWS_IOT_THING_NAME\\\"\" -DAWS_IOT_TEST_SHADOW_THING_NAME=\"\\\"$AWS_IOT_THING_NAME\\\"\" -DIOT_TEST_MQTT_TOPIC_PREFIX=\"\\\"$AWS_IOT_THING_NAME\\\"\" -DIOT_LOG_LEVEL_GLOBAL=IOT_LOG_DEBUG -DIOT_DEMO_MQTT_TOPIC_PREFIX=\"\\\"$AWS_IOT_THING_NAME\\\"\" -DIOT_TEST_COVERAGE=1 --coverage"
 make
 
+# Run Common tests with code coverage.
+./bin/iot_tests_mqtt 2&> /dev/null
+
 # Run MQTT tests and demo against AWS IoT with code coverage.
 ./bin/iot_tests_mqtt 2&> /dev/null
 ./bin/iot_demo_mqtt -h "$AWS_IOT_ENDPOINT" -p 443 -s -r ../credentials/AmazonRootCA1.pem -c ../credentials/clientCert.pem -k ../credentials/privateKey.pem -i "$AWS_IOT_THING_NAME" 2&> /dev/null
@@ -24,6 +27,9 @@ make
 # Run Shadow tests and demo with code coverage.
 ./bin/aws_iot_tests_shadow 2&> /dev/null
 ./bin/aws_iot_demo_shadow -h "$AWS_IOT_ENDPOINT" -p 443 -s -r ../credentials/AmazonRootCA1.pem -c ../credentials/clientCert.pem -k ../credentials/privateKey.pem -i "$AWS_IOT_THING_NAME" 2&> /dev/null
+
+# Run Defender tests with code coverage.
+./bin/aws_iot_tests_defender 2&> /dev/null
 
 # Generate code coverage results, excluding Unity test framework, demo files, and tests files.
 lcov --directory . --capture --output-file coverage.info
