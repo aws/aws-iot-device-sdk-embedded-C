04ba7c4ceaddce517a868a903b8a210a85ad1f2f
Mark R. Tuttle <mrtuttle@amazon.com>
2019-03-15 13:57:37 -0400

    Updated deserialize proofs to use the new API.

--- cbmc/proofs/DeserializePublish/DeserializePublish_harness.c
+++ cbmc/proofs/DeserializePublish/DeserializePublish_harness.c
@@ -7,15 +7,17 @@
 
 void harness()
 {
-    size_t dataLength;
-    uint8_t * pPublishStart = malloc( sizeof( uint8_t ) * dataLength );
-    IotMqttPublishInfo_t output;
-    uint16_t packetIdentifier;
-    size_t bytesProcessed;
+  IotMqttPublishInfo_t PublishInfo;
+  PublishInfo.pTopicName = malloc(PublishInfo.topicNameLength);
+  PublishInfo.pPayload = malloc(PublishInfo.payloadLength);
 
-    _IotMqtt_DeserializePublish( pPublishStart,
-                                 dataLength,
-                                 &output,
-                                 &packetIdentifier,
-                                 &bytesProcessed );
+  _mqttOperation_t Operation;
+  Operation.publishInfo = PublishInfo;
+
+  _mqttPacket_t Publish;
+  Publish.pRemainingData = malloc(sizeof(uint8_t) * Publish.remainingLength);
+  Publish.pIncomingPublish = &Operation;
+
+
+  _IotMqtt_DeserializePublish( &Publish );
 }
