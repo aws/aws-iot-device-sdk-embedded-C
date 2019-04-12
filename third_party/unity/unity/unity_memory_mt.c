85eb760ac8097b90cf306ea6759d2bf8fc7eaf6c
Gordon Wang <36049150+gordonwang0@users.noreply.github.com>
2019-04-12 11:50:38 -0700

    Submodule tinycbor and move unity to third_party (#369)

---
+++ third_party/unity/unity/unity_memory_mt.c
@@ -0,0 +1,89 @@
+/* Wrappers that make the unity memory functions thread-safe. Implemented for
+ * POSIX systems. */
+
+#include "unity_fixture.h"
+#include "unity_fixture_malloc_overrides.h"
+#include <pthread.h>
+
+pthread_mutex_t CriticalSectionMutex = PTHREAD_MUTEX_INITIALIZER;
+
+void UnityMalloc_StartTest(void)
+{
+    pthread_mutex_lock(&CriticalSectionMutex);
+    UnityMalloc_StartTest();
+    pthread_mutex_unlock(&CriticalSectionMutex);
+}
+
+void UnityMalloc_EndTest(void)
+{
+    pthread_mutex_lock(&CriticalSectionMutex);
+    UnityMalloc_EndTest();
+    pthread_mutex_unlock(&CriticalSectionMutex);
+}
+
+void UnityMalloc_MakeMallocFailAfterCount(int countdown)
+{
+    pthread_mutex_lock(&CriticalSectionMutex);
+    UnityMalloc_MakeMallocFailAfterCount(countdown);
+    pthread_mutex_unlock(&CriticalSectionMutex);
+}
+
+void* unity_malloc_mt(size_t size)
+{
+    void* mem = NULL;
+
+    if (pthread_mutex_lock(&CriticalSectionMutex) != 0) return NULL;
+
+    mem = unity_malloc(size);
+
+    if (pthread_mutex_unlock(&CriticalSectionMutex) != 0)
+    {
+        unity_free(mem);
+        mem = NULL;
+    }
+
+    return mem;
+}
+
+void* unity_calloc_mt(size_t num, size_t size)
+{
+    void* mem = NULL;
+
+    if (pthread_mutex_lock(&CriticalSectionMutex) != 0) return NULL;
+
+    mem = unity_calloc(num, size);
+
+    if (pthread_mutex_unlock(&CriticalSectionMutex) != 0)
+    {
+        unity_free(mem);
+        mem = NULL;
+    }
+
+    return mem;
+}
+
+void* unity_realloc_mt(void * oldMem, size_t size)
+{
+    void* mem = NULL;
+
+    if (pthread_mutex_lock(&CriticalSectionMutex) != 0) return NULL;
+
+    mem = unity_realloc(oldMem, size);
+
+    if (pthread_mutex_unlock(&CriticalSectionMutex) != 0)
+    {
+        unity_free(mem);
+        mem = NULL;
+    }
+
+    return mem;
+}
+
+void unity_free_mt(void * mem)
+{
+    pthread_mutex_lock(&CriticalSectionMutex);
+
+    unity_free(mem);
+
+    pthread_mutex_unlock(&CriticalSectionMutex);
+}
