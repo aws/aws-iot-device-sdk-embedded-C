0cb1b42bbbb7b5d9e39bd300bf6ea749aaa3474b
Gordon Wang <36049150+gordonwang0@users.noreply.github.com>
2019-05-20 12:46:37 -0700

    Add demo option to CI

--- scripts/ci_test_mqtt.sh
+++ scripts/ci_test_mqtt.sh
@@ -8,6 +8,7 @@ set -e
 # For pull request builds, run against test.mosquitto.org. Otherwise, run against AWS IoT.
 if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
     CMAKE_FLAGS="$AWS_IOT_CREDENTIAL_DEFINES -DIOT_TEST_MQTT_CLIENT_IDENTIFIER=\"\\\"$IOT_IDENTIFIER\\\"\" -DIOT_TEST_MQTT_TOPIC_PREFIX=\"\\\"$IOT_IDENTIFIER\\\"\" -DIOT_DEMO_MQTT_TOPIC_PREFIX=\"\\\"$IOT_IDENTIFIER\\\"\" $COMPILER_OPTIONS"
+    DEMO_OPTIONS="-n"
 else
     CMAKE_FLAGS="-DIOT_TEST_MQTT_MOSQUITTO=1 $COMPILER_OPTIONS"
 fi
@@ -18,7 +19,7 @@ make -j2 iot_tests_mqtt iot_demo_mqtt
 
 # Run tests and demos.
 ./bin/iot_tests_mqtt
-./bin/iot_demo_mqtt
+./bin/iot_demo_mqtt $DEMO_OPTIONS
 
 # Rebuild in static memory mode.
 cmake .. -DIOT_BUILD_TESTS=1 -DCMAKE_BUILD_TYPE=Debug -DIOT_NETWORK_USE_OPENSSL=$IOT_NETWORK_USE_OPENSSL -DCMAKE_C_FLAGS="$CMAKE_FLAGS -DIOT_STATIC_MEMORY_ONLY=1"
