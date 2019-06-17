419c72908d9e6d21a8ab8c156dad5feaedcfef68
Gordon Wang <36049150+gordonwang0@users.noreply.github.com>
2019-06-17 12:46:09 -0400

    Add empty files for Jobs library (#458)

---
+++ tests/jobs/unit/aws_iot_tests_jobs_api.c
@@ -0,0 +1,73 @@
+/*
+ * Copyright (C) 2019 Amazon.com, Inc. or its affiliates.  All Rights Reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+/**
+ * @file aws_iot_tests_jobs_api.c
+ * @brief Tests for the user-facing API functions (declared in aws_iot_jobs.h).
+ */
+
+/* The config header is always included first. */
+#include "iot_config.h"
+
+/* SDK initialization include. */
+#include "iot_init.h"
+
+/* Test framework includes. */
+#include "unity_fixture.h"
+
+/*-----------------------------------------------------------*/
+
+/**
+ * @brief Test group for Jobs API tests.
+ */
+TEST_GROUP( Jobs_Unit_API );
+
+/*-----------------------------------------------------------*/
+
+/**
+ * @brief Test setup for Jobs API tests.
+ */
+TEST_SETUP( Jobs_Unit_API )
+{
+    /* Initialize SDK. */
+    TEST_ASSERT_EQUAL_INT( true, IotSdk_Init() );
+}
+
+/*-----------------------------------------------------------*/
+
+/**
+ * @brief Test tear down for Jobs API tests.
+ */
+TEST_TEAR_DOWN( Jobs_Unit_API )
+{
+    IotSdk_Cleanup();
+}
+
+/*-----------------------------------------------------------*/
+
+/**
+ * @brief Test group runner for Jobs API tests.
+ */
+TEST_GROUP_RUNNER( Jobs_Unit_API )
+{
+}
+
+/*-----------------------------------------------------------*/
