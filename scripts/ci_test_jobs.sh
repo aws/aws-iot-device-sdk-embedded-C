eab5443af4c370c8b59c83030405ec3af9294c67
Gordon Wang <36049150+gordonwang0@users.noreply.github.com>
2019-06-17 12:46:09 -0400

    Add empty files for Jobs library (#458)

---
+++ scripts/ci_test_jobs.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# Travis CI uses this script to test the Jobs library.
+
+# Exit on any nonzero return code.
+set -e
+
+# CMake compiler flags for building Jobs.
+CMAKE_FLAGS="$COMPILER_OPTIONS"
+
+# Build executables.
+cmake .. -DIOT_BUILD_TESTS=1 -DCMAKE_BUILD_TYPE=Debug -DCMAKE_C_FLAGS="$CMAKE_FLAGS"
+make -j2 aws_iot_tests_jobs
+
+# Run tests.
+./bin/aws_iot_tests_jobs
+
+# Rebuild in static memory mode.
+cmake .. -DIOT_BUILD_TESTS=1 -DCMAKE_BUILD_TYPE=Debug -DCMAKE_C_FLAGS="$CMAKE_FLAGS -DIOT_STATIC_MEMORY_ONLY=1"
+make -j2 aws_iot_tests_jobs
+
+# Run tests in static memory mode.
+./bin/aws_iot_tests_jobs
