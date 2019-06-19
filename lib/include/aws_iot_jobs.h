6e7a83c00a4b459defcc7f1d31e4a87ed1a31ebd
Gordon Wang <36049150+gordonwang0@users.noreply.github.com>
2019-06-18 22:09:37 -0400

    Add Jobs enums and strerror functions

--- lib/include/aws_iot_jobs.h
+++ lib/include/aws_iot_jobs.h
@@ -35,4 +35,66 @@
 
 /*------------------------- Jobs library functions --------------------------*/
 
-#endif
+/**
+ * @functionspage{jobs,Jobs library}
+ * - @functionname{jobs_function_update}
+ * - @functionname{jobs_function_strerror}
+ * - @functionname{jobs_function_statename}
+ */
+
+/**
+ * @functionpage{AwsIotJobs_Update,jobs,update}
+ * @functionpage{AwsIotJobs_strerror,jobs,strerror}
+ * @functionpage{AwsIotJobs_StateName,jobs,statename}
+ */
+
+/**
+ * @brief Update the status of a job execution and receive an asynchronous
+ * notification when the Job update completes.
+ */
+/* @[declare_jobs_update] */
+AwsIotJobsError_t AwsIotJobs_Update( void );
+/* @[declare_jobs_update] */
+
+/*-------------------------- Jobs helper functions --------------------------*/
+
+/**
+ * @brief Returns a string that describes an #AwsIotJobsError_t.
+ *
+ * Like POSIX's `strerror`, this function returns a string describing a return
+ * code. In this case, the return code is a Jobs library error code, `status`.
+ *
+ * The string returned by this function <b>MUST</b> be treated as read-only: any
+ * attempt to modify its contents may result in a crash. Therefore, this function
+ * is limited to usage in logging.
+ *
+ * @param[in] status The status to describe.
+ *
+ * @return A read-only string that describes `status`.
+ *
+ * @warning The string returned by this function must never be modified.
+ */
+/* @[declare_jobs_strerror] */
+const char * AwsIotJobs_strerror( AwsIotJobsError_t status );
+/* @[declare_jobs_strerror] */
+
+/**
+ * @brief Returns a string that describes an #AwsIotJobState_t.
+ *
+ * This function returns a string describing a Job state, `state`.
+ *
+ * The string returned by this function <b>MUST</b> be treated as read-only: any
+ * attempt to modify its contents may result in a crash. Therefore, this function
+ * is limited to usage in logging.
+ *
+ * @param[in] state The job state to describe.
+ *
+ * @return A read-only string that describes `state`.
+ *
+ * @warning The string returned by this function must never be modified.
+ */
+/* @[declare_jobs_statename] */
+const char * AwsIotJobs_StateName( AwsIotJobState_t state );
+/* @[declare_jobs_statename] */
+
+#endif /* ifndef AWS_IOT_JOBS_H_ */
--- lib/include/types/aws_iot_jobs_types.h
+++ lib/include/types/aws_iot_jobs_types.h
@@ -30,4 +30,185 @@
 /* The config header is always included first. */
 #include "iot_config.h"
 
-#endif
+/*---------------------------- Jobs handle types ----------------------------*/
+
+/**
+ * @handles{jobs,Jobs library}
+ */
+
+/**
+ * @ingroup jobs_datatypes_handles
+ * @brief Opaque handle that references an in-progress Jobs operation.
+ */
+typedef struct _jobsOperation * AwsIotJobsOperation_t;
+
+/*-------------------------- Jobs enumerated types --------------------------*/
+
+/**
+ * @enums{jobs,Jobs library}
+ */
+
+/**
+ * @ingroup jobs_datatypes_enums
+ * @brief Return codes of [Jobs functions](@ref jobs_functions).
+ *
+ * The function @ref jobs_function_strerror can be used to get a return code's
+ * description.
+ *
+ * The values between #AWS_IOT_JOBS_INVALID_TOPIC and #AWS_IOT_JOBS_TERMINAL_STATE
+ * may be returned by the Jobs service upon failure of a Jobs operation. See [this page]
+ * (https://docs.aws.amazon.com/iot/latest/developerguide/jobs-api.html#jobs-mqtt-error-response)
+ * for more information.
+ */
+typedef enum AwsIotJobsError
+{
+    /**
+     * @brief Jobs operation completed successfully.
+     */
+    AWS_IOT_JOBS_SUCCESS = 0,
+
+    /**
+     * @brief Jobs operation queued, awaiting result.
+     */
+    AWS_IOT_JOBS_STATUS_PENDING,
+
+    /**
+     * @brief Initialization failed.
+     */
+    AWS_IOT_JOBS_INIT_FAILED,
+
+    /**
+     * @brief At least one parameter is invalid.
+     */
+    AWS_IOT_JOBS_BAD_PARAMETER,
+
+    /**
+     * @brief Jobs operation failed because of memory allocation failure.
+     */
+    AWS_IOT_JOBS_NO_MEMORY,
+
+    /**
+     * @brief Jobs operation failed because of failure in MQTT library.
+     */
+    AWS_IOT_JOBS_MQTT_ERROR,
+
+    /**
+     * @brief Response received from Jobs service not understood.
+     */
+    AWS_IOT_JOBS_BAD_RESPONSE,
+
+    /**
+     * @brief A blocking Jobs operation timed out.
+     */
+    AWS_IOT_JOBS_TIMEOUT,
+
+    /**
+     * @brief Jobs operation failed: A request was sent to an unknown topic.
+     */
+    AWS_IOT_JOBS_INVALID_TOPIC,
+
+    /**
+     * @brief Jobs operation failed: The contents of the request were not understood.
+     *
+     * Jobs requests must be UTF-8 encoded JSON documents.
+     */
+    AWS_IOT_JOBS_INVALID_JSON,
+
+    /**
+     * @brief Jobs operation failed: The contents of the request were invalid.
+     */
+    AWS_IOT_JOBS_INVALID_REQUEST,
+
+    /**
+     * @brief Jobs operation failed: An update attempted to change the job execution
+     * to an invalid state.
+     */
+    AWS_IOT_JOBS_INVALID_STATE,
+
+    /**
+     * @brief Jobs operation failed: The specified job execution does not exist.
+     */
+    AWS_IOT_JOBS_NOT_FOUND,
+
+    /**
+     * @brief Jobs operation failed: The Jobs service expected a version that did
+     * not match what was in the request.
+     */
+    AWS_IOT_JOBS_VERSION_MISMATCH,
+
+    /**
+     * @brief Jobs operation failed: The Jobs service encountered an internal error.
+     */
+    AWS_IOT_JOBS_INTERNAL_ERROR,
+
+    /**
+     * @brief Jobs operation failed: The request was throttled.
+     */
+    AWS_IOT_JOBS_THROTTLED,
+
+    /**
+     * @brief Jobs operation failed: Attempt to describe a Job in a terminal state.
+     */
+    AWS_IOT_JOBS_TERMINAL_STATE
+} AwsIotJobsError_t;
+
+/**
+ * @ingroup jobs_datatypes_enums
+ * @brief Possible states of jobs.
+ *
+ * The function @ref jobs_function_statename can be used to get a state's
+ * description.
+ *
+ * See [this page]
+ * (https://docs.aws.amazon.com/iot/latest/apireference/API_iot-jobs-data_JobExecutionState.html)
+ * for more information on Job states.
+ */
+typedef enum AwsIotJobsState
+{
+    /**
+     * @brief A Job is queued and awaiting execution.
+     */
+    AWS_IOT_JOB_STATE_QUEUED,
+
+    /**
+     * @brief A Job is currently executing.
+     */
+    AWS_IOT_JOB_STATE_IN_PROGRESS,
+
+    /**
+     * @brief A Job has failed. This is a terminal state.
+     */
+    AWS_IOT_JOB_STATE_FAILED,
+
+    /**
+     * @brief A Job has succeeded. This is a terminal state.
+     */
+    AWS_IOT_JOB_STATE_SUCCEEDED,
+
+    /**
+     * @brief A Job was canceled. This is a terminal state.
+     */
+    AWS_IOT_JOB_STATE_CANCELED,
+
+    /**
+     * @brief A Job's timer has expired. This is a terminal state.
+     *
+     * Jobs are considered timed out if they remain [IN_PROGRESS]
+     * (@ref AWS_IOT_JOB_STATE_IN_PROGRESS) for more than their
+     * `inProgressTimeoutInMinutes` property or a [Job update](@ref jobs_function_update)
+     * was not sent within [stepTimeoutInMinutes](@ref x).
+     */
+    AWS_IOT_JOB_STATE_TIMED_OUT,
+
+    /**
+     * @brief A Job was rejected by the device. This is a terminal state.
+     */
+    AWS_IOT_JOB_STATE_REJECTED,
+
+    /**
+     * @brief A Job was removed. This is a terminal state.
+     */
+    AWS_IOT_JOB_STATE_REMOVED
+} AwsIotJobState_t;
+
+#endif /* ifndef AWS_IOT_JOBS_TYPES_H_ */
