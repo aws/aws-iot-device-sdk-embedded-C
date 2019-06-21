be35f5a7ce4bcd528a1b7e60e788d2b93c9fd7b7
Gordon Wang <36049150+gordonwang0@users.noreply.github.com>
2019-06-20 20:07:02 -0400

    Move MQTT mocks to separate files  (#478)

--- tests/shadow/unit/aws_iot_tests_shadow_api.c
+++ tests/shadow/unit/aws_iot_tests_shadow_api.c
@@ -37,26 +37,14 @@
 /* Shadow internal include. */
 #include "private/aws_iot_shadow_internal.h"
 
-/* Undefine logging configuration set in Shadow internal header. */
-#undef LIBRARY_LOG_NAME
-#undef LIBRARY_LOG_LEVEL
-
-/* MQTT internal include. */
-#include "private/iot_mqtt_internal.h"
-
-/* Undefine logging configuration set in MQTT internal header. */
-#undef LIBRARY_LOG_NAME
-#undef LIBRARY_LOG_LEVEL
-
-/* Platform layer includes. */
-#include "platform/iot_clock.h"
-#include "platform/iot_threads.h"
-
 /* Test framework includes. */
 #include "unity_fixture.h"
 
-/* MQTT test access include. */
-#include "iot_test_access_mqtt.h"
+/* MQTT include. */
+#include "iot_mqtt.h"
+
+/* MQTT mock include. */
+#include "iot_tests_mqtt_mock.h"
 
 /* Require Shadow library asserts to be enabled for these tests. The Shadow
  * assert function is used to abort the tests on failure from the MQTT send
@@ -87,276 +75,19 @@
 /**
  * @brief The Thing Name shared among all the tests.
  */
-#define TEST_THING_NAME                "TestThingName"
+#define TEST_THING_NAME           "TestThingName"
 
 /**
  * @brief The length of #TEST_THING_NAME.
  */
-#define TEST_THING_NAME_LENGTH         ( sizeof( TEST_THING_NAME ) - 1 )
-
-/**
- * @brief A delay that simulates the time required for an MQTT packet to be sent
- * to the server and for the server to send a response.
- */
-#define NETWORK_ROUND_TRIP_TIME_MS     ( 25 )
-
-/**
- * @brief The maximum size of any MQTT acknowledgement packet (e.g. SUBACK,
- * PUBACK, UNSUBACK) used in these tests.
- */
-#define ACKNOWLEDGEMENT_PACKET_SIZE    ( 5 )
+#define TEST_THING_NAME_LENGTH    ( sizeof( TEST_THING_NAME ) - 1 )
 
 /*-----------------------------------------------------------*/
 
 /**
- * @brief Context for calls to the network receive function.
+ * @brief The MQTT connection shared among the tests.
  */
-typedef struct _receiveContext
-{
-    const uint8_t * pData; /**< @brief The data to receive. */
-    size_t dataLength;     /**< @brief Length of data. */
-    size_t dataIndex;      /**< @brief Next byte of data to read. */
-} _receiveContext_t;
-
-/*-----------------------------------------------------------*/
-
-/**
- * @brief The MQTT connection object shared among all the tests.
- */
-static _mqttConnection_t * _pMqttConnection = IOT_MQTT_CONNECTION_INITIALIZER;
-
-/**
- * @brief The #IotNetworkInterface_t to share among the tests.
- */
-static IotNetworkInterface_t _networkInterface = { 0 };
-
-/**
- * @brief Timer used to simulate a response from the network.
- */
-static IotTimer_t _receiveTimer;
-
-/**
- * @brief Synchronizes the MQTT send and receive threads in these tests.
- */
-static IotMutex_t _lastPacketMutex;
-
-/**
- * @brief The type of the last packet sent by the send thread.
- *
- * Must be one of: PUBLISH, SUBSCRIBE, UNSUBSCRIBE.
- */
-static uint8_t _lastPacketType = 0;
-
-/**
- * @brief The packet identifier of the last packet send by the send thread.
- */
-static uint16_t _lastPacketIdentifier = 0;
-
-/*-----------------------------------------------------------*/
-
-/**
- * @brief Invokes the MQTT receive callback to simulate a response received from
- * the network.
- */
-static void _receiveThread( void * pArgument )
-{
-    uint8_t pReceivedData[ ACKNOWLEDGEMENT_PACKET_SIZE ] = { 0 };
-    _receiveContext_t receiveContext = { 0 };
-
-    receiveContext.pData = pReceivedData;
-    receiveContext.dataLength = ACKNOWLEDGEMENT_PACKET_SIZE;
-
-    /* Silence warnings about unused parameters. */
-    ( void ) pArgument;
-
-    /* Lock mutex to read and process the last packet sent. */
-    IotMutex_Lock( &_lastPacketMutex );
-
-    /* Ensure that the last packet type and identifier were set. */
-    AwsIotShadow_Assert( _lastPacketType != 0 );
-    AwsIotShadow_Assert( _lastPacketIdentifier != 0 );
-
-    /* Set the packet identifier in the ACK packet. */
-    pReceivedData[ 2 ] = UINT16_HIGH_BYTE( _lastPacketIdentifier );
-    pReceivedData[ 3 ] = UINT16_LOW_BYTE( _lastPacketIdentifier );
-
-    /* Create the corresponding ACK packet based on the last packet type. */
-    switch( _lastPacketType )
-    {
-        case MQTT_PACKET_TYPE_PUBLISH:
-
-            pReceivedData[ 0 ] = MQTT_PACKET_TYPE_PUBACK;
-            pReceivedData[ 1 ] = 2;
-            receiveContext.dataLength = 4;
-            break;
-
-        case MQTT_PACKET_TYPE_SUBSCRIBE:
-
-            pReceivedData[ 0 ] = MQTT_PACKET_TYPE_SUBACK;
-            pReceivedData[ 1 ] = 3;
-            pReceivedData[ 4 ] = 1;
-            receiveContext.dataLength = 5;
-            break;
-
-        case MQTT_PACKET_TYPE_UNSUBSCRIBE:
-
-            pReceivedData[ 0 ] = MQTT_PACKET_TYPE_UNSUBACK;
-            pReceivedData[ 1 ] = 2;
-            receiveContext.dataLength = 4;
-            break;
-
-        default:
-
-            /* The only valid outgoing packets are PUBLISH, SUBSCRIBE, and
-             * UNSUBSCRIBE. Abort if any other packet is found. */
-            AwsIotShadow_Assert( 0 );
-    }
-
-    /* Call the MQTT receive callback to process the ACK packet. */
-    IotMqtt_ReceiveCallback( &receiveContext,
-                             _pMqttConnection );
-
-    IotMutex_Unlock( &_lastPacketMutex );
-}
-
-/*-----------------------------------------------------------*/
-
-/**
- * @brief A send function that always "succeeds". It also sets the receive
- * timer to respond with an ACK when necessary.
- */
-static size_t _sendSuccess( void * pSendContext,
-                            const uint8_t * pMessage,
-                            size_t messageLength )
-{
-    IotMqttError_t status = IOT_MQTT_STATUS_PENDING;
-    _mqttOperation_t deserializedPublish = { .link = { 0 } };
-    _mqttPacket_t mqttPacket = { .u.pMqttConnection = NULL };
-    _receiveContext_t receiveContext = { 0 };
-
-    /* Ignore the send context. */
-    ( void ) pSendContext;
-
-    /* Read the packet type, which is the first byte in the message. */
-    mqttPacket.type = *pMessage;
-
-    /* Set the members of the receive context. */
-    receiveContext.pData = pMessage + 1;
-    receiveContext.dataLength = messageLength;
-
-    /* Lock the mutex to modify the information on the last packet sent. */
-    IotMutex_Lock( &_lastPacketMutex );
-
-    /* Read the remaining length. */
-    mqttPacket.remainingLength = _IotMqtt_GetRemainingLength( &receiveContext,
-                                                              &_networkInterface );
-    AwsIotShadow_Assert( mqttPacket.remainingLength != MQTT_REMAINING_LENGTH_INVALID );
-
-    /* Set the last packet type based on the outgoing message. */
-    switch( mqttPacket.type & 0xf0 )
-    {
-        case MQTT_PACKET_TYPE_PUBLISH:
-
-            /* Only set the last packet type to PUBLISH for QoS 1. */
-            if( ( ( *pMessage & 0x06 ) >> 1 ) == 1 )
-            {
-                _lastPacketType = MQTT_PACKET_TYPE_PUBLISH;
-            }
-            else
-            {
-                _lastPacketType = 0;
-                _lastPacketIdentifier = 0;
-            }
-
-            break;
-
-        case ( MQTT_PACKET_TYPE_SUBSCRIBE & 0xf0 ):
-            _lastPacketType = MQTT_PACKET_TYPE_SUBSCRIBE;
-            break;
-
-        case ( MQTT_PACKET_TYPE_UNSUBSCRIBE & 0xf0 ):
-            _lastPacketType = MQTT_PACKET_TYPE_UNSUBSCRIBE;
-            break;
-
-        default:
-
-            /* The only valid outgoing packets are PUBLISH, SUBSCRIBE, and
-             * UNSUBSCRIBE. Abort if any other packet is found. */
-            AwsIotShadow_Assert( 0 );
-    }
-
-    /* Check if a network response is needed. */
-    if( _lastPacketType != 0 )
-    {
-        /* Save the packet identifier as the last packet identifier. */
-        if( _lastPacketType != MQTT_PACKET_TYPE_PUBLISH )
-        {
-            _lastPacketIdentifier = UINT16_DECODE( receiveContext.pData + receiveContext.dataIndex );
-            status = IOT_MQTT_SUCCESS;
-        }
-        else
-        {
-            mqttPacket.u.pIncomingPublish = &deserializedPublish;
-            mqttPacket.pRemainingData = ( uint8_t * ) pMessage + ( messageLength - mqttPacket.remainingLength );
-
-            status = _IotMqtt_DeserializePublish( &mqttPacket );
-            _lastPacketIdentifier = mqttPacket.packetIdentifier;
-        }
-
-        AwsIotShadow_Assert( status == IOT_MQTT_SUCCESS );
-        AwsIotShadow_Assert( _lastPacketIdentifier != 0 );
-
-        /* Set the receive thread to run after a "network round-trip". */
-        AwsIotShadow_Assert( IotClock_TimerArm( &_receiveTimer,
-                                                NETWORK_ROUND_TRIP_TIME_MS,
-                                                0 ) == true );
-    }
-
-    IotMutex_Unlock( &_lastPacketMutex );
-
-    /* Return the message length to simulate a successful send. */
-    return messageLength;
-}
-
-/*-----------------------------------------------------------*/
-
-/**
- * @brief Simulates a network receive function.
- */
-static size_t _receive( void * pConnection,
-                        uint8_t * pBuffer,
-                        size_t bytesRequested )
-{
-    size_t bytesReceived = 0;
-    _receiveContext_t * pReceiveContext = pConnection;
-
-    AwsIotShadow_Assert( bytesRequested != 0 );
-    AwsIotShadow_Assert( pReceiveContext->dataIndex < pReceiveContext->dataLength );
-
-    /* Calculate how much data to copy. */
-    const size_t dataAvailable = pReceiveContext->dataLength - pReceiveContext->dataIndex;
-
-    if( bytesRequested > dataAvailable )
-    {
-        bytesReceived = dataAvailable;
-    }
-    else
-    {
-        bytesReceived = bytesRequested;
-    }
-
-    /* Copy data into given buffer. */
-    if( bytesReceived > 0 )
-    {
-        ( void ) memcpy( pBuffer,
-                         pReceiveContext->pData + pReceiveContext->dataIndex,
-                         bytesReceived );
-
-        pReceiveContext->dataIndex += bytesReceived;
-    }
-
-    return bytesReceived;
-}
+static IotMqttConnection_t _pMqttConnection = IOT_MQTT_CONNECTION_INITIALIZER;
 
 /*-----------------------------------------------------------*/
 
@@ -372,8 +103,6 @@ TEST_GROUP( Shadow_Unit_API );
  */
 TEST_SETUP( Shadow_Unit_API )
 {
-    IotMqttNetworkInfo_t networkInfo = IOT_MQTT_NETWORK_INFO_INITIALIZER;
-
     /* Initialize SDK. */
     TEST_ASSERT_EQUAL_INT( true, IotSdk_Init() );
 
@@ -383,29 +112,8 @@ TEST_SETUP( Shadow_Unit_API )
     /* Initialize the Shadow library. */
     TEST_ASSERT_EQUAL( AWS_IOT_SHADOW_SUCCESS, AwsIotShadow_Init( 0 ) );
 
-    /* Clear the last packet type and identifier. */
-    _lastPacketType = 0;
-    _lastPacketIdentifier = 0;
-
-    /* Create the mutex that synchronizes the receive callback and send thread. */
-    TEST_ASSERT_EQUAL_INT( true, IotMutex_Create( &_lastPacketMutex, false ) );
-
-    /* Create the receive thread timer. */
-    IotClock_TimerCreate( &_receiveTimer,
-                          _receiveThread,
-                          NULL );
-
-    /* Set the network interface send function. */
-    ( void ) memset( &_networkInterface, 0x00, sizeof( IotNetworkInterface_t ) );
-    _networkInterface.send = _sendSuccess;
-    _networkInterface.receive = _receive;
-    networkInfo.pNetworkInterface = &_networkInterface;
-
-    /* Initialize the MQTT connection object to use for the Shadow tests. */
-    _pMqttConnection = IotTestMqtt_createMqttConnection( false,
-                                                         &networkInfo,
-                                                         0 );
-    TEST_ASSERT_NOT_NULL( _pMqttConnection );
+    /* Initialize MQTT mock. */
+    TEST_ASSERT_EQUAL_INT( true, IotTest_MqttMockInit( &_pMqttConnection ) );
 }
 
 /*-----------------------------------------------------------*/
@@ -415,8 +123,8 @@ TEST_SETUP( Shadow_Unit_API )
  */
 TEST_TEAR_DOWN( Shadow_Unit_API )
 {
-    /* Clean up the MQTT connection object. */
-    IotMqtt_Disconnect( _pMqttConnection, IOT_MQTT_FLAG_CLEANUP_ONLY );
+    /* Clean up MQTT mock. */
+    IotTest_MqttMockCleanup();
 
     /* Clean up the Shadow library. */
     AwsIotShadow_Cleanup();
@@ -426,16 +134,6 @@ TEST_TEAR_DOWN( Shadow_Unit_API )
 
     /* Clean up SDK. */
     IotSdk_Cleanup();
-
-    /* Destroy the receive thread timer. */
-    IotClock_TimerDestroy( &_receiveTimer );
-
-    /* Wait for the receive thread to finish and release the last packet mutex. */
-    IotMutex_Lock( &_lastPacketMutex );
-
-    /* Destroy the last packet mutex. */
-    IotMutex_Unlock( &_lastPacketMutex );
-    IotMutex_Destroy( &_lastPacketMutex );
 }
 
 /*-----------------------------------------------------------*/
