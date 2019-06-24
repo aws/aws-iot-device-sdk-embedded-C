bfc723f471af054bcbcd3091231944cf1bb58c6a
Gordon Wang <36049150+gordonwang0@users.noreply.github.com>
2019-06-24 17:15:13 -0400

    Add Jobs serialize DESCRIBE function (#490)

--- lib/source/jobs/aws_iot_jobs_serialize.c
+++ lib/source/jobs/aws_iot_jobs_serialize.c
@@ -70,6 +70,34 @@
 #define STEP_TIMEOUT_KEY_LENGTH             ( sizeof( STEP_TIMEOUT_KEY ) - 1 )
 
 /**
+ * @brief JSON key representing the "include Job document" flag.
+ */
+#define INCLUDE_JOB_DOCUMENT_KEY            "includeJobDocument"
+
+/**
+ * @brief Length of #INCLUDE_JOB_DOCUMENT_KEY.
+ */
+#define INCLUDE_JOB_DOCUMENT_KEY_LENGTH     ( sizeof( INCLUDE_JOB_DOCUMENT_KEY ) - 1 )
+
+/**
+ * @brief JSON key representing the Jobs execution number.
+ */
+#define EXECUTION_NUMBER_KEY                "executionNumber"
+
+/**
+ * @brief Length of #EXECUTION_NUMBER_KEY.
+ */
+#define EXECUTION_NUMBER_KEY_LENGTH         ( sizeof( EXECUTION_NUMBER_KEY ) - 1 )
+
+/**
+ * @brief Maximum length of the execution number when represented as a string.
+ *
+ * The execution number is a 32-bit integer. This can be represented in 10 digits,
+ * plus 1 for a possible negative sign, plus a NULL-terminator.
+ */
+#define EXECUTION_NUMBER_STRING_LENGTH      ( 12 )
+
+/**
  * @brief Append a string to a buffer.
  *
  * Also updates `copyOffset` with `stringLength`.
@@ -86,15 +114,23 @@
 /*-----------------------------------------------------------*/
 
 /**
- * @brief Generate and place a client token in the given buffer.
+ * @brief Place a client token in the given buffer.
  *
  * @param[in] pBuffer The buffer where the client token is placed.
+ * @param[in] copyOffset Offset in `pBuffer` where client token is placed.
+ * @param[in] pRequestInfo Contains information on a client token to place.
+ * @param[out] pOperation Location and length of client token are written here.
  *
  * @warning This function does not check the length of `pBuffer`! Any provided
  * buffer must be large enough to accommodate #CLIENT_TOKEN_AUTOGENERATE_LENGTH
  * characters.
+ *
+ * @return A value of `copyOffset` after the client token.
  */
-static void _generateClientToken( char * pBuffer );
+static size_t _appendClientToken( char * pBuffer,
+                                  size_t copyOffset,
+                                  const AwsIotJobsRequestInfo_t * pRequestInfo,
+                                  _jobsOperation_t * pOperation );
 
 /**
  * @brief Generates a request JSON for a GET PENDING operation.
@@ -120,24 +156,66 @@ static AwsIotJobsError_t _generateStartNextRequest( const AwsIotJobsRequestInfo_
                                                     const AwsIotJobsUpdateInfo_t * pUpdateInfo,
                                                     _jobsOperation_t * pOperation );
 
+/**
+ * @brief Generates a request JSON for a DESCRIBE operation.
+ *
+ * @param[in] pRequestInfo Common jobs request parameters.
+ * @param[in] executionNumber Job execution number to include in request.
+ * @param[in] includeJobDocument Whether the response should include the Job document.
+ * @param[in] pOperation Operation associated with the Jobs request.
+ *
+ * @return #AWS_IOT_JOBS_SUCCESS or #AWS_IOT_JOBS_NO_MEMORY.
+ */
+static AwsIotJobsError_t _generateDescribeRequest( const AwsIotJobsRequestInfo_t * pRequestInfo,
+                                                   int32_t executionNumber,
+                                                   bool includeJobDocument,
+                                                   _jobsOperation_t * pOperation );
+
 /*-----------------------------------------------------------*/
 
-static void _generateClientToken( char * pBuffer )
+static size_t _appendClientToken( char * pBuffer,
+                                  size_t copyOffset,
+                                  const AwsIotJobsRequestInfo_t * pRequestInfo,
+                                  _jobsOperation_t * pOperation )
 {
     int clientTokenLength = 0;
+    uint32_t clientToken = 0;
 
-    /* Client token length is not used when asserts are disabled. */
-    ( void ) clientTokenLength;
+    /* Place the client token key in the buffer. */
+    APPEND_STRING( pBuffer,
+                   copyOffset,
+                   AWS_IOT_CLIENT_TOKEN_KEY,
+                   AWS_IOT_CLIENT_TOKEN_KEY_LENGTH );
+    APPEND_STRING( pBuffer, copyOffset, "\":\"", 3 );
 
-    /* Take the address of the given buffer, truncated to 8 characters. This
-     * provides a client token that is very likely to be unique while in use. */
-    uint32_t clientToken = ( uint32_t ) ( ( uint64_t ) pBuffer % 100000000ULL );
+    /* Set the pointer to the client token. */
+    pOperation->pClientToken = pBuffer + copyOffset - 1;
 
-    clientTokenLength = snprintf( pBuffer,
-                                  CLIENT_TOKEN_AUTOGENERATE_LENGTH + 1,
-                                  "%08u", clientToken );
+    if( pRequestInfo->pClientToken == AWS_IOT_JOBS_CLIENT_TOKEN_AUTOGENERATE )
+    {
+        /* Take the address of the given buffer, truncated to 8 characters. This
+         * provides a client token that is very likely to be unique while in use. */
+        clientToken = ( uint32_t ) ( ( uint64_t ) pBuffer % 100000000ULL );
 
-    AwsIotJobs_Assert( clientTokenLength == CLIENT_TOKEN_AUTOGENERATE_LENGTH );
+        clientTokenLength = snprintf( pBuffer + copyOffset,
+                                      CLIENT_TOKEN_AUTOGENERATE_LENGTH + 1,
+                                      "%08u", clientToken );
+        AwsIotJobs_Assert( clientTokenLength == CLIENT_TOKEN_AUTOGENERATE_LENGTH );
+
+        copyOffset += ( size_t ) clientTokenLength;
+        pOperation->clientTokenLength = CLIENT_TOKEN_AUTOGENERATE_LENGTH + 2;
+    }
+    else
+    {
+        APPEND_STRING( pBuffer,
+                       copyOffset,
+                       pRequestInfo->pClientToken,
+                       pRequestInfo->clientTokenLength );
+
+        pOperation->clientTokenLength = pRequestInfo->clientTokenLength + 2;
+    }
+
+    return copyOffset;
 }
 
 /*-----------------------------------------------------------*/
@@ -175,32 +253,12 @@ static AwsIotJobsError_t _generateGetPendingRequest( const AwsIotJobsRequestInfo
 
     /* Construct the request JSON, which consists of just a clientToken key. */
     APPEND_STRING( pJobsRequest, copyOffset, "{\"", 2 );
-    APPEND_STRING( pJobsRequest, copyOffset, AWS_IOT_CLIENT_TOKEN_KEY, AWS_IOT_CLIENT_TOKEN_KEY_LENGTH );
-    APPEND_STRING( pJobsRequest, copyOffset, "\":\"", 3 );
-
-    if( pRequestInfo->pClientToken == AWS_IOT_JOBS_CLIENT_TOKEN_AUTOGENERATE )
-    {
-        _generateClientToken( pJobsRequest + copyOffset );
-        copyOffset += CLIENT_TOKEN_AUTOGENERATE_LENGTH;
-
-        pOperation->clientTokenLength = CLIENT_TOKEN_AUTOGENERATE_LENGTH + 2;
-    }
-    else
-    {
-        APPEND_STRING( pJobsRequest,
-                       copyOffset,
-                       pRequestInfo->pClientToken,
-                       pRequestInfo->clientTokenLength );
-
-        pOperation->clientTokenLength = pRequestInfo->clientTokenLength + 2;
-    }
-
+    copyOffset = _appendClientToken( pJobsRequest, copyOffset, pRequestInfo, pOperation );
     APPEND_STRING( pJobsRequest, copyOffset, "\"}", 2 );
 
     /* Set the output parameters. */
     pOperation->pJobsRequest = pJobsRequest;
     pOperation->jobsRequestLength = requestLength;
-    pOperation->pClientToken = pJobsRequest + AWS_IOT_CLIENT_TOKEN_KEY_LENGTH + 4;
 
     /* Ensure offsets are valid. */
     AwsIotJobs_Assert( copyOffset == requestLength );
@@ -304,31 +362,7 @@ static AwsIotJobsError_t _generateStartNextRequest( const AwsIotJobsRequestInfo_
     APPEND_STRING( pJobsRequest, copyOffset, ",\"", 2 );
 
     /* Add client token. */
-    APPEND_STRING( pJobsRequest,
-                   copyOffset,
-                   AWS_IOT_CLIENT_TOKEN_KEY,
-                   AWS_IOT_CLIENT_TOKEN_KEY_LENGTH );
-    APPEND_STRING( pJobsRequest, copyOffset, "\":\"", 3 );
-
-    /* Set the pointer to the client token. */
-    pOperation->pClientToken = pJobsRequest + copyOffset - 1;
-
-    if( pRequestInfo->pClientToken == AWS_IOT_JOBS_CLIENT_TOKEN_AUTOGENERATE )
-    {
-        _generateClientToken( pJobsRequest + copyOffset );
-        copyOffset += CLIENT_TOKEN_AUTOGENERATE_LENGTH;
-
-        pOperation->clientTokenLength = CLIENT_TOKEN_AUTOGENERATE_LENGTH + 2;
-    }
-    else
-    {
-        APPEND_STRING( pJobsRequest,
-                       copyOffset,
-                       pRequestInfo->pClientToken,
-                       pRequestInfo->clientTokenLength );
-
-        pOperation->clientTokenLength = pRequestInfo->clientTokenLength + 2;
-    }
+    copyOffset = _appendClientToken( pJobsRequest, copyOffset, pRequestInfo, pOperation );
 
     APPEND_STRING( pJobsRequest, copyOffset, "\"}", 2 );
 
@@ -343,6 +377,123 @@ static AwsIotJobsError_t _generateStartNextRequest( const AwsIotJobsRequestInfo_
                        pOperation->pJobsRequest + pOperation->jobsRequestLength );
 
     IotLogDebug( "Jobs START NEXT request: %.*s",
+                 pOperation->jobsRequestLength,
+                 pOperation->pJobsRequest );
+
+    IOT_FUNCTION_EXIT_NO_CLEANUP();
+}
+
+/*-----------------------------------------------------------*/
+
+static AwsIotJobsError_t _generateDescribeRequest( const AwsIotJobsRequestInfo_t * pRequestInfo,
+                                                   int32_t executionNumber,
+                                                   bool includeJobDocument,
+                                                   _jobsOperation_t * pOperation )
+{
+    IOT_FUNCTION_ENTRY( AwsIotJobsError_t, AWS_IOT_JOBS_SUCCESS );
+    char * pJobsRequest = NULL;
+    size_t copyOffset = 0;
+    size_t requestLength = MINIMUM_REQUEST_LENGTH;
+    char pExecutionNumber[ EXECUTION_NUMBER_STRING_LENGTH ] = { 0 };
+    int executionNumberLength = 0;
+
+    /* Add the "include job document" flag if false. The default value is true,
+     * so the flag is not needed if true. */
+    if( includeJobDocument == false )
+    {
+        /* Add the length of "includeJobDocument" plus 4 for 2 quotes, a colon,
+         * and a comma. */
+        requestLength += INCLUDE_JOB_DOCUMENT_KEY_LENGTH + 4;
+
+        /* Add the length of "false". */
+        requestLength += 5;
+    }
+
+    /* Add the length of the execution number if present. */
+    if( executionNumber != AWS_IOT_JOBS_NO_EXECUTION_NUMBER )
+    {
+        /* Convert the execution number to a string. */
+        executionNumberLength = snprintf( pExecutionNumber,
+                                          EXECUTION_NUMBER_STRING_LENGTH,
+                                          "%d",
+                                          executionNumber );
+        AwsIotJobs_Assert( executionNumberLength > 0 );
+        AwsIotJobs_Assert( executionNumberLength < EXECUTION_NUMBER_STRING_LENGTH );
+
+        requestLength += EXECUTION_NUMBER_KEY_LENGTH + 4;
+        requestLength += ( size_t ) executionNumberLength;
+    }
+
+    /* Add the length of the client token. */
+    if( pRequestInfo->pClientToken != AWS_IOT_JOBS_CLIENT_TOKEN_AUTOGENERATE )
+    {
+        AwsIotJobs_Assert( pRequestInfo->clientTokenLength > 0 );
+
+        requestLength += pRequestInfo->clientTokenLength;
+    }
+    else
+    {
+        requestLength += CLIENT_TOKEN_AUTOGENERATE_LENGTH;
+    }
+
+    /* Allocate memory for the request JSON. */
+    pJobsRequest = AwsIotJobs_MallocString( requestLength );
+
+    if( pJobsRequest == NULL )
+    {
+        IOT_SET_AND_GOTO_CLEANUP( AWS_IOT_JOBS_NO_MEMORY );
+    }
+
+    /* Clear the request JSON. */
+    ( void ) memset( pJobsRequest, 0x00, requestLength );
+
+    /* Construct the request JSON. */
+    APPEND_STRING( pJobsRequest, copyOffset, "{\"", 2 );
+
+    /* Add the "include job document" flag if false. */
+    if( includeJobDocument == false )
+    {
+        APPEND_STRING( pJobsRequest,
+                       copyOffset,
+                       INCLUDE_JOB_DOCUMENT_KEY,
+                       INCLUDE_JOB_DOCUMENT_KEY_LENGTH );
+        APPEND_STRING( pJobsRequest, copyOffset, "\":false,\"", 9 );
+    }
+
+    /* Add the length of the execution number if present. */
+    if( executionNumber != AWS_IOT_JOBS_NO_EXECUTION_NUMBER )
+    {
+        APPEND_STRING( pJobsRequest,
+                       copyOffset,
+                       EXECUTION_NUMBER_KEY,
+                       EXECUTION_NUMBER_KEY_LENGTH );
+        APPEND_STRING( pJobsRequest,
+                       copyOffset,
+                       "\":",
+                       2 );
+        APPEND_STRING( pJobsRequest,
+                       copyOffset,
+                       pExecutionNumber,
+                       executionNumberLength );
+        APPEND_STRING( pJobsRequest, copyOffset, ",\"", 2 );
+    }
+
+    /* Add client token. */
+    copyOffset = _appendClientToken( pJobsRequest, copyOffset, pRequestInfo, pOperation );
+
+    APPEND_STRING( pJobsRequest, copyOffset, "\"}", 2 );
+
+    /* Set the output parameters. */
+    pOperation->pJobsRequest = pJobsRequest;
+    pOperation->jobsRequestLength = requestLength;
+
+    /* Ensure offsets are valid. */
+    AwsIotJobs_Assert( copyOffset == requestLength );
+    AwsIotJobs_Assert( pOperation->pClientToken > pOperation->pJobsRequest );
+    AwsIotJobs_Assert( pOperation->pClientToken <
+                       pOperation->pJobsRequest + pOperation->jobsRequestLength );
+
+    IotLogDebug( "Jobs DESCRIBE request: %.*s",
                  pOperation->jobsRequestLength,
                  pOperation->pJobsRequest );
 
@@ -372,6 +523,10 @@ AwsIotJobsError_t _AwsIotJobs_GenerateJsonRequest( _jobsOperationType_t type,
             break;
 
         case JOBS_DESCRIBE:
+            status = _generateDescribeRequest( pRequestInfo,
+                                               pRequestContents->describe.executionNumber,
+                                               pRequestContents->describe.includeJobDocument,
+                                               pOperation );
             break;
 
         default:
