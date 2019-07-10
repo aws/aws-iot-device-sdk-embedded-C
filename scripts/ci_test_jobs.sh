0fa4601d5a0d7c3bf57ad053b766ccbd9dd4eb0e
Gordon Wang <36049150+gordonwang0@users.noreply.github.com>
2019-07-10 13:50:44 +0800

    Fix script

--- scripts/ci_test_jobs.sh
+++ scripts/ci_test_jobs.sh
@@ -52,15 +52,15 @@ cmake .. -DIOT_BUILD_TESTS=1 -DCMAKE_BUILD_TYPE=Debug -DCMAKE_C_FLAGS="$CMAKE_FL
 make -j2 aws_iot_tests_jobs
 
 # Run tests.
-if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then create_jobs fi
+if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then create_jobs; fi
 run_tests
-if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then delete_jobs fi
+if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then delete_jobs; fi
 
 # Rebuild in static memory mode.
 cmake .. -DIOT_BUILD_TESTS=1 -DCMAKE_BUILD_TYPE=Debug -DCMAKE_C_FLAGS="$CMAKE_FLAGS -DIOT_STATIC_MEMORY_ONLY=1"
 make -j2 aws_iot_tests_jobs
 
 # Run tests in static memory mode.
-if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then create_jobs fi
+if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then create_jobs; fi
 run_tests
-if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then delete_jobs fi
+if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then delete_jobs; fi
