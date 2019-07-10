dd1e0a5e0005e836688f32e2d20f0d635cdd9bde
Gordon Wang <36049150+gordonwang0@users.noreply.github.com>
2019-07-10 14:15:15 +0800

    Create Jobs with automated tests (#513)

--- scripts/ci_test_coverage.sh
+++ scripts/ci_test_coverage.sh
@@ -6,6 +6,31 @@
 # Exit on any nonzero return code.
 set -ev
 
+# Query the AWS account ID.
+AWS_ACCOUNT_ID=$(aws sts get-caller-identity --output text --query 'Account')
+
+# Prefix of Job IDs used in the tests, set by the create_jobs function.
+JOB_PREFIX=""
+
+# Function to create Jobs to use in the tests.
+create_jobs() {
+    JOB_PREFIX=$IOT_IDENTIFIER-$(jot -r 1 1 100000)
+    aws iot create-job \
+        --job-id $JOB_PREFIX-1 \
+        --targets arn:aws:iot:$AWS_DEFAULT_REGION:$AWS_ACCOUNT_ID:thing/$IOT_IDENTIFIER \
+        --document '{"action":"print","message":"Hello world!"}'
+    aws iot create-job \
+        --job-id $JOB_PREFIX-2 \
+        --targets arn:aws:iot:$AWS_DEFAULT_REGION:$AWS_ACCOUNT_ID:thing/$IOT_IDENTIFIER \
+        --document '{"action":"print","message":"Hello world!"}'
+}
+
+# Function to delete Jobs and clean up the tests.
+delete_jobs() {
+    aws iot delete-job --job-id $JOB_PREFIX-1 --force
+    aws iot delete-job --job-id $JOB_PREFIX-2 --force
+}
+
 # Build tests and demos against AWS IoT with coverage.
 cmake .. -DIOT_BUILD_TESTS=1 -DCMAKE_BUILD_TYPE=Debug -DCMAKE_C_FLAGS="$AWS_IOT_CREDENTIAL_DEFINES -DAWS_IOT_TEST_SHADOW_THING_NAME=\"\\\"$IOT_IDENTIFIER\\\"\" -DAWS_IOT_TEST_JOBS_THING_NAME=\"\\\"$IOT_IDENTIFIER\\\"\" -DIOT_LOG_LEVEL_GLOBAL=IOT_LOG_DEBUG -DIOT_DEMO_MQTT_TOPIC_PREFIX=\"\\\"$IOT_IDENTIFIER\\\"\" -DIOT_TEST_COVERAGE=1 --coverage"
 make -j2
@@ -20,7 +45,9 @@ make -j2
 ./bin/aws_iot_tests_shadow
 
 # Run Jobs tests with code coverage.
+create_jobs
 ./bin/aws_iot_tests_jobs
+delete_jobs
 
 # Generate code coverage results, but only for files in lib/.
 lcov --directory . --capture --output-file coverage.info
--- scripts/ci_test_jobs.sh
+++ scripts/ci_test_jobs.sh
@@ -5,6 +5,14 @@
 # Exit on any nonzero return code.
 set -e
 
+# Query the AWS account ID.
+if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
+    AWS_ACCOUNT_ID=$(aws sts get-caller-identity --output text --query 'Account')
+fi
+
+# Prefix of Job IDs used in the tests, set by the create_jobs function.
+JOB_PREFIX=""
+
 # Function for running the existing test executables.
 run_tests() {
     # For commit builds, run the full Jobs tests. For pull request builds,
@@ -17,6 +25,25 @@ run_tests() {
     fi
 }
 
+# Function to create Jobs to use in the tests.
+create_jobs() {
+    JOB_PREFIX=$IOT_IDENTIFIER-$(jot -r 1 1 100000)
+    aws iot create-job \
+        --job-id $JOB_PREFIX-1 \
+        --targets arn:aws:iot:$AWS_DEFAULT_REGION:$AWS_ACCOUNT_ID:thing/$IOT_IDENTIFIER \
+        --document '{"action":"print","message":"Hello world!"}'
+    aws iot create-job \
+        --job-id $JOB_PREFIX-2 \
+        --targets arn:aws:iot:$AWS_DEFAULT_REGION:$AWS_ACCOUNT_ID:thing/$IOT_IDENTIFIER \
+        --document '{"action":"print","message":"Hello world!"}'
+}
+
+# Function to delete Jobs and clean up the tests.
+delete_jobs() {
+    aws iot delete-job --job-id $JOB_PREFIX-1 --force
+    aws iot delete-job --job-id $JOB_PREFIX-2 --force
+}
+
 # CMake compiler flags for building Jobs.
 CMAKE_FLAGS="$AWS_IOT_CREDENTIAL_DEFINES -DAWS_IOT_TEST_JOBS_THING_NAME=\"\\\"$IOT_IDENTIFIER\\\"\" $COMPILER_OPTIONS"
 
@@ -25,11 +52,15 @@ cmake .. -DIOT_BUILD_TESTS=1 -DCMAKE_BUILD_TYPE=Debug -DCMAKE_C_FLAGS="$CMAKE_FL
 make -j2 aws_iot_tests_jobs
 
 # Run tests.
+if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then create_jobs; fi
 run_tests
+if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then delete_jobs; fi
 
 # Rebuild in static memory mode.
 cmake .. -DIOT_BUILD_TESTS=1 -DCMAKE_BUILD_TYPE=Debug -DCMAKE_C_FLAGS="$CMAKE_FLAGS -DIOT_STATIC_MEMORY_ONLY=1"
 make -j2 aws_iot_tests_jobs
 
 # Run tests in static memory mode.
+if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then create_jobs; fi
 run_tests
+if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then delete_jobs; fi
