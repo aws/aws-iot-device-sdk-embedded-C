6aed9b209469fb5d91759aa0d231467c3a5adc43
Gordon Wang <36049150+gordonwang0@users.noreply.github.com>
2019-07-18 05:51:54 +0800

    Fix log message in keep alive job (#526)

--- lib/source/mqtt/iot_mqtt_operation.c
+++ lib/source/mqtt/iot_mqtt_operation.c
@@ -781,9 +781,9 @@ void _IotMqtt_ProcessKeepAlive( IotTaskPool_t pTaskPool,
 
         if( taskPoolStatus == IOT_TASKPOOL_SUCCESS )
         {
-            IotLogDebug( "(MQTT connection %p) Next keep-alive job in %d ms.",
+            IotLogDebug( "(MQTT connection %p) Next keep-alive job in %lu ms.",
                          pMqttConnection,
-                         IOT_MQTT_RESPONSE_WAIT_MS );
+                         ( unsigned long ) pPingreqOperation->u.operation.periodic.ping.nextPeriodMs );
         }
         else
         {
