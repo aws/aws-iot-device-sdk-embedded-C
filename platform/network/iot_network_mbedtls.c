44029d1ada84ba17dbf14c62900afc276028352f
abhidixi11 <44424462+abhidixi11@users.noreply.github.com>
2019-09-11 14:10:08 -0700

    Update iot_network_mbedtls.c

--- platform/network/iot_network_mbedtls.c
+++ platform/network/iot_network_mbedtls.c
@@ -147,7 +147,6 @@
 #define FLAG_HAS_RECEIVE_CALLBACK    ( 0x00000002UL ) /**< @brief Connection has receive callback. */
 #define FLAG_CLOSED                  ( 0x00000004UL ) /**< @brief Connection is closed. */
 
-
 /*-----------------------------------------------------------*/
 
 /**
