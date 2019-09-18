139dbae75dad34a51be201f24659e13e7737cff1
Gordon Wang <36049150+gordonwang0@users.noreply.github.com>
2019-09-18 10:21:08 -0700

    Release timer dispatch blocks

--- platform/ports/macos/source/iot_clock_macos.c
+++ platform/ports/macos/source/iot_clock_macos.c
@@ -194,6 +194,11 @@ void IotClock_TimerDestroy( IotTimer_t * pTimer )
     if( pTimer->dispatchBlock != NULL )
     {
         dispatch_block_cancel( pTimer->dispatchBlock );
+
+        /* dispatch_release only needs to be called if ARC is not being used. */
+        #if( OS_OBJECT_HAVE_OBJC_SUPPORT == 0 )
+            dispatch_release( pTimer->dispatchBlock );
+        #endif
     }
 }
 
