23a0865681d7cc8bff032b9a09c2420e505c0ae9
Muneeb Ahmed <ahmemune@amazon.com>
2019-10-03 16:26:25 -0700

    Cache macOS Homebrew metadata in CI

--- scripts/setup/ci_setup_osx.sh
+++ scripts/setup/ci_setup_osx.sh
@@ -2,11 +2,8 @@
 
 # Travis CI uses this script to set up the test environment on macOS.
 
-# Homebrew updates take a while on CI.
-export HOMEBREW_NO_AUTO_UPDATE=1
-
 # Install and start Mosquitto for MQTT pull request builds.
-if [ "$TRAVIS_PULL_REQUEST" != "false" ] && [ "$RUN_TEST" = "mqtt" ]; then
+if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
     brew install mosquitto
     brew services start mosquitto
 fi
