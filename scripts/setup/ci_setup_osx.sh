e6e594038d22271206f881f0f48cad06990e1b0e
Muneeb Ahmed <ahmemune@amazon.com>
2019-10-03 13:55:44 -0700

    Remove brew updates in macOS CI setup

--- scripts/setup/ci_setup_osx.sh
+++ scripts/setup/ci_setup_osx.sh
@@ -2,13 +2,13 @@
 
 # Travis CI uses this script to set up the test environment on macOS.
 
-# Update repositories.
-brew update
+# Homebrew updates take a while on CI.
+export HOMEBREW_NO_AUTO_UPDATE=1
 
 # Install and start Mosquitto for MQTT pull request builds.
 if [ "$TRAVIS_PULL_REQUEST" != "false" ] && [ "$RUN_TEST" = "mqtt" ]; then
-    brew install mosquitto;
-    brew services start mosquitto;
+    brew install mosquitto
+    brew services start mosquitto
 fi
 
 # Set compiler options for macOS. Silence field initializer warnings.
