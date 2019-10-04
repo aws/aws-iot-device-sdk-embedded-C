d48b68c566c6ff2724cc12ffcdd66dbfd4a267ea
Muneeb Ahmed <ahmemune@amazon.com>
2019-10-04 13:41:38 -0700

    Fix CI env vars and add Windows to Travis

--- scripts/ci_test_mqtt.sh
+++ scripts/ci_test_mqtt.sh
@@ -11,6 +11,9 @@ CMAKE_FLAGS="-DIOT_DEMO_MQTT_TOPIC_PREFIX=\"\\\"$IOT_IDENTIFIER\\\"\" "
 if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
     CMAKE_FLAGS+="$AWS_IOT_CREDENTIAL_DEFINES $COMPILER_OPTIONS"
     DEMO_OPTIONS="-i $IOT_IDENTIFIER"
+elif [ "$TRAVIS_OS_NAME" = "windows" ]; then
+    CMAKE_FLAGS+=$COMPILER_OPTIONS
+    DEMO_OPTIONS="-n -i $IOT_IDENTIFIER"
 else
     CMAKE_FLAGS+="-DIOT_TEST_MQTT_MOSQUITTO=1 -DIOT_TEST_SERVER=\\\"localhost\\\" $COMPILER_OPTIONS"
     DEMO_OPTIONS="-n -i $IOT_IDENTIFIER -u -h localhost -p 1883"
@@ -18,7 +21,13 @@ fi
 
 # Build executables.
 cmake .. -DIOT_BUILD_TESTS=1 -DCMAKE_BUILD_TYPE=Debug -DIOT_NETWORK_USE_OPENSSL=$IOT_NETWORK_USE_OPENSSL -DCMAKE_C_FLAGS="$CMAKE_FLAGS"
-make -j2 iot_tests_mqtt iot_demo_mqtt
+if [ "$TRAVIS_OS_NAME" != "windows" ]; then
+    make -j2 iot_tests_mqtt iot_demo_mqtt
+else
+    MSBuild.exe tests/mqtt/iot_tests_mqtt.vcxproj -m -clp:summary -verbosity:minimal
+    MSBuild.exe demos/app/iot_demo_mqtt.vcxproj -m -clp:summary -verbosity:minimal
+    mv ./bin/Debug/* ./bin/
+fi
 
 # Run tests and demos.
 ./bin/iot_tests_mqtt
@@ -26,7 +35,12 @@ make -j2 iot_tests_mqtt iot_demo_mqtt
 
 # Rebuild in static memory mode.
 cmake .. -DIOT_BUILD_TESTS=1 -DCMAKE_BUILD_TYPE=Debug -DIOT_NETWORK_USE_OPENSSL=$IOT_NETWORK_USE_OPENSSL -DCMAKE_C_FLAGS="$CMAKE_FLAGS -DIOT_STATIC_MEMORY_ONLY=1"
-make -j2 iot_tests_mqtt
+if [ "$TRAVIS_OS_NAME" != "windows" ]; then
+    make -j2 iot_tests_mqtt
+else
+    MSBuild.exe tests/mqtt/iot_tests_mqtt.vcxproj -m -clp:summary -verbosity:minimal
+    mv ./bin/Debug/* ./bin/
+fi
 
 # Run tests in static memory mode.
 ./bin/iot_tests_mqtt
