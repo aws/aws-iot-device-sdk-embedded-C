5fa3eec4b6f03707801a326ac42865caaab10fa4
Gordon Wang <36049150+gordonwang0@users.noreply.github.com>
2019-10-07 10:44:37 -0700

    Fix include in MQTT static memory

--- libraries/standard/mqtt/src/iot_mqtt_static_memory.c
+++ libraries/standard/mqtt/src/iot_mqtt_static_memory.c
@@ -36,7 +36,7 @@
 #include <string.h>
 
 /* Static memory include. */
-#include "private/iot_static_memory.h"
+#include "iot_static_memory.h"
 
 /* MQTT internal include. */
 #include "private/iot_mqtt_internal.h"
