684199dc2f63910f751f56f3edbc43857e5f4e3e
Gordon Wang <36049150+gordonwang0@users.noreply.github.com>
2019-10-07 11:52:02 -0700

    Fix include in Shadow internal

--- libraries/aws/shadow/src/private/aws_iot_shadow_internal.h
+++ libraries/aws/shadow/src/private/aws_iot_shadow_internal.h
@@ -84,7 +84,7 @@
  * the usage of dynamic memory allocation.
  */
 #if IOT_STATIC_MEMORY_ONLY == 1
-    #include "private/iot_static_memory.h"
+    #include "iot_static_memory.h"
 
 /**
  * @brief Allocate a #_shadowOperation_t. This function should have the same
