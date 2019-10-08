b9f6fc71577c8efdd1bc7dd336bc879ee4a3fa5e
Gordon Wang <36049150+gordonwang0@users.noreply.github.com>
2019-10-08 09:40:05 -0700

    Add build check for PR

---
+++ scripts/ci_test_build.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+# Travis CI uses this script to test various build configurations.
+
+# Exit on any nonzero return code.
+set -e
+
+# Build demos. Enable all logging.
+cmake .. -DCMAKE_C_FLAGS="$COMPILER_OPTIONS -DIOT_LOG_LEVEL_GLOBAL=IOT_LOG_DEBUG"
+
+if [ "$TRAVIS_OS_NAME" = "windows" ]; then
+    MSBuild.exe ALL_BUILD.vcxproj -m -clp:summary -verbosity:minimal
+else
+    make -j2
+fi
+
+# Build tests. Enable all logging.
+cmake .. -DIOT_BUILD_TESTS=1 -DCMAKE_C_FLAGS="$COMPILER_OPTIONS -DIOT_LOG_LEVEL_GLOBAL=IOT_LOG_DEBUG"
+
+if [ "$TRAVIS_OS_NAME" = "windows" ]; then
+    MSBuild.exe ALL_BUILD.vcxproj -m -clp:summary -verbosity:minimal
+else
+    make -j2
+fi
