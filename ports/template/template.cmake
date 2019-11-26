e9a4cc647a520f6aa65f286c637b5fd4c52b8c6f
Gordon Wang <36049150+gordonwang0@users.noreply.github.com>
2019-11-26 10:06:01 -0800

    Update template CMake

--- ports/template/template.cmake
+++ ports/template/template.cmake
@@ -4,15 +4,10 @@
 # Warn that the template port only builds. It will not create usable libraries.
 message( WARNING "This is a template port that contains only stubs. Libraries built with this port will not work!")
 
-# Add the template network header.
-set( PLATFORM_COMMON_HEADERS ${PLATFORM_COMMON_HEADERS}
-     ${PORTS_DIRECTORY}/common/include/iot_network_template.h )
-
-# Template platform sources.
+# Template platform sources. A network implementation (not listed below) is also needed.
 set( PLATFORM_SOURCES
      ${PORTS_DIRECTORY}/${IOT_PLATFORM_NAME}/src/iot_clock_${IOT_PLATFORM_NAME}.c
-     ${PORTS_DIRECTORY}/${IOT_PLATFORM_NAME}/src/iot_threads_${IOT_PLATFORM_NAME}.c
-     ${PORTS_DIRECTORY}/${IOT_PLATFORM_NAME}/src/iot_network_${IOT_PLATFORM_NAME}.c )
+     ${PORTS_DIRECTORY}/${IOT_PLATFORM_NAME}/src/iot_threads_${IOT_PLATFORM_NAME}.c )
 
 # Set the types header for this port.
 set( PORT_TYPES_HEADER ${PORTS_DIRECTORY}/${IOT_PLATFORM_NAME}/include/iot_platform_types_${IOT_PLATFORM_NAME}.h )
