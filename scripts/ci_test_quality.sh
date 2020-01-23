d5773f87ae78081f70a806e463359b2217175830
Gordon Wang <36049150+gordonwang0@users.noreply.github.com>
2020-01-23 14:12:45 -0800

    Enable quality check, but allow it to fail (#739)

--- scripts/ci_test_quality.sh
+++ scripts/ci_test_quality.sh
@@ -8,4 +8,4 @@ set -e
 # Get the list of files to check. Only check library files and exclude tests.
 # Run complexity with a threshold of 8.
 find ../libraries/ \( -name '*.c' ! -name *tests*.c \) -type f | \
-xargs complexity --scores --threshold=8 --horrid-threshold=8
+xargs complexity --scores --threshold=8 #--horrid-threshold=8
