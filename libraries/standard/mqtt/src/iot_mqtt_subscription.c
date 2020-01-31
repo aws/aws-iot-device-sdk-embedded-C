6b1b1f89dcd361a268ba49ffcd8f109f1491e37c
Qiutong Song <qiutongs@amazon.com>
2020-01-31 10:56:32 -0800

    fix spell

--- libraries/standard/mqtt/src/iot_mqtt_subscription.c
+++ libraries/standard/mqtt/src/iot_mqtt_subscription.c
@@ -697,7 +697,7 @@ bool IotMqtt_IsSubscribed( IotMqttConnection_t mqttConnection,
 /*-----------------------------------------------------------*/
 
 /* Provide access to internal functions and variables if testing. */
-/* IOT_BUILD_TESTS is defined outside the codebase, e.g. passed in by build command. */
+/* IOT_BUILD_TESTS is defined outside the code base, e.g. passed in by build command. */
 /* coverity[misra_c_2012_rule_20_9_violation] */
 /* coverity[caretline] */
 #if IOT_BUILD_TESTS == 1
