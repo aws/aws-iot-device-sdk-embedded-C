7b984e9298b976a08ece85876692723bc34f2161
Gordon Wang <36049150+gordonwang0@users.noreply.github.com>
2020-02-03 11:07:07 -0800

    Fix test

--- libraries/standard/mqtt/test/unit/iot_tests_mqtt_api.c
+++ libraries/standard/mqtt/test/unit/iot_tests_mqtt_api.c
@@ -2324,7 +2324,7 @@ TEST( MQTT_Unit_API, GetIncomingMQTTPacketTypeAndLengthChecks )
 {
     IotMqttError_t status = IOT_MQTT_SUCCESS;
     IotMqttPacketInfo_t mqttPacket;
-    uint8_t buffer[ 10 ] = { 0 };
+    uint8_t buffer[ 10 ];
     uint8_t * bufPtr = buffer;
 
     /* Dummy network interface - pointer to pointer to a buffer. */
@@ -2339,15 +2339,18 @@ TEST( MQTT_Unit_API, GetIncomingMQTTPacketTypeAndLengthChecks )
     TEST_ASSERT_EQUAL_INT( 0x02, mqttPacket.remainingLength );
 
     /* Test with NULL network interface */
+    bufPtr = buffer;
     status = IotMqtt_GetIncomingMQTTPacketTypeAndLength( &mqttPacket, _getNextByte, NULL );
     TEST_ASSERT_EQUAL( IOT_MQTT_NETWORK_ERROR, status );
 
     /* Test with incorrect packet type. */
+    bufPtr = buffer;
     buffer[ 0 ] = 0x10; /* INVALID */
     status = IotMqtt_GetIncomingMQTTPacketTypeAndLength( &mqttPacket, _getNextByte, pNetworkInterface );
     TEST_ASSERT_EQUAL( IOT_MQTT_BAD_RESPONSE, status );
 
     /* Test with invalid remaining length. */
+    bufPtr = buffer;
     buffer[ 0 ] = 0x20; /* CONN ACK */
 
     /* To generate invalid remaining length response,
