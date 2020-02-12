f50ddaf67e2200006e772b40b04b8984bd68bf4c
Felipe R. Monteiro <felisous@amazon.com>
2020-02-12 00:59:55 +0000

    Adds a proof harness for the IotMqtt_Wait function
    Signed-off-by: Felipe R. Monteiro <felisous@amazon.com>

---
+++ cbmc/proofs/IotMqtt_Wait/IotMqtt_Wait_harness.c
@@ -0,0 +1,28 @@
+#include "iot_config.h"
+#include "private/iot_mqtt_internal.h"
+
+#include <stdlib.h>
+#include <string.h>
+
+#include "mqtt_state.h"
+
+void harness()
+{
+  IotMqttConnection_t mqttConnection = allocate_IotMqttConnection(NULL);
+  __CPROVER_assume(mqttConnection != NULL);
+  __CPROVER_assume(mqttConnection->pNetworkInterface != NULL);
+  __CPROVER_assume( IS_STUBBED_NETWORKIF_DESTROY( mqttConnection->pNetworkInterface ) );
+  ensure_IotMqttConnection_has_lists(mqttConnection);
+  __CPROVER_assume(valid_IotMqttConnection(mqttConnection));
+
+  IotMqttOperation_t publishOperation = allocate_IotMqttOperation(NULL, mqttConnection);
+  __CPROVER_assume(valid_IotMqttOperation(publishOperation));
+
+  __CPROVER_assume(publishOperation->link.pNext == NULL);
+  __CPROVER_assume(publishOperation->link.pPrevious == NULL);
+
+  uint32_t timeoutMs;
+
+  IotMqtt_Wait( nondet_bool() ? publishOperation : NULL, timeoutMs );
+}
+
