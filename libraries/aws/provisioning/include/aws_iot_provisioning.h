2f42482c8ee543959d574bc16728f30263ce9066
Archit Aggarwal <architag@amazon.com>
2020-03-17 17:08:16 +0000

    Limit comment line width to be screen readable

--- libraries/aws/provisioning/include/aws_iot_provisioning.h
+++ libraries/aws/provisioning/include/aws_iot_provisioning.h
@@ -81,39 +81,48 @@ AwsIotProvisioningError_t AwsIotProvisioning_Init( uint32_t mqttTimeout );
 /* @[declare_provisioning_init] */
 
 /**
- * @brief Requests a new public-private key pair and certificate for the device from AWS IoT Core and
- * invokes the provided user-callback with the response from the server.
+ * @brief Requests a new public-private key pair and certificate for the device
+ * from AWS IoT Core and invokes the provided user-callback with the response from
+ * the server.
  *
- * @note It is advised to use a shared MQTT connection to AWS IoT Core across all API functions.
+ * @note It is advised to use a shared MQTT connection to AWS IoT Core across all
+ * API functions.
  *
- * @warning This function is NOT thread-safe. Concurrent calls to the library API functions can result in undefined
- * behavior. Device provisioning with this library REQUIRES calling the API functions of this library sequentially.
+ * @warning This function is NOT thread-safe. Concurrent calls to the library API
+ * functions can result in undefined behavior. Device provisioning with this library
+ * REQUIRES calling the API functions of this library sequentially.
  *
- * @param[in] connection The MQTT connection handle to the user AWS IoT account, which will be used for
- * communicating with the server for creating new device credentials.
- * @param[in] flags The flags for configuring the behavior of the API. See the options available in the
- * aws_iot_provisioning_types.h file.
- * @param[in] timeoutMs The timeout (in milliseconds) for a response from the server. If there is a timeout, this
- * function returns #AWS_IOT_PROVISIONING_TIMEOUT.
- * @param[in] pResponseCallback The user-defined callback that will be invoked with the response from the server,
- * whether new credentials for the device in case of success, OR error response in case of server rejection of the
- * credentials generation request.
- * The callback should be defined appropriately for storing the credentials provided by the server on the device.
- * @warning Do not overwrite the existing Provisioning claim credentials with the new credentials provided by
- * the server. It is RECOMMENDED NOT to delete the certificate and private key used for the passed connection handle
- * until the device has been provisioned with a new certificate with the @ref provisioning_function_registerthing
+ * @param[in] connection The MQTT connection handle to the user AWS IoT account, which
+ * will be used for communicating with the server for creating new device credentials.
+ * @param[in] flags The flags for configuring the behavior of the API. See the options
+ * available in the aws_iot_provisioning_types.h file.
+ * @param[in] timeoutMs The timeout (in milliseconds) for a response from the server.
+ * If there is a timeout, this function returns #AWS_IOT_PROVISIONING_TIMEOUT.
+ * @param[in] pResponseCallback The user-defined callback that will be invoked with the
+ * response from the server, whether new credentials for the device in case of success,
+ * OR error response in case of server rejection of the credentials generation request.
+ * The callback should be defined appropriately for storing the credentials provided by
+ * the server on the device.
+ * @warning Do not overwrite the existing Provisioning claim credentials with the new
+ * credentials provided by the server. It is RECOMMENDED NOT to delete the certificate
+ * and private key used for the passed connection handle until the device has been
+ * provisioned with a new certificate with the @ref provisioning_function_registerthing
  * function.
  * @return This function will return #AWS_IOT_PROVISIONING_SUCCESS upon success; otherwise,
- *   #AWS_IOT_PROVISIONING_NOT_INITIALIZED, if the API is called without initializing the Provisioning library (i.e.
- *   with a prior call to @ref AwsIotProvisioning_Init function.)
+ *   #AWS_IOT_PROVISIONING_NOT_INITIALIZED, if the API is called without initializing
+ *   the Provisioning library (i.e. with a prior call to @ref AwsIotProvisioning_Init function.)
  *   #AWS_IOT_PROVISIONING_BAD_PARAMETER, if one or more input parameters are invalid.
- *   #AWS_IOT_PROVISIONING_NO_MEMORY, if there is insufficient memory for allocation in internal operations.
+ *   #AWS_IOT_PROVISIONING_NO_MEMORY, if there is insufficient memory for
+ *   allocation in internal operations.
  *   #AWS_IOT_PROVISIONING_MQTT_ERROR, for errors from the MQTT stack.
- *   #AWS_IOT_PROVISIONING_TIMEOUT, if there is a timeout in waiting for the server response for the request to
- *   generate new credentials for the device.
- *   #AWS_IOT_PROVISIONING_SERVER_REFUSED, if the server rejects the request for generating device credentials.
- *   #AWS_IOT_PROVISIONING_BAD_RESPONSE, if the response from the server cannot be successfully parsed or comprehended.
- *   #AWS_IOT_PROVISIONING_INTERNAL_FAILURE, if any there are operation failures internal to the library.
+ *   #AWS_IOT_PROVISIONING_TIMEOUT, if there is a timeout in waiting for the server
+ *   response for the request to generate new credentials for the device.
+ *   #AWS_IOT_PROVISIONING_SERVER_REFUSED, if the server rejects the request for
+ *   generating device credentials.
+ *   #AWS_IOT_PROVISIONING_BAD_RESPONSE, if the response from the server cannot be
+ *   successfully parsed or comprehended.
+ *   #AWS_IOT_PROVISIONING_INTERNAL_FAILURE, if any there are operation failures
+ *   internal to the library.
  */
 /* @[declare_provisioning_createkeysandcertificate] */
 AwsIotProvisioningError_t AwsIotProvisioning_CreateKeysAndCertificate( IotMqttConnection_t connection,
@@ -123,42 +132,56 @@ AwsIotProvisioningError_t AwsIotProvisioning_CreateKeysAndCertificate( IotMqttCo
 /* @[declare_provisioning_createkeysandcertificate] */
 
 /**
- * @brief Requests the AWS IoT Core service for a certificate by sending a Certificate-Signing Request, and
- * invokes the provided user-defined response handler with the response it receives from the server.
+ * @brief Requests the AWS IoT Core service for a certificate by sending a
+ * Certificate-Signing Request, and invokes the provided user-defined response
+ * handler with the response it receives from the server.
  *
- * @note It is advised to use a shared MQTT connection to AWS IoT Core across all API functions.
+ * @note It is advised to use a shared MQTT connection to AWS IoT Core across
+ * all API functions.
  *
- * @warning This function is NOT thread-safe. Concurrent calls to the library API functions can result in undefined
- * behavior. Device provisioning with this library REQUIRES calling the API functions of this library sequentially.
+ * @warning This function is NOT thread-safe. Concurrent calls to the library
+ * API functions can result in undefined behavior. Device provisioning with this
+ * library REQUIRES calling the API functions of this library sequentially.
  *
- * @param[in] connection The MQTT connection handle that will be used to communicate with AWS IoT Core for
- * the Certificate-Signing Request.
- * @param[in] operationQos The Quality of Service (QoS) level for the MQTT publish/subscribe communication with the server.
- * @param[in] pCertificateSigningRequest The PEM encoded string for the Certificate-Signing Request.
+ * @param[in] connection The MQTT connection handle that will be used to communicate
+ * with AWS IoT Core for the Certificate-Signing Request.
+ * @param[in] operationQos The Quality of Service (QoS) level for the MQTT
+ * publish/subscribe communication with the server.
+ * @param[in] pCertificateSigningRequest The PEM encoded string for the
+ * Certificate-Signing Request.
  * @param[in] csrLength The length of the Certificate-Signing Request string.
- * @param[in] timeoutMs The timeout (in milliseconds) for a response from the server. If there is a timeout, this
- * function returns #AWS_IOT_PROVISIONING_TIMEOUT.
- * @param[in] pResponseCallback The user-defined callback that will be invoked with the server's response to the CSR request.
- * The server can respond either with the new certificate information (in case of request acceptance) OR with error (in case of request
- * rejection).
+ * @param[in] timeoutMs The timeout (in milliseconds) for a response from the server.
+ * If there is a timeout, this function returns #AWS_IOT_PROVISIONING_TIMEOUT.
+ * @param[in] pResponseCallback The user-defined callback that will be invoked with
+ * the server's response to the CSR request. The server can respond either with the
+ * new certificate information (in case of request acceptance) OR with error (in case
+ * of request rejection).
  *
- * @note The callback is expected to store/retain the new certificate information from the server response, which will be required for
- * registering the device with @ref provisioning_function_registerthing function.
+ * @note The callback is expected to store/retain the new certificate information from
+ * the server response, which will be required for registering the device with
+ * @ref provisioning_function_registerthing function.
  *
- * @warning Do not overwrite the existing certificate (used for making the server connection) with the new certificate that is provided
- * by the server. It is RECOMMENDED to retain the Provisinoning claim credentials until the device has been provisioned with a new certificate
- * using the @ref provisioning_function_registerthing function.
- * @return This function will return #AWS_IOT_PROVISIONING_SUCCESS upon success; otherwise,
- *   #AWS_IOT_PROVISIONING_NOT_INITIALIZED, if the API is called without initializing the Provisioning library (i.e.
- *   with a prior call to @ref AwsIotProvisioning_Init function.)
+ * @warning Do not overwrite the claim credentials (used for making the
+ * server connection) with the new certificate that is provided by the server.
+ * It is RECOMMENDED to retain the Provisinoning claim private key and certificate
+ * until the device has been provisioned with a new certificate using the
+ * @ref provisioning_function_registerthing function.
+ * @return This function will return #AWS_IOT_PROVISIONING_SUCCESS upon success;
+ *   otherwise,
+ *   #AWS_IOT_PROVISIONING_NOT_INITIALIZED, if the API is called without initializing the
+ *   Provisioning library (i.e. with a prior call to @ref AwsIotProvisioning_Init function.)
  *   #AWS_IOT_PROVISIONING_BAD_PARAMETER, if one or more input parameters are invalid.
- *   #AWS_IOT_PROVISIONING_NO_MEMORY, if there is insufficient memory for allocation in internal operations.
+ *   #AWS_IOT_PROVISIONING_NO_MEMORY, if there is insufficient memory for allocation
+ *   in internal operations.
  *   #AWS_IOT_PROVISIONING_MQTT_ERROR, for errors from the MQTT stack.
- *   #AWS_IOT_PROVISIONING_TIMEOUT, if there is a timeout in waiting for the server response for the request to
- *   generate new credentials for the device.
- *   #AWS_IOT_PROVISIONING_SERVER_REFUSED, if the server rejects the request for generating device credentials.
- *   #AWS_IOT_PROVISIONING_BAD_RESPONSE, if the response from the server cannot be successfully parsed or comprehended.
- *   #AWS_IOT_PROVISIONING_INTERNAL_FAILURE, if any there are operation failures internal to the library.
+ *   #AWS_IOT_PROVISIONING_TIMEOUT, if there is a timeout in waiting for the server
+ *   response for the request to generate new credentials for the device.
+ *   #AWS_IOT_PROVISIONING_SERVER_REFUSED, if the server rejects the request for
+ *   generating device credentials.
+ *   #AWS_IOT_PROVISIONING_BAD_RESPONSE, if the response from the server cannot be
+ *   successfully parsed or comprehended.
+ *   #AWS_IOT_PROVISIONING_INTERNAL_FAILURE, if any there are operation failures internal
+ *   to the library.
  */
 /* @[declare_provisioning_createcertificatefromcsr] */
 AwsIotProvisioningError_t AwsIotProvisioning_CreateCertificateFromCsr( IotMqttConnection_t connection,
@@ -170,43 +193,54 @@ AwsIotProvisioningError_t AwsIotProvisioning_CreateCertificateFromCsr( IotMqttCo
 /* @[declare_provisioning_createcertificatefromcsr] */
 
 /**
- * @brief Requests the AWS IoT Core service to register the device, and invokes the user-defined callback with the
- * response it receives from the server.
+ * @brief Requests the AWS IoT Core service to register the device, and invokes
+ * the user-defined callback with the response it receives from the server.
  *
- * For provisioning the device, the service is expected to register the certificate, and optionally set up the Thing,
- * Attributes and other cloud settings based on the fleet provisioning template and device context information that are
- * passed to the API.
+ * For registering the device, the service is expected to provide the new certificate,
+ * and optionally set up the Thing, Attributes and other cloud settings based on
+ * the fleet provisioning template and device context information that are passed
+ * to the API.
  *
- * @note It is advised to use a shared MQTT connection to AWS IoT Core across all API functions.
+ * @note It is advised to use a shared MQTT connection to AWS IoT Core across all
+ * API functions.
  *
- * Also, the AWS IoT account being connected to for provisioning the device SHOULD have a fleet provisioning template
- * created, whose template name should be passed to this API for requesting device provisioning.
+ * Also, the AWS IoT account being connected to for registering the device SHOULD
+ * have a fleet provisioning template created, whose template name should be passed
+ * to this API for requesting device registration.
  *
- * @warning This function is NOT thread-safe. Concurrent calls to the library API functions can result in undefined
- * behavior. Device provisioning with this library REQUIRES calling the API functions of this library sequentially.
+ * @warning This function is NOT thread-safe. Concurrent calls to the library API
+ * functions can result in undefined behavior. Device provisioning with this library
+ * REQUIRES calling the API functions of this library sequentially.
  *
- * @param[in] connection The MQTT connection handle to the user AWS IoT account that will be used for
- * registering the device.
- * @param[in] pProvisioningDataInfo The data (including the certificate) that needs to be sent to the server for
- * provisioning the device.
- * @param[in] timeoutMs The timeout (in milliseconds) for a response from the server. If there is a timeout, this
- * function returns #AWS_IOT_PROVISIONING_TIMEOUT.
- * @param[in] pResponseCallback The user-defined functor that will be called with the response received from the server,
- * whether post-provisioning data in case of success OR error message in case of server rejection of provisioning
- * request.
- * @note In case of success response, the server may send device-specific configuration data, which will be provided as
- * a list of key-value pairs in the callback.
- * @return This function will return #AWS_IOT_PROVISIONING_SUCCESS upon success; otherwise,
- *   #AWS_IOT_PROVISIONING_NOT_INITIALIZED, if the API is called without initializing the Provisioning library (i.e.
- *   with a prior call to @ref AwsIotProvisioning_Init function.)
+ * @param[in] connection The MQTT connection handle to the user AWS IoT account that
+ * will be used for registering the device.
+ * @param[in] pProvisioningDataInfo The data (including the certificate) that needs
+ * to be sent to the server for registering the device.
+ * @param[in] timeoutMs The timeout (in milliseconds) for a response from the server.
+ * If there is a timeout, this function returns #AWS_IOT_PROVISIONING_TIMEOUT.
+ * @param[in] pResponseCallback The user-defined functor that will be called with the
+ * response received from the server, whether post-provisioning data in case of
+ * success OR error message in case of server rejection of registration request.
+ * @note In case of success response, the server may send device-specific
+ * configuration data, which will be provided as a list of key-value pairs in the
+ * callback.
+ * @return This function will return #AWS_IOT_PROVISIONING_SUCCESS upon success;
+ *   otherwise,
+ *   #AWS_IOT_PROVISIONING_NOT_INITIALIZED, if the API is called without initializing
+ *   the Provisioning library (i.e. with a prior call to @ref AwsIotProvisioning_Init
+ *   function.)
  *   #AWS_IOT_PROVISIONING_BAD_PARAMETER, if one or more input parameters are invalid.
- *   #AWS_IOT_PROVISIONING_NO_MEMORY, if there is insufficient memory for allocation in internal operations.
+ *   #AWS_IOT_PROVISIONING_NO_MEMORY, if there is insufficient memory for allocation
+ *   in internal operations.
  *   #AWS_IOT_PROVISIONING_MQTT_ERROR, for errors from the MQTT stack.
- *   #AWS_IOT_PROVISIONING_TIMEOUT, if there is a timeout in waiting for the server response for the request to
- *   provision device.
- *   #AWS_IOT_PROVISIONING_SERVER_REFUSED, if the server rejects the request for provisioning the device.
- *   #AWS_IOT_PROVISIONING_BAD_RESPONSE, if the response from the server cannot be successfully parsed or comprehended.
- *   #AWS_IOT_PROVISIONING_INTERNAL_FAILURE, if any there are operation failures internal to the library.
+ *   #AWS_IOT_PROVISIONING_TIMEOUT, if there is a timeout in waiting for the server
+ *   response for the request to register the device.
+ *   #AWS_IOT_PROVISIONING_SERVER_REFUSED, if the server rejects the request for
+ *   register the device.
+ *   #AWS_IOT_PROVISIONING_BAD_RESPONSE, if the response from the server cannot be
+ *   successfully parsed or comprehended.
+ *   #AWS_IOT_PROVISIONING_INTERNAL_FAILURE, if any there are operation failures
+ *   internal to the library.
  */
 /* @[declare_provisioning_registerthing] */
 
