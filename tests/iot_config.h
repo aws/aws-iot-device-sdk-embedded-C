b4138ad7c43191ecf5795ea3552ca80c0f64bc7a
Archit Aggarwal <architag@amazon.com>
2020-03-26 21:44:09 +0000

    Add default definition of AWS_IOT_TEST_PROVISIONING_CSR_PEM macro

--- tests/iot_config.h
+++ tests/iot_config.h
@@ -86,9 +86,12 @@
 #endif
 
 /* Provisioning tests configuration */
+#ifndef AWS_IOT_TEST_PROVISIONING_CSR_PEM
+    #define AWS_IOT_TEST_PROVISIONING_CSR_PEM          ""
+#endif /* ifndef AWS_IOT_TEST_PROVISIONING_CSR_PEM */
 #ifndef AWS_IOT_TEST_PROVISIONING_TEMPLATE_NAME
     #define AWS_IOT_TEST_PROVISIONING_TEMPLATE_NAME    ""
-#endif
+#endif /* ifndef AWS_IOT_TEST_PROVISIONING_TEMPLATE_NAME */
 #ifndef AWS_IOT_TEST_PROVISIONING_TEMPLATE_PARAMETERS
     #define AWS_IOT_TEST_PROVISIONING_TEMPLATE_PARAMETERS       \
     {                                                           \
@@ -166,7 +169,7 @@
 
 /* Enable asserts in the libraries. */
 #define IOT_CONTAINERS_ENABLE_ASSERTS           ( 1 )
-#define IOT_MQTT_ENABLE_ASSERTS                 ( ! IOT_TEST_COVERAGE )
+#define IOT_MQTT_ENABLE_ASSERTS                 ( !IOT_TEST_COVERAGE )
 #define IOT_TASKPOOL_ENABLE_ASSERTS             ( 1 )
 #define IOT_SERIALIZER_ENABLE_ASSERTS           ( 1 )
 #define AWS_IOT_SHADOW_ENABLE_ASSERTS           ( 1 )
@@ -264,10 +267,10 @@
         #error "Unsupported compiler. Only gcc and clang are supported for coverage."
     #endif
 
-    /* Define a custom logging puts function. This function allows coverage
-     * testing of logging functions, but prevents excessive logs from being
-     * printed. */
-    #define IotLogging_Puts       _coveragePuts
+/* Define a custom logging puts function. This function allows coverage
+ * testing of logging functions, but prevents excessive logs from being
+ * printed. */
+    #define IotLogging_Puts    _coveragePuts
 
 /* Includes for coverage logging puts. */
     #include <stdbool.h>
