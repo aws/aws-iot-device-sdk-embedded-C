c50f52afe8a9df89be7be1b1e3c216ca09e6ea96
Gordon Wang <36049150+gordonwang0@users.noreply.github.com>
2020-04-05 13:11:26 -0700

    Refresh documentation for MQTT v3

--- include/aws_iot_mqtt_client_interface.h
+++ include/aws_iot_mqtt_client_interface.h
@@ -58,154 +58,242 @@ extern "C" {
 #include "network_interface.h"
 #include "timer_interface.h"
 
-
 /**
-* @brief Clean mqtt client from all dynamic memory allocate
-*
-* This function will free up memory that was dynamically allocated for the client.
-*
-* @param pClient MQTT Client that was previously created by calling aws_iot_mqtt_init
-* @return An IoT Error Type defining successful/failed freeing
-*/
- IoT_Error_t aws_iot_mqtt_free( AWS_IoT_Client *pClient );
-
-/**
- * @brief MQTT Client Initialization Function
+ * @functionspage{mqtt,MQTT library}
  *
- * Called to initialize the MQTT Client
- *
- * @param pClient Reference to the IoT Client
- * @param pInitParams Pointer to MQTT connection parameters
- *
- * @return IoT_Error_t Type defining successful/failed API call
+ * API functions
+ * - @functionname{mqtt_function_init}
+ * - @functionname{mqtt_function_free}
+ * - @functionname{mqtt_function_connect}
+ * - @functionname{mqtt_function_publish}
+ * - @functionname{mqtt_function_subscribe}
+ * - @functionname{mqtt_function_resubscribe}
+ * - @functionname{mqtt_function_unsubscribe}
+ * - @functionname{mqtt_function_disconnect}
+ * - @functionname{mqtt_function_yield}
+ * - @functionname{mqtt_function_attempt_reconnect}
+ * - @functionname{mqtt_function_get_next_packet_id}
+ * - @functionname{mqtt_function_set_connect_params}
+ * - @functionname{mqtt_function_is_client_connected}
+ * - @functionname{mqtt_function_get_client_state}
+ * - @functionname{mqtt_function_is_autoreconnect_enabled}
+ * - @functionname{mqtt_function_set_disconnect_handler}
+ * - @functionname{mqtt_function_autoreconnect_set_status}
+ * - @functionname{mqtt_function_get_network_disconnected_count}
+ * - @functionname{mqtt_function_reset_network_disconnected_count}
  */
+
+/**
+ * @functionpage{aws_iot_mqtt_init,mqtt,init}
+ * @functionpage{aws_iot_mqtt_free,mqtt,free}
+ * @functionpage{aws_iot_mqtt_connect,mqtt,connect}
+ * @functionpage{aws_iot_mqtt_publish,mqtt,publish}
+ * @functionpage{aws_iot_mqtt_subscribe,mqtt,subscribe}
+ * @functionpage{aws_iot_mqtt_resubscribe,mqtt,resubscribe}
+ * @functionpage{aws_iot_mqtt_unsubscribe,mqtt,unsubscribe}
+ * @functionpage{aws_iot_mqtt_disconnect,mqtt,disconnect}
+ * @functionpage{aws_iot_mqtt_yield,mqtt,yield}
+ * @functionpage{aws_iot_mqtt_attempt_reconnect,mqtt,attempt_reconnect}
+ */
+
+/**
+ * @brief Initialize a new MQTT client context.
+ *
+ * This function should be called before any other MQTT function to initialize
+ * a new MQTT client context. Once the client context is no longer needed,
+ * @ref mqtt_function_free should be called.
+ *
+ * @param[in] pClient MQTT client context to initialize
+ * @param[in] pInitParams Pointer to MQTT connection parameters
+ *
+ * @return `IoT_Error_t`: See `aws_iot_error.h`
+ */
+/* @[declare_mqtt_init] */
 IoT_Error_t aws_iot_mqtt_init(AWS_IoT_Client *pClient, IoT_Client_Init_Params *pInitParams);
+/* @[declare_mqtt_init] */
 
 /**
- * @brief MQTT Connection Function
+ * @brief Clean up an MQTT client context that is no longer needed.
  *
- * Called to establish an MQTT connection with the AWS IoT Service
+ * This function will free up resources used by an MQTT client context. It should
+ * only be called when that context is no longer needed.
  *
- * @param pClient Reference to the IoT Client
- * @param pConnectParams Pointer to MQTT connection parameters
+ * @param[in] pClient MQTT client context that was previously initialized by
+ * @ref mqtt_function_init
  *
- * @return An IoT Error Type defining successful/failed connection
+ * @return `IoT_Error_t`: See `aws_iot_error.h`
  */
+/* @[declare_mqtt_free] */
+IoT_Error_t aws_iot_mqtt_free( AWS_IoT_Client *pClient );
+/* @[declare_mqtt_free] */
+
+/**
+ * @brief Establish a connection with an MQTT server.
+ *
+ * This function should be called once and after @ref mqtt_function_init. It sends
+ * the MQTT CONNECT packet to the server, which establishes an MQTT session. Once
+ * the session is no longer needed, it can be closed with @ref mqtt_function_disconnect.
+ *
+ * @param[in] pClient MQTT client context
+ * @param[in] pConnectParams MQTT connection parameters
+ *
+ * @return `IoT_Error_t`: See `aws_iot_error.h`
+ */
+/* @[declare_mqtt_connect] */
 IoT_Error_t aws_iot_mqtt_connect(AWS_IoT_Client *pClient, IoT_Client_Connect_Params *pConnectParams);
+/* @[declare_mqtt_connect] */
 
 /**
- * @brief Publish an MQTT message on a topic
+ * @brief Publish an MQTT message to a topic.
  *
- * Called to publish an MQTT message on a topic.
- * @note Call is blocking.  In the case of a QoS 0 message the function returns
- * after the message was successfully passed to the TLS layer.  In the case of QoS 1
- * the function returns after the receipt of the PUBACK control packet.
+ * This function sends an MQTT message to the server. The server will then
+ * forward this message to any clients with subscriptions to topic filters
+ * that match the message's topic.
  *
- * @param pClient Reference to the IoT Client
- * @param pTopicName Topic Name to publish to
+ * For a QoS 0 message, this function returns after the message is successfully
+ * passed to the TLS layer. For a QoS 1 message, this function returns after the
+ * receipt of the PUBACK for the transmitted message.
+ *
+ * @param pClient MQTT client context
+ * @param pTopicName Topic name to publish to
  * @param topicNameLen Length of the topic name
- * @param pParams Pointer to Publish Message parameters
+ * @param pParams Publish message parameters
  *
- * @return An IoT Error Type defining successful/failed publish
+ * @return `IoT_Error_t`: See `aws_iot_error.h`
  */
+/* @[declare_mqtt_publish] */
 IoT_Error_t aws_iot_mqtt_publish(AWS_IoT_Client *pClient, const char *pTopicName, uint16_t topicNameLen,
 								 IoT_Publish_Message_Params *pParams);
+/* @[declare_mqtt_publish] */
 
 /**
  * @brief Subscribe to an MQTT topic.
  *
- * Called to send a subscribe message to the broker requesting a subscription
- * to an MQTT topic.
- * @note Call is blocking.  The call returns after the receipt of the SUBACK control packet.
- * @warning pTopicName and pApplicationHandlerData need to be static in memory.
+ * This function sends an MQTT subscribe packet to the server. It registers
+ * a subscription that will cause the provided callback function to be invoked
+ * when the server sends a matching message to the client.
  *
- * @param pClient Reference to the IoT Client
- * @param pTopicName Topic Name to publish to. pTopicName needs to be static in memory since
- * no malloc is performed by the SDK
- * @param topicNameLen Length of the topic name
- * @param qos Quality of service for subscription
- * @param pApplicationHandler Reference to the handler function for this subscription
- * @param pApplicationHandlerData Point to data passed to the callback. pApplicationHandlerData
- * also needs to be static in memory since no malloc is performed by the SDK
+ * @note Incoming messages are handled by @ref mqtt_function_yield. Therefore,
+ * @ref mqtt_function_yield must always be called regularly if any subscriptions
+ * are active.
  *
- * @return An IoT Error Type defining successful/failed subscription
+ * @param[in] pClient MQTT client context
+ * @param[in] pTopicName Topic for subscription
+ * @param[in] topicNameLen Length of topic
+ * @param[in] qos Quality of service for subscription
+ * @param[in] pApplicationHandler Callback function for incoming messages that arrive
+ * on this subscription.
+ * @param[in] pApplicationHandlerData Data passed to the callback.
+ *
+ * @return `IoT_Error_t`: See `aws_iot_error.h`
+ *
+ * @attention The `pTopicName` parameter is not copied. It must remain valid for the duration
+ * of the subscription (until @ref mqtt_function_unsubscribe) is called.
  */
+/* @[declare_mqtt_subscribe] */
 IoT_Error_t aws_iot_mqtt_subscribe(AWS_IoT_Client *pClient, const char *pTopicName, uint16_t topicNameLen,
 								   QoS qos, pApplicationHandler_t pApplicationHandler, void *pApplicationHandlerData);
+/* @[declare_mqtt_subscribe] */
 
 /**
- * @brief Subscribe to an MQTT topic.
+ * @brief Resubscribe to topic filter subscriptions in a previous MQTT session.
  *
- * Called to resubscribe to the topics that the client has active subscriptions on.
- * Internally called when autoreconnect is enabled
+ * This function restores subscriptions that were previously present in an
+ * MQTT session. Its primary use is to restore subscriptions after a session
+ * is manually disconnected and reopened.
  *
- * @note Call is blocking.  The call returns after the receipt of the SUBACK control packet.
+ * @note This function does not need to be called after @ref mqtt_function_attempt_reconnect
+ * or if auto-reconnect is enabled.
  *
- * @param pClient Reference to the IoT Client
+ * @param[in] pClient MQTT client context
  *
- * @return An IoT Error Type defining successful/failed subscription
+ * @return `IoT_Error_t`: See `aws_iot_error.h`
  */
+/* @[declare_mqtt_resubscribe] */
 IoT_Error_t aws_iot_mqtt_resubscribe(AWS_IoT_Client *pClient);
+/* @[declare_mqtt_resubscribe] */
 
 /**
- * @brief Unsubscribe to an MQTT topic.
+ * @brief Unsubscribe from an MQTT topic filter.
  *
- * Called to send an unsubscribe message to the broker requesting removal of a subscription
- * to an MQTT topic.
- * @note Call is blocking.  The call returns after the receipt of the UNSUBACK control packet.
+ * This function removes an MQTT subscription previously set by @ref mqtt_function_subscribe.
+ * It sends an MQTT UNSUBSCRIBE packet to the server and removes the topic's message
+ * handler stored by the client.
  *
- * @param pClient Reference to the IoT Client
- * @param pTopicFilter Topic filter of subscription
- * @param topicFilterLen Length of the topic filter
+ * @param[in] pClient MQTT client context
+ * @param[in] pTopicFilter Topic filter of the subscription to remove
+ * @param[in] topicFilterLen Length of topic filter to remove
  *
- * @return An IoT Error Type defining successful/failed unsubscribe call
+ * @return `IoT_Error_t`: See `aws_iot_error.h`
  */
+/* @[declare_mqtt_unsubscribe] */
 IoT_Error_t aws_iot_mqtt_unsubscribe(AWS_IoT_Client *pClient, const char *pTopicFilter, uint16_t topicFilterLen);
+/* @[declare_mqtt_unsubscribe] */
 
 /**
- * @brief Disconnect an MQTT Connection
+ * @brief Disconnect an MQTT session.
  *
- * Called to send a disconnect message to the broker.
+ * This function sends the MQTT DISCONNECT packet, which closes the MQTT session
+ * between the client and server. After this function returns, the MQTT client
+ * context should be either freed with @ref mqtt_function_free or reopened with
+ * @ref mqtt_function_connect.
  *
- * @param pClient Reference to the IoT Client
+ * @param[in] pClient MQTT client context
  *
- * @return An IoT Error Type defining successful/failed send of the disconnect control packet.
+ * @return `IoT_Error_t`: See `aws_iot_error.h`
  */
+/* @[declare_mqtt_disconnect] */
 IoT_Error_t aws_iot_mqtt_disconnect(AWS_IoT_Client *pClient);
+/* @[declare_mqtt_disconnect] */
 
 /**
- * @brief Yield to the MQTT client
+ * @brief Provide time for the MQTT client to process events.
  *
- * Called to yield the current thread to the underlying MQTT client.  This time is used by
- * the MQTT client to manage PING requests to monitor the health of the TCP connection as
- * well as periodically check the socket receive buffer for subscribe messages.  Yield()
- * must be called at a rate faster than the keepalive interval.  It must also be called
- * at a rate faster than the incoming message rate as this is the only way the client receives
- * processing time to manage incoming messages.
+ * This function processes the following events:
+ * - Incoming messages from the server <br>
+ * Topic filter subscriptions registered with @ref mqtt_function_subscribe instruct
+ * the server to send matching messages to this client. These messages are received
+ * by this function, which then calls the appropriate message handler. This function
+ * must be called at a rate faster than the incoming messages, as it is the only way
+ * the client receives processing time to manage incoming messages.
+ * - MQTT keep-alive (sending ping requests and processing ping responses) <br>
+ * The MQTT keep-alive mechanism involves sending pings to the server if the connection
+ * is idle. Therefore, in the absence of any other messages, <b>this function must be called
+ * at least once every keep-alive period to send the ping request</b>.
+ * - @ref mqtt_autoreconnect (if enabled) <br>
+ * If the client detects a disconnect, the reconnection will be performed in this function.
  *
- * @param pClient Reference to the IoT Client
- * @param timeout_ms Maximum number of milliseconds to pass thread execution to the client.
+ * @param[in] pClient MQTT client context
+ * @param[in] timeout_ms Amount of time to yield. This function will return to the caller
+ * after AT LEAST this amount of thime has passed.
  *
- * @return An IoT Error Type defining successful/failed client processing.
- *         If this call results in an error it is likely the MQTT connection has dropped.
- *         iot_is_mqtt_connected can be called to confirm.
+ * @return `IoT_Error_t`: See `aws_iot_error.h`
+ * @return If this call results a negative value, it is likely the MQTT connection has dropped.
+ * @ref mqtt_function_is_client_connected can be called to confirm.
  */
+/* @[declare_mqtt_yield] */
 IoT_Error_t aws_iot_mqtt_yield(AWS_IoT_Client *pClient, uint32_t timeout_ms);
+/* @[declare_mqtt_yield] */
 
 /**
- * @brief MQTT Manual Re-Connection Function
+ * @brief Attempt to reconnect with the MQTT server.
  *
- * Called to establish an MQTT connection with the AWS IoT Service
- * using parameters from the last time a connection was attempted
- * Use after disconnect to start the reconnect process manually
- * Makes only one reconnect attempt Sets the client state to
- * pending reconnect in case of failure
+ * This function makes a single reconnect attempt with the server. If the
+ * reconnection is successful, subscriptions from the client's previous
+ * session are restored as well.
  *
- * @param pClient Reference to the IoT Client
+ * If this function fails, the client's state is set to `CLIENT_STATE_PENDING_RECONNECT`.
  *
- * @return An IoT Error Type defining successful/failed connection
+ * @param[in] pClient MQTT client context
+ *
+ * @return `IoT_Error_t`: See `aws_iot_error.h`
+ *
+ * @note Generally, it is not necessary to call this function if @ref mqtt_autoreconnect
+ * is enabled. Nevertheless, this function may still be called to initiate a reconnect.
  */
+/* @[declare_mqtt_attempt_reconnect] */
 IoT_Error_t aws_iot_mqtt_attempt_reconnect(AWS_IoT_Client *pClient);
+/* @[declare_mqtt_attempt_reconnect] */
 
 #ifdef __cplusplus
 }
