a0a47ed2516f1fdc52fab396e8f15c18e619f758
Lee George Thomas <ltham@amazon.com>
2020-04-09 07:59:42 -0700

    Clean up CMake files

--- libraries/standard/mqtt/filePaths.cmake
+++ libraries/standard/mqtt/filePaths.cmake
@@ -1,5 +1,9 @@
-# This file is to populate source files and include directories
-# into a variables so that it can be reused.
+# This file is to add source files and include directories
+# into variables so that it can be reused from different repositories
+# in their Cmake based build system by including this file.
+#
+# Files specific to the repository such as test runner, platform tests
+# are not added to the variables.
 
 # MQTT library source files.
 set( MQTT_SOURCES
@@ -13,11 +17,11 @@ set( MQTT_SOURCES
      src/iot_mqtt_subscription.c
      src/iot_mqtt_validate.c )
 
-# MQTT library Include directories. 
+# MQTT library Include directories.
 set( MQTT_INCLUDE_PUBLIC_DIRS
      include )
 
-# MQTT system test sources.
+# MQTT system test source files.
 set( MQTT_SYSTEM_TEST_SOURCES
      test/system/iot_tests_mqtt_system.c )
 
@@ -31,3 +35,13 @@ set( MQTT_UNIT_TEST_SOURCES
 # MQTT test include directories.
 set( MQTT_TEST_INCLUDE_PRIVATE_DIRS
      src )
+
+# MQTT mock source files.
+set( MQTT_TEST_MOCK_SOURCES
+     test/mock/iot_tests_mqtt_mock.c )
+
+# MQTT mock include directories.
+set( MQTT_TEST_MOCK_INCLUDE_PRIVATE_DIRS
+     src )
+set( MQTT_TEST_MOCK_INCLUDE_PUBLIC_DIRS
+     test/mock )
