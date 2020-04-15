4379d0b9bd515b9c4a5f2e810a01b2350efc66ea
Gordon Wang <36049150+gordonwang0@users.noreply.github.com>
2020-04-15 15:50:47 -0700

    Add MQTT connect API implementation (#870)

--- libraries/standard/mqtt/include/mqtt.h
+++ libraries/standard/mqtt/include/mqtt.h
@@ -1,3 +1,24 @@
+/*
+ * Copyright (C) 2020 Amazon.com, Inc. or its affiliates.  All Rights Reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 #include "config.h"
 #include "mqtt_lightweight.h"
 
@@ -13,11 +34,11 @@ typedef struct MQTTContext MQTTContext_t;
 struct MQTTTransportInterface;
 typedef struct MQTTTransportInterface MQTTTransportInterface_t;
 
-typedef int32_t (* TransportWriteFunc_t )( MQTTNetworkContext_t context,
-                                           void * pBuffer,
-                                           size_t bytesToWrite );
+typedef int32_t (* MQTTTransportSendFunc_t )( MQTTNetworkContext_t context,
+                                              const void * pMessage,
+                                              size_t bytesToSend );
 
-typedef uint32_t (* GetCurrentTimeFunc_t )( void );
+typedef uint32_t (* MQTTGetCurrentTimeFunc_t )( void );
 
 typedef void (* MQTTEventCallback_t )( MQTTContext_t * pContext,
                                        MQTTPacketInfo_t * pPacketInfo );
@@ -25,7 +46,6 @@ typedef void (* MQTTEventCallback_t )( MQTTContext_t * pContext,
 typedef enum MQTTConnectionStatus
 {
     MQTTNotConnected,
-    MQTTConnectionInProgress,
     MQTTConnected
 } MQTTConnectionStatus_t;
 
@@ -53,14 +73,14 @@ typedef enum MQTTPubAckType
 
 struct MQTTTransportInterface
 {
-    TransportWriteFunc_t write;
-    TransportReadFunc_t read;
-    MQTTNetworkContext_t * networkContext;
+    MQTTTransportSendFunc_t send;
+    MQTTTransportRecvFunc_t recv;
+    MQTTNetworkContext_t networkContext;
 };
 
 struct MQTTApplicationCallbacks
 {
-    GetCurrentTimeFunc_t getTime;
+    MQTTGetCurrentTimeFunc_t getTime;
     MQTTEventCallback_t appCallback;
 };
 
@@ -78,25 +98,23 @@ struct MQTTContext
     MQTTPubAckInfo_t incomingPublishRecords[ MQTT_MAX_QUEUED_PUBLISH_MESSAGES ];
     size_t incomingPublishCount;
 
-    MQTTTransportInterface_t transportInterface;
+    const MQTTTransportInterface_t * pTransportInterface;
+    const MQTTFixedBuffer_t * pNetworkBuffer;
+
     uint16_t nextPacketId;
-
-    MQTTFixedBuffer_t txBuffer;
-    MQTTFixedBuffer_t rxBuffer;
-
     MQTTConnectionStatus_t connectStatus;
-    MQTTApplicationCallbacks_t callbacks;
-    bool controlPacketSent;
+    const MQTTApplicationCallbacks_t * pCallbacks;
+    uint32_t lastPacketTime;
 };
 
 void MQTT_Init( MQTTContext_t * const pContext,
                 const MQTTTransportInterface_t * const pTransportInterface,
                 const MQTTApplicationCallbacks_t * const pCallbacks,
-                const MQTTFixedBuffer_t * const pTxBuffer,
-                const MQTTFixedBuffer_t * const pRxBuffer );
+                const MQTTFixedBuffer_t * const pNetworkBuffer );
 
 MQTTStatus_t MQTT_Connect( MQTTContext_t * const pContext,
                            const MQTTConnectInfo_t * const pConnectInfo,
+                           const MQTTPublishInfo_t * const pWillInfo,
                            bool * const pSessionPresent );
 
 MQTTStatus_t MQTT_Subscribe( MQTTContext_t * const pContext,
