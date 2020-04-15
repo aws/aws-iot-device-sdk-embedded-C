c15fe112c95fbf4dd7375fc5255db677a9c39937
Gordon Wang <36049150+gordonwang0@users.noreply.github.com>
2020-04-15 10:05:54 -0700

    Address PR comments

--- libraries/standard/mqtt/include/mqtt.h
+++ libraries/standard/mqtt/include/mqtt.h
@@ -35,7 +35,7 @@ struct MQTTTransportInterface;
 typedef struct MQTTTransportInterface MQTTTransportInterface_t;
 
 typedef int32_t (* MQTTTransportSendFunc_t )( MQTTNetworkContext_t context,
-                                              void * pBuffer,
+                                              const void * pBuffer,
                                               size_t bytesToSend );
 
 typedef uint32_t (* MQTTGetCurrentTimeFunc_t )( void );
