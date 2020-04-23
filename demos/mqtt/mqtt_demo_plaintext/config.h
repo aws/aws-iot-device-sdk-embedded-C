19518d0b0b3cc53f2ce0e052e70b2ecfcc925126
Gordon Wang <36049150+gordonwang0@users.noreply.github.com>
2020-04-23 14:05:57 -0700

    Resolve merge conflicts

--- demos/mqtt/mqtt_demo_plaintext/config.h
+++ demos/mqtt/mqtt_demo_plaintext/config.h
@@ -22,25 +22,9 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-
 /* Set network context to socket (int). */
 typedef int MQTTNetworkContext_t;
 
 #define MQTT_MAX_QUEUED_PUBLISH_MESSAGES    10
-
-/* Include file for POSIX reference implementation. */
-#include "iot_logging.h"
-
-/* Define the IotLog logging interface to enabling logging.
- * This demo maps the macro to the reference POSIX implementation for logging.
- * Note: @ref LIBRARY_LOG_NAME adds the name of the library, that produces the
- * log, as metadata in each log message. */
-#define IotLog( messageLevel, pFormat, ... ) \
-    IotLog_Generic( messageLevel,            \
-                    "[%s:%d] [%s] "pFormat,  \
-                    __FILE__,                \
-                    __LINE__,                \
-                    LIBRARY_LOG_NAME,        \
-                    __VA_ARGS__ )
 
 #endif /* ifndef CONFIG_H */
