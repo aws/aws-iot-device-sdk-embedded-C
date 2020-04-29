a13ea65bc70a0aa66f361f40ee97023154b16801
Gordon Wang <36049150+gordonwang0@users.noreply.github.com>
2020-04-29 14:36:05 -0700

    Document MQTT lightweight and API functions (#902)

--- libraries/standard/mqtt/src/mqtt.c
+++ libraries/standard/mqtt/src/mqtt.c
@@ -23,6 +23,8 @@
 
 #include "mqtt.h"
 
+/*-----------------------------------------------------------*/
+
 static int32_t sendPacket( MQTTContext_t * pContext, size_t bytesToSend )
 {
     const uint8_t * pIndex = pContext->networkBuffer.pBuffer;
@@ -61,6 +63,8 @@ static int32_t sendPacket( MQTTContext_t * pContext, size_t bytesToSend )
     return totalBytesSent;
 }
 
+/*-----------------------------------------------------------*/
+
 void MQTT_Init( MQTTContext_t * const pContext,
                 const MQTTTransportInterface_t * const pTransportInterface,
                 const MQTTApplicationCallbacks_t * const pCallbacks,
@@ -76,6 +80,8 @@ void MQTT_Init( MQTTContext_t * const pContext,
     /* Zero is not a valid packet ID per MQTT spec. Start from 1. */
     pContext->nextPacketId = 1;
 }
+
+/*-----------------------------------------------------------*/
 
 MQTTStatus_t MQTT_Connect( MQTTContext_t * const pContext,
                            const MQTTConnectInfo_t * const pConnectInfo,
@@ -136,6 +142,8 @@ MQTTStatus_t MQTT_Connect( MQTTContext_t * const pContext,
     return status;
 }
 
+/*-----------------------------------------------------------*/
+
 MQTTStatus_t MQTT_Subscribe( MQTTContext_t * const pContext,
                              const MQTTSubscribeInfo_t * const pSubscriptionList,
                              size_t subscriptionCount )
@@ -143,16 +151,22 @@ MQTTStatus_t MQTT_Subscribe( MQTTContext_t * const pContext,
     return MQTTSuccess;
 }
 
+/*-----------------------------------------------------------*/
+
 MQTTStatus_t MQTT_Publish( MQTTContext_t * const pContext,
                            const MQTTPublishInfo_t * const pPublishInfo )
 {
     return MQTTSuccess;
 }
 
+/*-----------------------------------------------------------*/
+
 MQTTStatus_t MQTT_Ping( MQTTContext_t * const pContext )
 {
     return MQTTSuccess;
 }
+
+/*-----------------------------------------------------------*/
 
 MQTTStatus_t MQTT_Unsubscribe( MQTTContext_t * const pContext,
                                const MQTTSubscribeInfo_t * const pSubscriptionList,
@@ -160,6 +174,8 @@ MQTTStatus_t MQTT_Unsubscribe( MQTTContext_t * const pContext,
 {
     return MQTTSuccess;
 }
+
+/*-----------------------------------------------------------*/
 
 MQTTStatus_t MQTT_Disconnect( MQTTContext_t * const pContext )
 {
@@ -191,11 +207,15 @@ MQTTStatus_t MQTT_Disconnect( MQTTContext_t * const pContext )
     return status;
 }
 
+/*-----------------------------------------------------------*/
+
 MQTTStatus_t MQTT_Process( MQTTContext_t * const pContext,
                            uint32_t timeoutMs )
 {
     return MQTTSuccess;
 }
+
+/*-----------------------------------------------------------*/
 
 uint16_t MQTT_GetPacketId( MQTTContext_t * const pContext )
 {
@@ -210,3 +230,5 @@ uint16_t MQTT_GetPacketId( MQTTContext_t * const pContext )
 
     return packetId;
 }
+
+/*-----------------------------------------------------------*/
