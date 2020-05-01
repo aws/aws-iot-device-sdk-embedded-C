3e745b3a9391790cf4fbb3fa40c512fba1a6d628
Archit Aggarwal <architag@amazon.com>
2020-05-01 23:03:56 +0000

    Fix definition of assert() in unit tests

--- libraries/standard/http/test/common.h
+++ libraries/standard/http/test/common.h
@@ -7,6 +7,6 @@
 #include "private/http_client_internal.h"
 #include "private/http_client_parse.h"
 
-static int _assertCount;
-#define assertReset()    do { _assertCount = 0; } while( 0 )
-#define assert( x )      do { _assertCount++; } while( 0 )
+static int _assertFailureCount;
+#define assertReset()    do { _assertFailureCount = 0; } while( 0 )
+#define assert( x )      do { if( x == false ) { _assertFailureCount++; } } while( 0 )
