7ecf4c3d903b0264bab7344e6a75094730811ef8
Ubuntu <ubuntu@ip-172-31-18-0.ec2.internal>
2020-05-03 00:11:15 +0000

    Fix test build issue

--- libraries/standard/http/test/common.h
+++ libraries/standard/http/test/common.h
@@ -10,4 +10,4 @@
 
 static int _assertFailureCount;
 #define assertReset()    do { _assertFailureCount = 0; } while( 0 )
-#define assert( x )      do { if( x == false ) { _assertFailureCount++; } } while( 0 )
+#define assert( x )      do { if( !(x) ) { _assertFailureCount++; } } while( 0 )
