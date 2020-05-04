833272fed187bef479f4b891b299495a7ace001e
Oscar Abrina <abrinao@amazon.com>
2020-05-04 13:46:43 -0700

    More cmake changes

--- tools/cmake/filepaths.cmake
+++ tools/cmake/filepaths.cmake
@@ -1,4 +1,4 @@
 # Set some global path variables.
-get_filename_component(__root_dir "${CMAKE_CURRENT_LIST_DIR}/.." ABSOLUTE)
+get_filename_component(__root_dir "${CMAKE_CURRENT_LIST_DIR}/../.." ABSOLUTE)
 set(ROOT_DIR ${__root_dir} CACHE INTERNAL "C SDK source root.")
 set(DEMOS_DIR "${AFR_ROOT_DIR}/demos" CACHE INTERNAL "C SDK demos root.")
