cb97fdcb9df8abefd9d0438e8d2a9b7a7e7626b3
Oscar Abrina <abrinao@amazon.com>
2020-05-07 11:39:50 -0700

    Add missing defines to config.h

--- demos/mqtt/mqtt_demo_basic_tls/config.h
+++ demos/mqtt/mqtt_demo_basic_tls/config.h
@@ -30,6 +30,18 @@ typedef SSL * MQTTNetworkContext_t;
  * @brief The maximum number of MQTT PUBLISH messages that may be pending
  * acknowledgement at any time.
  *
+ * QoS 1 and 2 MQTT PUBLISHes require acknowledgement from the server before
+ * they can be completed. While they are awaiting the acknowledgement, the
+ * client must maintain information about their state. The value of this
+ * macro sets the limit on how many simultaneous PUBLISH states an MQTT
+ * context maintains.
+ */
+#define MQTT_STATE_ARRAY_MAX_COUNT          10U
+
+/**
+ * @brief The maximum number of MQTT PUBLISH messages that may be pending
+ * acknowledgement at any time.
+ *
  * QoS 1 and 2 MQTT PUBLISHes require acknowlegement from the server before
  * they can be completed. While they are awaiting the acknowledgement, the
  * client must maintain information about their state. The value of this
@@ -43,6 +55,6 @@ typedef SSL * MQTTNetworkContext_t;
  *
  * No two clients may use the same client identifier simultaneously.
  */
-#define CLIENT_IDENTIFIER           "testclient"
+#define CLIENT_IDENTIFIER                   "testclient"
 
 #endif /* ifndef CONFIG_H */
--- libraries/standard/mqtt/utest/config.h
+++ libraries/standard/mqtt/utest/config.h
@@ -41,6 +41,18 @@ typedef int MQTTNetworkContext_t;
 #define MQTT_MAX_QUEUED_PUBLISH_MESSAGES    10
 
 /**
+ * @brief The maximum number of MQTT PUBLISH messages that may be pending
+ * acknowledgement at any time.
+ *
+ * QoS 1 and 2 MQTT PUBLISHes require acknowledgement from the server before
+ * they can be completed. While they are awaiting the acknowledgement, the
+ * client must maintain information about their state. The value of this
+ * macro sets the limit on how many simultaneous PUBLISH states an MQTT
+ * context maintains.
+ */
+#define MQTT_STATE_ARRAY_MAX_COUNT          10U
+
+/**
  * @brief MQTT client identifier.
  *
  * No two clients may use the same client identifier simultaneously.
