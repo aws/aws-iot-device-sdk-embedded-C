a576aee4580a2ad2db70a07960294f372e61eabd
Archit Aggarwal <architag@amazon.com>
2020-05-16 00:18:30 +0000

    Remove default definition of WithArgs from libraries

--- libraries/standard/http/src/private/http_client_internal.h
+++ libraries/standard/http/src/private/http_client_internal.h
@@ -7,32 +7,16 @@
     #define LogError( message )
 #endif
 
-#ifndef LogErrorWithArgs
-    #define LogErrorWithArgs( formatAndStrings )
-#endif
-
 #ifndef LogWarn
     #define LogWarn( message )
-#endif
-
-#ifndef LogWarnWithArgs
-    #define LogWarnWithArgs( formatAndStrings )
 #endif
 
 #ifndef LogInfo
     #define LogInfo( message )
 #endif
 
-#ifndef LogInfoWithArgs
-    #define LogInfoWithArgs( formatAndStrings )
-#endif
-
 #ifndef LogDebug
     #define LogDebug( message )
-#endif
-
-#ifndef LogDebugWithArgs
-    #define LogDebugWithArgs( formatAndStrings )
 #endif
 
 /**
