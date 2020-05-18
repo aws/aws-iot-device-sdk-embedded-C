38b88fb684dab95c87d3fe5ddbfbed8643af37bc
Oscar Michael Abrina <abrinao@amazon.com>
2020-05-18 14:34:29 -0700

    Add MQTT_Init unit tests using Unity Framework (#941)
    * Coverage for MQTT_Init
    
    * Remove mock for mqtt_lightweight
    
    * Change to expected, actual order for TEST_ASSERT
    
    * Provide updated comments for unity fixtures
    
    * Add validation for context.nextPacketId
    
    * Add tests for null parameters
    
    * Add missing sc;
    
    * Remove warning for suiteTearDown
    
    * Do not return numFailures
    
    * Return correct error code
    
    * return numFailures
    
    * Add docs

--- libraries/standard/mqtt/utest/mqtt_utest.c
+++ libraries/standard/mqtt/utest/mqtt_utest.c
@@ -2,36 +2,81 @@
 
 #include "unity.h"
 
-#include "mock_mqtt_lightweight.h"
-
 /* Include paths for public enums, structures, and macros. */
 #include "mqtt.h"
 
+/**
+ * @brief A valid starting packet ID per MQTT spec. Start from 1.
+ */
+#define MQTT_NEXT_PACKET_ID_START    ( 1 )
+
 /* ============================   UNITY FIXTURES ============================ */
+
+/* Called before each test method. */
 void setUp( void )
 {
 }
 
-/* called before each testcase */
+/* Called after each test method. */
 void tearDown( void )
 {
 }
 
-/* called at the beginning of the whole suite */
+/* Called at the beginning of the whole suite. */
 void suiteSetUp()
 {
 }
 
-/* called at the end of the whole suite */
+/* Called at the end of the whole suite. */
 int suiteTearDown( int numFailures )
 {
+    return numFailures;
 }
 
-/* ============================   Testing MQTT_Connect ====================== */
-void test_Mqtt_connect_packet_size_gt_max( void )
-{
-    /* This mocked method will be called inside MQTT_Connect(...). */
-    MQTT_GetConnectPacketSize_ExpectAnyArgsAndReturn( MQTTBadParameter );
+/* ============================   Testing MQTT_Init ========================= */
 
-    TEST_ASSERT_EQUAL( MQTT_Connect( NULL, NULL, NULL, 0U, NULL ), MQTTBadParameter );
+/**
+ * @brief Test that MQTT_Init is able to update the context object correctly.
+ */
+void test_MQTT_Init_Happy_path( void )
+{
+    MQTTStatus_t mqttStatus;
+    MQTTContext_t context;
+    MQTTTransportInterface_t transport;
+    MQTTFixedBuffer_t networkBuffer;
+    MQTTApplicationCallbacks_t callbacks;
+
+    mqttStatus = MQTT_Init( &context, &transport, &callbacks, &networkBuffer );
+    TEST_ASSERT_EQUAL( MQTTSuccess, mqttStatus );
+    TEST_ASSERT_EQUAL( MQTTNotConnected, context.connectStatus );
+    TEST_ASSERT_EQUAL( MQTT_NEXT_PACKET_ID_START, context.nextPacketId );
+    /* These Unity assertions take pointers and compare their contents. */
+    TEST_ASSERT_EQUAL_MEMORY( &transport, &context.transportInterface, sizeof( transport ) );
+    TEST_ASSERT_EQUAL_MEMORY( &callbacks, &context.callbacks, sizeof( callbacks ) );
+    TEST_ASSERT_EQUAL_MEMORY( &networkBuffer, &context.networkBuffer, sizeof( networkBuffer ) );
+}
+
+/**
+ * @brief Test that any NULL parameter causes MQTT_Init to return MQTTBadParameter.
+ */
+void test_MQTT_Init_Invalid_params( void )
+{
+    MQTTStatus_t mqttStatus;
+    MQTTContext_t context;
+    MQTTTransportInterface_t transport;
+    MQTTFixedBuffer_t networkBuffer;
+    MQTTApplicationCallbacks_t callbacks;
+
+    /* Check that MQTTBadParameter is returned if any NULL parameters are passed. */
+    mqttStatus = MQTT_Init( NULL, &transport, &callbacks, &networkBuffer );
+    TEST_ASSERT_EQUAL( MQTTBadParameter, mqttStatus );
+
+    mqttStatus = MQTT_Init( &context, NULL, &callbacks, &networkBuffer );
+    TEST_ASSERT_EQUAL( MQTTBadParameter, mqttStatus );
+
+    mqttStatus = MQTT_Init( &context, &transport, NULL, &networkBuffer );
+    TEST_ASSERT_EQUAL( MQTTBadParameter, mqttStatus );
+
+    mqttStatus = MQTT_Init( &context, &transport, &callbacks, NULL );
+    TEST_ASSERT_EQUAL( MQTTBadParameter, mqttStatus );
 }
