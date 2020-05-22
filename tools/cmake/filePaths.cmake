297350512deaab9f7baa54b59840c72e09f6dfc4
Oscar Michael Abrina <abrinao@amazon.com>
2020-05-22 02:46:45 -0700

    Add MQTT_SerializeConnect unit tests using Unity (#942)
    * Add mqtt_lightweight_utest.c
    
    * Achieve 100% coverage on MQTT_SerializeConnect
    
    * Remove accidental changes
    
    * Move comment
    
    * Change TEST_ASSERT to correct order (expected, actual)
    
    * Add checks for serialized network packet
    
    * Update docs
    
    * Remove warning about suiteTearDown
    
    * Add docs for private methods
    
    * Address PR comments
    
    * Explicitly declare combinations of willInfo and connectInfo being used for the test
    
    * Rename to pDestination
    
    * Remove tools/cmake/filePaths.cmake
    
    * Merge mqtt/utest/CMakeLists.txt
    
    * Add assert for pSource

--- tools/cmake/filePaths.cmake
+++
@@ -1,6 +0,0 @@
-# Set some global path variables.
-get_filename_component(__root_dir "${CMAKE_CURRENT_LIST_DIR}/../.." ABSOLUTE)
-set(ROOT_DIR ${__root_dir} CACHE INTERNAL "C SDK source root.")
-set(DEMOS_DIR "${ROOT_DIR}/demos" CACHE INTERNAL "C SDK demos root.")
-set(MODULES_DIR "${ROOT_DIR}/libraries" CACHE INTERNAL "C SDK modules root.")
-set(3RDPARTY_DIR "${MODULES_DIR}/3rdparty" CACHE INTERNAL "3rdparty libraries root.")
