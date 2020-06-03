78b109b53b85662b49126aa8d8a9f82d045964be
Muneeb Ahmed <ahmemune@amazon.com>
2020-06-03 02:13:42 -0700

    Fix MQTT build warnings and add status strerror

--- libraries/standard/mqtt/src/mqtt.c
+++ libraries/standard/mqtt/src/mqtt.c
@@ -948,7 +948,9 @@ MQTTStatus_t MQTT_Connect( MQTTContext_t * const pContext,
     size_t remainingLength = 0UL, packetSize = 0UL;
     int32_t bytesSent;
     MQTTStatus_t status = MQTTSuccess;
-    MQTTPacketInfo_t incomingPacket = { .type = ( ( uint8_t ) 0 ) };
+    MQTTPacketInfo_t incomingPacket = { 0 };
+
+    incomingPacket.type = ( uint8_t ) 0;
 
     if( ( pContext == NULL ) || ( pConnectInfo == NULL ) || ( pSessionPresent == NULL ) )
     {
@@ -1477,6 +1479,66 @@ uint16_t MQTT_GetPacketId( MQTTContext_t * const pContext )
     }
 
     return packetId;
+}
+
+/*-----------------------------------------------------------*/
+
+const char * MQTT_Status_strerror( MQTTStatus_t status )
+{
+    const char * str = NULL;
+
+    switch( status )
+    {
+        case MQTTSuccess:
+            str = "MQTTSuccess";
+            break;
+
+        case MQTTBadParameter:
+            str = "MQTTBadParameter";
+            break;
+
+        case MQTTNoMemory:
+            str = "MQTTNoMemory";
+            break;
+
+        case MQTTSendFailed:
+            str = "MQTTSendFailed";
+            break;
+
+        case MQTTRecvFailed:
+            str = "MQTTRecvFailed";
+            break;
+
+        case MQTTBadResponse:
+            str = "MQTTBadResponse";
+            break;
+
+        case MQTTServerRefused:
+            str = "MQTTServerRefused";
+            break;
+
+        case MQTTNoDataAvailable:
+            str = "MQTTNoDataAvailable";
+            break;
+
+        case MQTTIllegalState:
+            str = "MQTTIllegalState";
+            break;
+
+        case MQTTStateCollision:
+            str = "MQTTStateCollision";
+            break;
+
+        case MQTTKeepAliveTimeout:
+            str = "MQTTKeepAliveTimeout";
+            break;
+
+        default:
+            str = "Invalid MQTT Status code";
+            break;
+    }
+
+    return str;
 }
 
 /*-----------------------------------------------------------*/
--- libraries/standard/mqtt/utest/mqtt_utest.c
+++ libraries/standard/mqtt/utest/mqtt_utest.c
@@ -1588,3 +1588,62 @@ void test_MQTT_Ping_error_path( void )
 }
 
 /* ========================================================================== */
+
+/**
+ * @brief Test MQTT_Status_strerror returns correct strings.
+ */
+void test_MQTT_Status_strerror( void )
+{
+    MQTTStatus_t status;
+    const char * str = NULL;
+
+    status = MQTTSuccess;
+    str = MQTT_Status_strerror( status );
+    TEST_ASSERT_EQUAL_STRING( "MQTTSuccess", str );
+
+    status = MQTTBadParameter;
+    str = MQTT_Status_strerror( status );
+    TEST_ASSERT_EQUAL_STRING( "MQTTBadParameter", str );
+
+    status = MQTTNoMemory;
+    str = MQTT_Status_strerror( status );
+    TEST_ASSERT_EQUAL_STRING( "MQTTNoMemory", str );
+
+    status = MQTTSendFailed;
+    str = MQTT_Status_strerror( status );
+    TEST_ASSERT_EQUAL_STRING( "MQTTSendFailed", str );
+
+    status = MQTTRecvFailed;
+    str = MQTT_Status_strerror( status );
+    TEST_ASSERT_EQUAL_STRING( "MQTTRecvFailed", str );
+
+    status = MQTTBadResponse;
+    str = MQTT_Status_strerror( status );
+    TEST_ASSERT_EQUAL_STRING( "MQTTBadResponse", str );
+
+    status = MQTTServerRefused;
+    str = MQTT_Status_strerror( status );
+    TEST_ASSERT_EQUAL_STRING( "MQTTServerRefused", str );
+
+    status = MQTTNoDataAvailable;
+    str = MQTT_Status_strerror( status );
+    TEST_ASSERT_EQUAL_STRING( "MQTTNoDataAvailable", str );
+
+    status = MQTTIllegalState;
+    str = MQTT_Status_strerror( status );
+    TEST_ASSERT_EQUAL_STRING( "MQTTIllegalState", str );
+
+    status = MQTTStateCollision;
+    str = MQTT_Status_strerror( status );
+    TEST_ASSERT_EQUAL_STRING( "MQTTStateCollision", str );
+
+    status = MQTTKeepAliveTimeout;
+    str = MQTT_Status_strerror( status );
+    TEST_ASSERT_EQUAL_STRING( "MQTTKeepAliveTimeout", str );
+
+    status = MQTTKeepAliveTimeout + 1;
+    str = MQTT_Status_strerror( status );
+    TEST_ASSERT_EQUAL_STRING( "Invalid MQTT Status code", str );
+}
+
+/* ========================================================================== */
