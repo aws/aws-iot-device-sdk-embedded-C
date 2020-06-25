8dbf274dc0ddbb9324e03e73d67a8c665348f111
Archit Aggarwal <architag@amazon.com>
2020-06-25 20:34:47 +0000

    Remove another assert causing MQTT test failure

--- libraries/standard/mqtt/src/iot_mqtt_api.c
+++ libraries/standard/mqtt/src/iot_mqtt_api.c
@@ -1697,7 +1697,6 @@ static IotMqttError_t _publishAsync( IotMqttConnection_t mqttConnection,
     uint8_t ** pPacketIdentifierHigh = NULL;
 
     IotMqtt_Assert( pPublishInfo != NULL );
-    IotMqtt_Assert( pPublishOperation != NULL );
 
     /* Create a PUBLISH operation. */
     status = _IotMqtt_CreateOperation( mqttConnection,
