c6b88b970a19586dfe488d33e066b209564ece10
Oscar Abrina <abrinao@amazon.com>
2020-06-25 16:27:59 -0700

    Remove SPDX-License-Identifier for harness.c

--- libraries/standard/http/cbmc/proofs/HTTPClient_InitializeRequestHeaders/HTTPClient_InitializeRequestHeaders_harness.c
+++ libraries/standard/http/cbmc/proofs/HTTPClient_InitializeRequestHeaders/HTTPClient_InitializeRequestHeaders_harness.c
@@ -1,6 +1,3 @@
-/* Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved. */
-/* SPDX-License-Identifier: Apache-2.0 */
-
 /*
  * Copyright (C) 2020 Amazon.com, Inc. or its affiliates.  All Rights Reserved.
  *
