6c387eaeaa83d0377368e83ead8f2b370e2f6e49
Oscar Michael Abrina <abrinao@amazon.com>
2020-06-25 17:35:58 -0700

    Add transport interface with POSIX implementations (#1002)
    * Add tcp connect/disconnect and plaintext transport send/recv
    
    * Get transport code to work with http plaintext demo
    
    * Finish draft for plaintext transport
    
    * Add filepaths for http plaintext demo
    
    * Add updated plaintext demo
    
    * Remove network_common.h and move setTimeout to plaintext transport
    
    * Remove wrong declaration in plaintext transport header file
    
    * Address feedback
    
    * Refactor tcp_posix
    
    * Remove all warnings from transport implementations
    
    * Rename send/recvTimeout to tcpSend/RecvTimeout
    
    * Update http basic tls demos to work with new transport interface
    
    * Revert changes to libraries and demos, which are to be changed in succeeding PRs
    
    * Update TCP_Disconnect if socket parameter is invalid
    
    * pContext -> pNetworkContext
    
    * Use INET6_ADDRSTRLEN macro from POSIX instead of defining one
    
    * Decouple resolveHostName and attemptConnection
    
    * Use double ptr for pListHead in resolveHostName
    
    * Wrap resolveHostName around returnStatus check
    
    * Fix capitalization of openssl_posix.h include
    
    * Fix switched parameters for LogDebug
    
    * Make some minor formatting changes
    
    * Remove premature X509_free
    
    * Revert to using setsockopt for setting the timeout in TCP_connect as a parameter
    
    * Use both usec and sec fields for timeval structure to prevent EDOM error
    
    * Refactor transport code for complexity <= 8
    
    * Log more errors depending on errno in tcp_posix.h
    
    * Create common sockets utility for establishing connection to server
    
    * Fix some warnings and errors from testing on a demo
    
    * Update docstrings
    
    * Address PR comments
    
    * Address remaining PR comments
    
    * Remove unnecessary includes and add asserts to private methods
    
    * Fix docstring
    
    * Update docstring for transport interface
    
    * Remove more unneeded includes
    
    * bytesToRead -> bytesToRecv
    
    * Update based on Gaurav's changes
    
    * Apply more changes from Gaurav
    
    * Fix some warnings from includes
    
    * Update docstrings for openssl_posix
    
    * Remove accidental commit of http_Demo_plaintext
    
    * Undo accidental git rm

---
+++ platform/posix/transport/src/plaintext_posix.c
@@ -0,0 +1,168 @@
+/*
+ * Copyright (C) 2020 Amazon.com, Inc. or its affiliates.  All Rights Reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+/* POSIX socket includes. */
+#include <errno.h>
+#include <sys/socket.h>
+
+#include "plaintext_posix.h"
+
+/*-----------------------------------------------------------*/
+
+/**
+ * @brief Defined by transport layer to check send or receive error.
+ */
+extern int errno;
+
+/*-----------------------------------------------------------*/
+
+/**
+ * @brief Log possible error from send/recv.
+ */
+static void logTransportError( void );
+
+/*-----------------------------------------------------------*/
+
+static void logTransportError( void )
+{
+    switch( errno )
+    {
+        case EBADF:
+            LogError( ( "The socket argument is not a valid file descriptor." ) );
+            break;
+
+        case ECONNRESET:
+            LogError( ( "A connection was forcibly closed by a peer." ) );
+            break;
+
+        case EDESTADDRREQ:
+            LogError( ( "The socket is not connection-mode and no peer address is set." ) );
+            break;
+
+        case EINTR:
+            LogError( ( "A signal interrupted send/recv." ) );
+            break;
+
+        case EINVAL:
+            LogError( ( "The MSG_OOB flag is set and no out-of-band data is available." ) );
+            break;
+
+        case ENOTCONN:
+            LogError( ( "A send/receive is attempted on a connection-mode socket that is not connected." ) );
+            break;
+
+        case ENOTSOCK:
+            LogError( ( "The socket argument does not refer to a socket." ) );
+            break;
+
+        case EOPNOTSUPP:
+            LogError( ( "The specified flags are not supported for this socket type or protocol." ) );
+            break;
+
+        case ETIMEDOUT:
+            LogError( ( "The connection timed out during connection establishment, or due to a transmission timeout on active connection." ) );
+            break;
+
+        case EMSGSIZE:
+            LogError( ( "The message is too large to be sent all at once, as the socket requires." ) );
+            break;
+
+        case EPIPE:
+            LogError( ( "The socket is shut down for writing, or the socket is connection-mode and is no longer connected. In the latter case, and if the socket is of type SOCK_STREAM or SOCK_SEQPACKET and the MSG_NOSIGNAL flag is not set, the SIGPIPE signal is generated to the calling thread." ) );
+            break;
+    }
+}
+/*-----------------------------------------------------------*/
+
+SocketStatus_t Plaintext_Connect( NetworkContext_t * pNetworkContext,
+                                  const ServerInfo_t * pServerInfo,
+                                  uint32_t sendTimeoutMs,
+                                  uint32_t recvTimeoutMs )
+{
+    return Sockets_Connect( &pNetworkContext->socketDescriptor,
+                            pServerInfo,
+                            sendTimeoutMs,
+                            recvTimeoutMs );
+}
+/*-----------------------------------------------------------*/
+
+SocketStatus_t Plaintext_Disconnect( const NetworkContext_t * pNetworkContext )
+{
+    return Sockets_Disconnect( pNetworkContext->socketDescriptor );
+}
+/*-----------------------------------------------------------*/
+
+int32_t Plaintext_Recv( NetworkContext_t * pNetworkContext,
+                        void * pBuffer,
+                        size_t bytesToRecv )
+{
+    int32_t bytesReceived = 0;
+
+    bytesReceived = recv( pNetworkContext->socketDescriptor, pBuffer, bytesToRecv, 0 );
+
+    if( bytesReceived == 0 )
+    {
+        /* Server closed the connection, treat it as an error. */
+        bytesReceived = -1;
+    }
+    else if( bytesReceived < 0 )
+    {
+        logTransportError();
+
+        /* Check if it was time out. */
+        if( ( errno == EAGAIN ) || ( errno == EWOULDBLOCK ) )
+        {
+            /* Set return value to 0 to indicate nothing to receive. */
+            bytesReceived = 0;
+        }
+    }
+    else
+    {
+        /* Empty else. */
+    }
+
+    return bytesReceived;
+}
+/*-----------------------------------------------------------*/
+
+int32_t Plaintext_Send( NetworkContext_t * pNetworkContext,
+                        const void * pBuffer,
+                        size_t bytesToSend )
+{
+    int32_t bytesSent = 0;
+
+    bytesSent = send( pNetworkContext->socketDescriptor, pBuffer, bytesToSend, 0 );
+
+    if( bytesSent < 0 )
+    {
+        logTransportError();
+
+        /* Check if it was time out */
+        if( ( errno == EAGAIN ) || ( errno == EWOULDBLOCK ) )
+        {
+            /* Set return value to 0 to indicate that send had timed out. */
+            bytesSent = 0;
+        }
+    }
+
+    return bytesSent;
+}
+/*-----------------------------------------------------------*/
