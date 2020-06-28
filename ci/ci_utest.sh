7c2a32026798d11288689b28a7e4f143d2b699a7
nateglims <glimsdal@amazon.com>
2020-06-27 21:25:03 -0700

    Check test return value.
    Tested locally by invalidating a test condition and verifying `$?` is 1
    after the script executes.

--- ci/ci_utest.sh
+++ ci/ci_utest.sh
@@ -8,10 +8,18 @@ cmake -S . -B build/ -DBUILD_TESTS=1 -G "Unix Makefiles"
 # Gather all the make targets and select unit tests. They have utest in the name.
 make help -C build/ | grep utest | tr -d '. ' | xargs make -C build/
 
+STATUS=
 for t in build/bin/tests/*
 do
     echo "= $( basename $t ) ="
     ./$t
 
+    if [ $? -ne "0" ] ; then
+        STATUS=1
+    fi
+
 done
 
+if [ "$STATUS" = "1" ] ; then
+    exit 1
+fi
