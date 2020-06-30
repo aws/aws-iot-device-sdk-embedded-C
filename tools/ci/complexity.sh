62832e8c2ba55c7660fd69bb747e9eb4c898b0e5
nateglims <glimsdal@amazon.com>
2020-06-29 20:08:22 -0700

    Fix and add comments to script.

--- tools/ci/complexity.sh
+++ tools/ci/complexity.sh
@@ -4,7 +4,7 @@ function usage {
     cat <<USAGE
 Usage: $0 [files...]
 
-An script to run complexity in CI. This is needed to avoid issues with
+A script to run complexity in CI. This is needed to avoid issues with
 exit codes.
 
 USAGE
@@ -17,6 +17,9 @@ fi
 
 complexity --scores --threshold=9 --horrid-threshold=8 $@
 
+# When no functions are scored (i.e. below the threshold) complexity has an
+# exit code of 5. The "horrid-threshold" value will cause the program to exit
+# with a different code. Other non-zero values indicate a failure of some type.
 if [ "$?" = "5" ] ; then
     exit 0
 else
