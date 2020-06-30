d2999b0978e6d414b4d9e1007b4e39c579f88f5f
nateglims <glimsdal@amazon.com>
2020-06-30 10:50:31 -0700

    More comments. Check for program existence.
    * Change if tests to be consistent. (-eq instead of "" = "")
    * Check if complexity program exists.
    * Add comments to the complexity command to explain the options.

--- tools/ci/complexity.sh
+++ tools/ci/complexity.sh
@@ -10,17 +10,30 @@ exit codes.
 USAGE
 }
 
+# If there are no arguments, we get a cryptic message from complexity.
 if [ $# -eq 0 ] ; then
     usage
     exit 1
 fi
 
+
+# Explicitly check that complexity exists.
+which complexity > /dev/null
+if [ $? -ne 0 ] ; then
+    echo "complexity not found"
+    exit 1
+fi
+
+# Run complexity. The `--threshold` option prints functions with a cylcomatic
+# complexity greater than or equal to the value. The `--horrid-threshold` makes
+# the program exit with a non-zero exit code if a function is strictly greater
+# than the value.
 complexity --scores --threshold=9 --horrid-threshold=8 $@
 
 # When no functions are scored (i.e. below the threshold) complexity has an
 # exit code of 5. The "horrid-threshold" value will cause the program to exit
 # with a different code. Other non-zero values indicate a failure of some type.
-if [ "$?" = "5" ] ; then
+if [ $? -eq 5 ] ; then
     exit 0
 else
     exit $?
