807ffe746c7449ef885b6049c5c836eb03fa9022
Oscar Abrina <abrinao@amazon.com>
2020-06-29 17:39:32 -0700

    Revert cbmc state

--- libraries/standard/http/cbmc/sources/http_cbmc_state.c
+++ libraries/standard/http/cbmc/sources/http_cbmc_state.c
@@ -142,11 +142,11 @@ int32_t TransportInterfaceSendStub( NetworkContext_t pContext,
     if( tries >= MAX_TRIES )
     {
         tries = 0;
-        return size;
+        return bytesToSend;
     }
 
     tries++;
-    return bytesToSend;
+    return size;
 }
 
 int32_t TransportInterfaceReceiveStub( NetworkContext_t context,
@@ -179,11 +179,11 @@ int32_t TransportInterfaceReceiveStub( NetworkContext_t context,
     if( tries >= MAX_TRIES )
     {
         tries = 0;
-        return size;
+        return bytesToRecv;
     }
 
     tries++;
-    return bytesToRecv;
+    return size;
 }
 
 HTTPTransportInterface_t * allocateTransportInterface( HTTPTransportInterface_t * pTransport )
