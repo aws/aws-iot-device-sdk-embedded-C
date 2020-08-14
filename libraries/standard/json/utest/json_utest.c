0a5d3df5cb902c02e01f2643d77ff74dddc8f8c4
Oscar Abrina <abrinao@amazon.com>
2020-08-13 20:38:39 -0700

    Add some simple cases

--- libraries/standard/json/utest/json_utest.c
+++ libraries/standard/json/utest/json_utest.c
@@ -6,10 +6,18 @@
 #include "json.h"
 
 /* Sample test from the docs. */
+#define JSON_QUERY_SEPARATOR               '.'
+
 #define JSON_DEPTH_2                       "{\"foo\":\"abc\",\"bar\":{\"foo\":\"xyz\"}}"
 #define JSON_DEPTH_2_LEN                   ( sizeof( JSON_DEPTH_2 ) - 1 )
 
-#define JSON_DEPTH_2_TRAILING_COMMA        "{\"foo\":\"abc\",\"bar\":{\"foo\":\"xyz\",}}"
+#define JSON_DEPTH_2_QUERY_KEY             "bar.foo"
+#define JSON_DEPTH_2_QUERY_KEY_LEN         ( sizeof( JSON_DEPTH_2_QUERY_KEY ) - 1 )
+
+#define JSON_EXPECTED_QUERY_ANSWER         "xyz"
+#define JSON_EXPECTED_QUERY_ANSWER_LEN     ( sizeof( JSON_EXPECTED_QUERY_ANSWER ) - 1 )
+
+#define JSON_DEPTH_2_TRAILING_COMMA        "{\"foo\":\"abc\",\"bar\":{\"foo\" : \"xyz\",}}"
 #define JSON_DEPTH_2_TRAILING_COMMA_LEN    ( sizeof( JSON_DEPTH_2_TRAILING_COMMA ) - 1 )
 
 /* ============================   UNITY FIXTURES ============================ */
@@ -38,16 +46,46 @@ int suiteTearDown( int numFailures )
 /* ========================================================================== */
 
 /**
- * @brief Test that JSON_Validate is able to update the context object correctly.
+ * @brief Test that JSON_Validate is able to classify valid JSON correctly.
  */
-void test_JSON_Validate_Happy_Path( void )
+void test_JSON_Validate_Valid_JSON( void )
 {
     JSONStatus_t jsonStatus;
 
     jsonStatus = JSON_Validate( JSON_DEPTH_2, JSON_DEPTH_2_LEN );
     TEST_ASSERT_EQUAL( JSONSuccess, jsonStatus );
+}
+
+/**
+ * @brief Test that JSON_Validate is able to classify invalid JSON correctly.
+ */
+void test_JSON_Validate_Invalid_JSON( void )
+{
+    JSONStatus_t jsonStatus;
 
     jsonStatus = JSON_Validate( JSON_DEPTH_2_TRAILING_COMMA,
                                 JSON_DEPTH_2_TRAILING_COMMA_LEN );
     TEST_ASSERT_EQUAL( JSONIllegalDocument, jsonStatus );
+}
+
+/**
+ * @brief Test that JSON_Search is able to classify invalid JSON correctly.
+ */
+void test_JSON_Search_Valid_JSON( void )
+{
+    JSONStatus_t jsonStatus;
+    char * outValue;
+    size_t outValueLength;
+
+    jsonStatus = JSON_Search( JSON_DEPTH_2,
+                              JSON_DEPTH_2_LEN,
+                              JSON_DEPTH_2_QUERY_KEY,
+                              JSON_DEPTH_2_QUERY_KEY_LEN,
+                              JSON_QUERY_SEPARATOR,
+                              &outValue,
+                              &outValueLength );
+
+    TEST_ASSERT_EQUAL( JSONSuccess, jsonStatus );
+    TEST_ASSERT_EQUAL( outValueLength, JSON_EXPECTED_QUERY_ANSWER_LEN );
+    TEST_ASSERT_EQUAL_STRING_LEN( JSON_EXPECTED_QUERY_ANSWER, outValue, outValueLength );
 }
