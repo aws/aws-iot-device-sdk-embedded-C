80bebdc722e5ec8a8c0d6e9be15b57cc9d75d8e1
Oscar Abrina <abrinao@amazon.com>
2020-08-17 18:31:26 -0700

    Complete coverage for nextKeyValuePair

--- libraries/standard/json/utest/json_utest.c
+++ libraries/standard/json/utest/json_utest.c
@@ -6,7 +6,7 @@
 #include "json.h"
 
 /* Sample test from the docs. */
-#define JSON_QUERY_SEPARATOR               '.'
+#define JSON_QUERY_SEPARATOR                    '.'
 
 #define SAMPLE_JSON                                                                  \
     "{\"literal\":true, \"more_literals\": {\"literal2\":false, \"literal3\":null}," \
@@ -14,37 +14,49 @@
     "\"number\": -123412, "                                                          \
     "\"decimal\":109238.42091289, "                                                  \
     "\"foo\":\"abc\",\"bar\":{\"foo\":\"xyz\"}}"
-#define SAMPLE_JSON_LEN                    ( sizeof( SAMPLE_JSON ) - 1 )
+#define SAMPLE_JSON_LEN                         ( sizeof( SAMPLE_JSON ) - 1 )
 
-#define SAMPLE_JSON_QUERY_KEY              "bar.foo"
-#define SAMPLE_JSON_QUERY_KEY_LEN          ( sizeof( SAMPLE_JSON_QUERY_KEY ) - 1 )
+#define SAMPLE_JSON_QUERY_KEY                   "bar.foo"
+#define SAMPLE_JSON_QUERY_KEY_LEN               ( sizeof( SAMPLE_JSON_QUERY_KEY ) - 1 )
 
-#define JSON_EXPECTED_QUERY_ANSWER         "xyz"
-#define JSON_EXPECTED_QUERY_ANSWER_LEN     ( sizeof( JSON_EXPECTED_QUERY_ANSWER ) - 1 )
+#define JSON_EXPECTED_QUERY_ANSWER              "xyz"
+#define JSON_EXPECTED_QUERY_ANSWER_LEN          ( sizeof( JSON_EXPECTED_QUERY_ANSWER ) - 1 )
 
-#define SINGLE_SCALAR                      "\"4102985123a\""
-#define SINGLE_SCALAR_LEN                  ( sizeof( SINGLE_SCALAR ) - 1 )
+#define SINGLE_SCALAR                           "\"4102985123a\""
+#define SINGLE_SCALAR_LEN                       ( sizeof( SINGLE_SCALAR ) - 1 )
 
-#define SAMPLE_JSON_TRAILING_CHAR          "{\"foo\":\"abc\",\"bar\":{\"foo\" : \"xyz\",}} ;"
-#define SAMPLE_JSON_TRAILING_CHAR_LEN      ( sizeof( SAMPLE_JSON_TRAILING_CHAR ) - 1 )
+#define TRAILING_COMMA_AFTER_VALUE              "{\"foo\":\"abc\",\"bar\":{\"foo\" : \"xyz\",}}"
+#define TRAILING_COMMA_AFTER_VALUE_LENGTH       ( sizeof( TRAILING_COMMA_AFTER_VALUE ) - 1 )
 
-#define SAMPLE_JSON_TRAILING_SPACES        "{\"foo\":\"abc\",\"bar\":{\"foo\" : \"xyz\"}}  "
-#define SAMPLE_JSON_TRAILING_SPACES_LEN    ( sizeof( SAMPLE_JSON_TRAILING_SPACES ) - 1 )
+#define INCORRECT_OBJECT_SEPARATOR              "{\"foo\": \"bar\"; \"bar\": \"foo\"}"
+#define INCORRECT_OBJECT_SEPARATOR_LENGTH       ( sizeof( INCORRECT_OBJECT_SEPARATOR ) - 1 )
 
-#define SAMPLE_JSON_KEY_CUT                "{\"foo\":\"abc\",\"bar\":{\""
-#define SAMPLE_JSON_KEY_CUT_LEN            ( sizeof( SAMPLE_JSON_KEY_CUT ) - 1 )
+#define SAMPLE_JSON_TRAILING_SPACES             "{\"foo\":\"abc\",\"bar\":{\"foo\" : \"xyz\"}}  "
+#define SAMPLE_JSON_TRAILING_SPACES_LEN         ( sizeof( SAMPLE_JSON_TRAILING_SPACES ) - 1 )
 
-#define MULTIPLE_VALID_ESCAPES             "\"\\\\ \\\" \\/ \\b \\f \\n \\r \\t \\\x12\" "
-#define MULTIPLE_VALID_ESCAPES_LENGTH      ( sizeof( MULTIPLE_VALID_ESCAPES ) - 1 )
+#define SAMPLE_JSON_KEY_CUT                     "{\"foo\":\"abc\",\"bar\":{\""
+#define SAMPLE_JSON_KEY_CUT_LEN                 ( sizeof( SAMPLE_JSON_KEY_CUT ) - 1 )
 
-#define ILLEGAL_LEADING_ZEROS              "07"
-#define ILLEGAL_LEADING_ZEROS_LENGTH       ( sizeof( ILLEGAL_LEADING_ZEROS ) - 1 )
+#define MULTIPLE_VALID_ESCAPES                  "\"\\\\ \\\" \\/ \\b \\f \\n \\r \\t \\\x12\" "
+#define MULTIPLE_VALID_ESCAPES_LENGTH           ( sizeof( MULTIPLE_VALID_ESCAPES ) - 1 )
 
-#define TRAILING_COMMA_IN_OBJECT           "{\"foo\":\"abc\",\"bar\":{\"foo\" : \"xyz\",}}"
-#define TRAILING_COMMA_IN_OBJECT_LENGTH    ( sizeof( TRAILING_COMMA_IN_OBJECT ) - 1 )
+#define ILLEGAL_LEADING_ZEROS                   "07"
+#define ILLEGAL_LEADING_ZEROS_LENGTH            ( sizeof( ILLEGAL_LEADING_ZEROS ) - 1 )
 
-#define TRAILING_COMMA_IN_ARRAY            "{\"hello\": [\"world\",]}"
-#define TRAILING_COMMA_IN_ARRAY_LENGTH     ( sizeof( TRAILING_COMMA_IN_ARRAY ) - 1 )
+#define TRAILING_COMMA_IN_OBJECT                "[{\"hello\": [\"foo\",]}]"
+#define TRAILING_COMMA_IN_OBJECT_LENGTH         ( sizeof( TRAILING_COMMA_IN_OBJECT ) - 1 )
+
+#define NOTHING_AFTER_COMMA_SEPARATOR           "{\"hello\": [5,"
+#define NOTHING_AFTER_COMMA_SEPARATOR_LENGTH    ( sizeof( NOTHING_AFTER_COMMA_SEPARATOR ) - 1 )
+
+#define CLOSING_SQUARE_BRACKET                  "]"
+#define CLOSING_SQUARE_BRACKET_LENGTH           ( sizeof( CLOSING_SQUARE_BRACKET ) - 1 )
+
+#define CLOSING_CURLY_BRACKET                   "}"
+#define CLOSING_CURLY_BRACKET_LENGTH            ( sizeof( CLOSING_CURLY_BRACKET ) - 1 )
+
+#define OPENING_CURLY_BRACKET                   "{"
+#define OPENING_CURLY_BRACKET_LENGTH            ( sizeof( OPENING_CURLY_BRACKET ) - 1 )
 
 /*#define UNICODE_CHARS                      "\"\\u\xf0\x9f\xa7\x99\"" */
 /*#define UNICODE_CHARS_LENGTH               ( sizeof( UNICODE_CHARS ) - 1 ) */
@@ -55,7 +67,7 @@
 #define INVALID_UTF8_PREMATURE_CUT                   "{\"foo\":\"abc\",\"bar\":{\"\xc2\xa9 "
 #define INVALID_UTF8_PREMATURE_CUT_LENGTH            ( sizeof( INVALID_UTF8_PREMATURE_CUT ) - 1 )
 
-#define WRONG_KEY_VALUE_SEPARATOR                    "{\"hello\";\"world\""
+#define WRONG_KEY_VALUE_SEPARATOR                    "{\"hello\";\"world\"}"
 #define WRONG_KEY_VALUE_SEPARATOR_LENGTH             ( sizeof( WRONG_KEY_VALUE_SEPARATOR ) - 1 )
 
 #define INVALID_UTF8_NEXT_BYTE                       "\"\xc2\x00\""
@@ -85,6 +97,12 @@
 #define INVALID_UTF8_LT_MAX_CP_FOUR_BYTES            "\"\xF0\x80\x80\x80\""
 #define INVALID_UTF8_LT_MAX_CP_FOUR_BYTES_LENGTH     ( sizeof( INVALID_UTF8_LT_MAX_CP_FOUR_BYTES ) - 1 )
 
+#define MISSING_ENCLOSING_ARRAY_MARKER               "{\"key\": []]}"
+#define MISSING_ENCLOSING_ARRAY_MARKER_LENGTH        ( sizeof( MISSING_ENCLOSING_ARRAY_MARKER ) - 1 )
+
+#define MISSING_ENCLOSING_OBJECT_MARKER              "\"{\"foo\":\"abc\",\"bar\":{{\"hey\": \"yo\"}"
+#define MISSING_ENCLOSING_OBJECT_MARKER_LENGTH       ( sizeof( MISSING_ENCLOSING_OBJECT_MARKER ) - 1 )
+
 /* The following escapes are considered invalid. */
 #define NUL_ESCAPE                                   "\"\\\x0\""
 #define NUL_ESCAPE_LENGTH                            ( sizeof( NUL_ESCAPE ) - 1 )
@@ -92,11 +110,11 @@
 #define ESCAPE_CHAR_ALONE                            "\"\\\""
 #define ESCAPE_CHAR_ALONE_LENGTH                     ( sizeof( ESCAPE_CHAR_ALONE ) - 1 )
 
-/* Triggers the case in which i < max for skipDecimals. */
+/* Triggers the case in which i < max for skipDigits. */
 #define NOTHING_AFTER_NUMBER                         "{\"decimal\": 1"
 #define NOTHING_AFTER_NUMBER_LENGTH                  ( sizeof( NOTHING_AFTER_NUMBER ) - 1 )
 
-/* Triggers the case in which i < max for skipDigits. */
+/* Triggers the case in which i < max for skipDecimals. */
 #define NOTHING_AFTER_DECIMAL_POINT                  "{\"decimal\": 1."
 #define NOTHING_AFTER_DECIMAL_POINT_LENGTH           ( sizeof( NOTHING_AFTER_DECIMAL_POINT ) - 1 )
 
@@ -128,8 +146,12 @@
 #define INVALID_EXPONENT_LENGTH                      ( sizeof( INVALID_EXPONENT ) - 1 )
 
 /* Illegal scalar entry in the array. */
-#define ILLEGAL_SCALAR_IN_ARRAY                      "{\"hello\": [5, world]\""
+#define ILLEGAL_SCALAR_IN_ARRAY                      "{\"hello\": [42, world]\""
 #define ILLEGAL_SCALAR_IN_ARRAY_LENGTH               ( sizeof( ILLEGAL_SCALAR_IN_ARRAY ) - 1 )
+
+/* Key is not a string. */
+#define ILLEGAL_KEY_NOT_STRING                       "{key: true}"
+#define ILLEGAL_KEY_NOT_STRING_LENGTH                ( sizeof( ILLEGAL_KEY_NOT_STRING ) - 1 )
 
 #define UNKNOWN_ESCAPE                               "\"\\\x20\""
 #define UNKNOWN_ESCAPE_LENGTH                        ( sizeof( UNKNOWN_ESCAPE ) - 1 )
@@ -196,18 +218,26 @@ void test_JSON_Validate_Invalid_JSON( void )
                                 TRAILING_COMMA_IN_OBJECT_LENGTH );
     TEST_ASSERT_EQUAL( JSONIllegalDocument, jsonStatus );
 
-    jsonStatus = JSON_Validate( TRAILING_COMMA_IN_ARRAY,
-                                TRAILING_COMMA_IN_ARRAY_LENGTH );
+    jsonStatus = JSON_Validate( NOTHING_AFTER_COMMA_SEPARATOR,
+                                NOTHING_AFTER_COMMA_SEPARATOR_LENGTH );
     TEST_ASSERT_EQUAL( JSONIllegalDocument, jsonStatus );
 
-    jsonStatus = JSON_Validate( SAMPLE_JSON_TRAILING_CHAR,
-                                SAMPLE_JSON_TRAILING_CHAR_LEN );
+    jsonStatus = JSON_Validate( TRAILING_COMMA_AFTER_VALUE,
+                                TRAILING_COMMA_AFTER_VALUE_LENGTH );
     TEST_ASSERT_EQUAL( JSONIllegalDocument, jsonStatus );
 
     jsonStatus = JSON_Validate( NUL_ESCAPE, NUL_ESCAPE_LENGTH );
     TEST_ASSERT_EQUAL( JSONIllegalDocument, jsonStatus );
 
     jsonStatus = JSON_Validate( UNKNOWN_ESCAPE, UNKNOWN_ESCAPE_LENGTH );
+    TEST_ASSERT_EQUAL( JSONIllegalDocument, jsonStatus );
+
+    jsonStatus = JSON_Validate( CLOSING_SQUARE_BRACKET,
+                                CLOSING_SQUARE_BRACKET_LENGTH );
+    TEST_ASSERT_EQUAL( JSONIllegalDocument, jsonStatus );
+
+    jsonStatus = JSON_Validate( CLOSING_CURLY_BRACKET,
+                                CLOSING_CURLY_BRACKET_LENGTH );
     TEST_ASSERT_EQUAL( JSONIllegalDocument, jsonStatus );
 
     jsonStatus = JSON_Validate( ESCAPE_CHAR_ALONE, ESCAPE_CHAR_ALONE_LENGTH );
@@ -238,6 +268,10 @@ void test_JSON_Validate_Invalid_JSON( void )
 
     jsonStatus = JSON_Validate( NOTHING_AFTER_EXPONENT_MARKER,
                                 NOTHING_AFTER_EXPONENT_MARKER_LENGTH );
+    TEST_ASSERT_EQUAL( JSONIllegalDocument, jsonStatus );
+
+    jsonStatus = JSON_Validate( MISSING_ENCLOSING_ARRAY_MARKER,
+                                MISSING_ENCLOSING_ARRAY_MARKER_LENGTH );
     TEST_ASSERT_EQUAL( JSONIllegalDocument, jsonStatus );
 
     jsonStatus = JSON_Validate( INVALID_EXPONENT,
@@ -316,7 +350,6 @@ void test_JSON_Search_Valid_JSON( void )
                               JSON_QUERY_SEPARATOR,
                               &outValue,
                               &outValueLength );
-
     TEST_ASSERT_EQUAL( JSONSuccess, jsonStatus );
     TEST_ASSERT_EQUAL( outValueLength, JSON_EXPECTED_QUERY_ANSWER_LEN );
     TEST_ASSERT_EQUAL_STRING_LEN( JSON_EXPECTED_QUERY_ANSWER, outValue, outValueLength );
@@ -339,6 +372,68 @@ void test_JSON_Search_Invalid_JSON( void )
                               JSON_QUERY_SEPARATOR,
                               &outValue,
                               &outValueLength );
+    TEST_ASSERT_EQUAL( JSONIllegalDocument, jsonStatus );
 
+    jsonStatus = JSON_Search( WHITE_SPACE,
+                              WHITE_SPACE_LENGTH,
+                              SAMPLE_JSON_QUERY_KEY,
+                              SAMPLE_JSON_QUERY_KEY_LEN,
+                              JSON_QUERY_SEPARATOR,
+                              &outValue,
+                              &outValueLength );
+    TEST_ASSERT_EQUAL( JSONIllegalDocument, jsonStatus );
+
+    jsonStatus = JSON_Search( CLOSING_CURLY_BRACKET,
+                              CLOSING_CURLY_BRACKET_LENGTH,
+                              SAMPLE_JSON_QUERY_KEY,
+                              SAMPLE_JSON_QUERY_KEY_LEN,
+                              JSON_QUERY_SEPARATOR,
+                              &outValue,
+                              &outValueLength );
+    TEST_ASSERT_EQUAL( JSONNotFound, jsonStatus );
+
+    jsonStatus = JSON_Search( OPENING_CURLY_BRACKET,
+                              OPENING_CURLY_BRACKET_LENGTH,
+                              SAMPLE_JSON_QUERY_KEY,
+                              SAMPLE_JSON_QUERY_KEY_LEN,
+                              JSON_QUERY_SEPARATOR,
+                              &outValue,
+                              &outValueLength );
+    TEST_ASSERT_EQUAL( JSONIllegalDocument, jsonStatus );
+
+    jsonStatus = JSON_Search( INCORRECT_OBJECT_SEPARATOR,
+                              INCORRECT_OBJECT_SEPARATOR_LENGTH,
+                              SAMPLE_JSON_QUERY_KEY,
+                              SAMPLE_JSON_QUERY_KEY_LEN,
+                              JSON_QUERY_SEPARATOR,
+                              &outValue,
+                              &outValueLength );
+    TEST_ASSERT_EQUAL( JSONIllegalDocument, jsonStatus );
+
+    jsonStatus = JSON_Search( ILLEGAL_KEY_NOT_STRING,
+                              ILLEGAL_KEY_NOT_STRING_LENGTH,
+                              SAMPLE_JSON_QUERY_KEY,
+                              SAMPLE_JSON_QUERY_KEY_LEN,
+                              JSON_QUERY_SEPARATOR,
+                              &outValue,
+                              &outValueLength );
+    TEST_ASSERT_EQUAL( JSONIllegalDocument, jsonStatus );
+
+    jsonStatus = JSON_Search( WRONG_KEY_VALUE_SEPARATOR,
+                              WRONG_KEY_VALUE_SEPARATOR_LENGTH,
+                              SAMPLE_JSON_QUERY_KEY,
+                              SAMPLE_JSON_QUERY_KEY_LEN,
+                              JSON_QUERY_SEPARATOR,
+                              &outValue,
+                              &outValueLength );
+    TEST_ASSERT_EQUAL( JSONIllegalDocument, jsonStatus );
+
+    jsonStatus = JSON_Search( NOTHING_AFTER_KEY,
+                              NOTHING_AFTER_KEY_LENGTH,
+                              SAMPLE_JSON_QUERY_KEY,
+                              SAMPLE_JSON_QUERY_KEY_LEN,
+                              JSON_QUERY_SEPARATOR,
+                              &outValue,
+                              &outValueLength );
     TEST_ASSERT_EQUAL( JSONIllegalDocument, jsonStatus );
 }
