4cc7ac3e8a08a41d1c4609784a8c45b7eb9b9f65
Oscar Abrina <abrinao@amazon.com>
2020-08-22 12:08:38 -0700

    Mock close(int fd) successfully with CMock

---
+++ platform/posix/transport/utest/sockets_utest.c
@@ -0,0 +1,168 @@
+#include <string.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+#include "unity.h"
+
+/* Include paths for public enums, structures, and macros. */
+#include "sockets_posix.h"
+
+#include "mock_netdb.h"
+#include "mock_socket.h"
+#include "mock_inet.h"
+#include "mock_close.h"
+
+#define NUM_ADDR_INFO        3
+#define SEND_RECV_TIMEOUT    0
+
+/* ============================   UNITY FIXTURES ============================ */
+
+/* Called before each test method. */
+void setUp()
+{
+}
+
+/* Called after each test method. */
+void tearDown()
+{
+}
+
+/* Called at the beginning of the whole suite. */
+void suiteSetUp()
+{
+}
+
+/* Called at the end of the whole suite. */
+int suiteTearDown( int numFailures )
+{
+    return numFailures;
+}
+
+/* ========================================================================== */
+
+void allocateAddrInfoLinkedList( struct addrinfo ** head )
+{
+    struct addrinfo * index = NULL;
+    int i;
+
+    TEST_ASSERT_NOT_NULL( head );
+
+    for( i = 0; i < NUM_ADDR_INFO; i++ )
+    {
+        struct addrinfo * next = malloc( sizeof( struct addrinfo ) );
+        memset( next, 0, sizeof( struct addrinfo ) );
+        next->ai_family = AF_INET;
+        next->ai_socktype = SOCK_STREAM;
+        next->ai_protocol = IPPROTO_TCP;
+        next->ai_next = NULL;
+
+        /* Every other IP address will be IPv4 for coverage. */
+        struct sockaddr * ai_addr = malloc( sizeof( struct sockaddr ) );
+
+        if( i % 2 )
+        {
+            ai_addr->sa_family = AF_INET6;
+        }
+        else
+        {
+            ai_addr->sa_family = AF_INET;
+        }
+
+        next->ai_addr = ai_addr;
+
+        if( i == 0 )
+        {
+            index = next;
+            *head = index;
+        }
+        else
+        {
+            index->ai_next = next;
+            index = index->ai_next;
+        }
+    }
+}
+
+void freeAddrInfoLinkedList( struct addrinfo * head )
+{
+    struct addrinfo * tmp;
+
+    TEST_ASSERT_NOT_NULL( head );
+
+    while( head != NULL )
+    {
+        tmp = head;
+        head = head->ai_next;
+        free( tmp );
+        free( tmp->ai_addr );
+    }
+}
+
+/**
+ * @brief Test Sockets_Disconnect.
+ */
+void test_Sockets_Disconnect_Valid_Socket( void )
+{
+    SocketStatus_t socketStatus;
+    int tcpSocket = 1;
+
+    shutdown_ExpectAnyArgsAndReturn( 0 );
+    close_ExpectAnyArgsAndReturn( 0 );
+
+    socketStatus = Sockets_Disconnect( tcpSocket );
+    TEST_ASSERT_EQUAL( SOCKETS_SUCCESS, socketStatus );
+}
+
+void test_Sockets_Disconnect_Invalid_Socket( void )
+{
+    SocketStatus_t socketStatus;
+    int tcpSocket = 0;
+
+    socketStatus = Sockets_Disconnect( tcpSocket );
+    TEST_ASSERT_EQUAL( SOCKETS_INVALID_PARAMETER, socketStatus );
+}
+
+void test_Sockets_Connect_Valid_Socket( void )
+{
+    SocketStatus_t socketStatus;
+    ServerInfo_t serverInfo;
+    int tcpSocket = 1, i = 1;
+    struct addrinfo * addrInfo;
+
+    serverInfo.pHostName = "google";
+    serverInfo.hostNameLength = strlen( "google" );
+    serverInfo.port = 80;
+
+    allocateAddrInfoLinkedList( &addrInfo );
+    getaddrinfo_ExpectAnyArgsAndReturn( 0 );
+    getaddrinfo_ReturnThruPtr___pai( &addrInfo );
+
+    for( i = 1; i <= NUM_ADDR_INFO; i++ )
+    {
+        socket_ExpectAnyArgsAndReturn( tcpSocket );
+        inet_ntop_ExpectAnyArgsAndReturn( NULL );
+
+        /* The last iteration should make connect() succeed. */
+        if( i < NUM_ADDR_INFO )
+        {
+            connect_ExpectAnyArgsAndReturn( -1 );
+            close_ExpectAnyArgsAndReturn( 0 );
+        }
+        else
+        {
+            connect_ExpectAnyArgsAndReturn( 0 );
+        }
+    }
+
+    freeaddrinfo_ExpectAnyArgs();
+    setsockopt_ExpectAnyArgsAndReturn( 0 );
+    setsockopt_ExpectAnyArgsAndReturn( 0 );
+
+    socketStatus = Sockets_Connect( &tcpSocket,
+                                    &serverInfo,
+                                    SEND_RECV_TIMEOUT,
+                                    SEND_RECV_TIMEOUT );
+    TEST_ASSERT_EQUAL( SOCKETS_SUCCESS, socketStatus );
+
+    freeAddrInfoLinkedList( addrInfo );
+}
