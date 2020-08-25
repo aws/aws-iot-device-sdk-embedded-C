ca776e529fdf25c83a5b02ecdd563fd396e7a3ad
Oscar Abrina <abrinao@amazon.com>
2020-08-24 17:19:30 -0700

    Successfully create an api header file for openssl to use as input for CMock

--- platform/posix/transport/utest/mocks/openssl_api.h
+++ platform/posix/transport/utest/mocks/openssl_api.h
@@ -1,6 +1,9 @@
 #ifndef OPENSSL_API_H_
 #define OPENSSL_API_H_
 
+#include <openssl/ssl.h>
+#include <openssl/err.h>
+
 /**
  * @file openssl_api.h
  * @brief This file is used to generate mocks for the OpenSSL API's used in
@@ -8,67 +11,112 @@
  * errors from parsing its macros.
  */
 
+struct ssl_st
+{
+    int filler;
+};
 
-int X509_STORE_add_cert( X509_STORE * ctx,
-                         X509 * x );
+struct x509_st
+{
+    int filler;
+};
 
-X509_STORE * SSL_CTX_get_cert_store( const SSL_CTX * );
+struct x509_store_st
+{
+    int filler;
+};
 
-int SSL_CTX_use_certificate_chain_file( SSL_CTX * ctx,
-                                        const char * file );
+struct ssl_ctx_st
+{
+    int filler;
+};
 
-int SSL_CTX_use_PrivateKey_file( SSL_CTX * ctx,
-                                 const char * file,
-                                 int type );
+struct ssl_method_st
+{
+    int filler;
+};
 
-int SSL_set_alpn_protos( SSL * ssl,
-                         const unsigned char * protos,
-                         unsigned int protos_len );
+#undef SSL_set_max_send_fragment
+#undef SSL_set_tlsext_host_name
+#undef SSL_CTX_set_mode
 
-long SSL_set_max_send_fragment( SSL * ssl,
-                                long m );
+extern int X509_STORE_add_cert( X509_STORE * ctx,
+                                X509 * x );
 
-void SSL_set_default_read_buffer_len( SSL * s,
-                                      size_t len );
+X509 * PEM_read_X509( FILE * fp,
+                      X509 ** x,
+                      pem_password_cb * cb,
+                      void * u );
 
-int SSL_set_tlsext_host_name( const SSL * s,
-                              const char * name );
+extern X509_STORE * SSL_CTX_get_cert_store( const SSL_CTX * ctx );
 
-SSL_CTX * SSL_CTX_new( const SSL_METHOD * meth );
+extern int SSL_CTX_use_certificate_chain_file( SSL_CTX * ctx,
+                                               const char * file );
 
-const SSL_METHOD * TLS_client_method( void );
+extern int SSL_CTX_use_PrivateKey_file( SSL_CTX * ctx,
+                                        const char * file,
+                                        int type );
 
-long SSL_CTX_set_mode( SSL_CTX * ctx,
-                       long mode );
+extern int SSL_set_alpn_protos( SSL * ssl,
+                                const unsigned char * protos,
+                                unsigned int protos_len );
 
-SSL * SSL_new( SSL_CTX * ctx );
+extern int SSL_set_max_send_fragment( SSL * ssl,
+                                      long m );
 
-void SSL_set_verify( SSL * s,
-                     int mode,
-                     SSL_verify_cb callback );
+extern void SSL_set_default_read_buffer_len( SSL * s,
+                                             size_t len );
 
-int SSL_set_fd( SSL * s,
-                int fd );
+extern int SSL_set_tlsext_host_name( const SSL * s,
+                                     const char * name );
 
-int SSL_connect( SSL * ssl );
+extern SSL_CTX * SSL_CTX_new( const SSL_METHOD * meth );
 
-long SSL_get_verify_result( const SSL * ssl );
+extern const SSL_METHOD * TLS_client_method( void );
 
-void SSL_CTX_free( SSL_CTX * );
+extern long SSL_CTX_set_mode( SSL_CTX * ctx,
+                              long mode );
 
-void SSL_free( SSL * ssl );
+extern SSL * SSL_new( SSL_CTX * ctx );
 
-int SSL_shutdown( SSL * s );
+extern void SSL_set_verify( SSL * s,
+                            int mode,
+                            SSL_verify_cb callback );
 
-int SSL_read( SSL * ssl,
-              void * buf,
-              int num );
+extern int SSL_set_fd( SSL * s,
+                       int fd );
 
-int SSL_get_error( const SSL * s,
-                   int ret_code );
+extern int SSL_connect( SSL * ssl );
 
-int SSL_write( SSL * ssl,
-               const void * buf,
-               int num );
+extern long SSL_get_verify_result( const SSL * ssl );
+
+extern void SSL_CTX_free( SSL_CTX * );
+
+extern void SSL_free( SSL * ssl );
+
+extern long SSL_CTX_ctrl( SSL_CTX * ctx,
+                          int cmd,
+                          long larg,
+                          void * parg );
+
+extern long SSL_ctrl( SSL * ssl,
+                      int cmd,
+                      long larg,
+                      void * parg );
+
+extern int SSL_shutdown( SSL * s );
+
+extern int SSL_read( SSL * ssl,
+                     void * buf,
+                     int num );
+
+extern int SSL_get_error( const SSL * s,
+                          int ret_code );
+
+extern int SSL_write( SSL * ssl,
+                      const void * buf,
+                      int num );
+
+const char * ERR_reason_error_string( unsigned long e );
 
 #endif /* ifndef OPENSSL_API_H_ */
