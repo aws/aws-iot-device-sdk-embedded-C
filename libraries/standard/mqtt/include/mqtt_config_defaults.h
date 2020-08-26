08fdf5615501a2b41a3ff368fbe719cfd674fdc9
Archit Aggarwal <architag@amazon.com>
2020-08-26 01:29:33 +0000

    Add individual doxygen sections for logging macros, and add @note about intended purpose of the config_defaults.h file

--- libraries/standard/mqtt/include/mqtt_config_defaults.h
+++ libraries/standard/mqtt/include/mqtt_config_defaults.h
@@ -23,6 +23,10 @@
  * @file mqtt_config_defaults.h
  * @brief This represents the default values for the configuration macros
  * for the MQTT library.
+ *
+ * @note This file SHOULD NOT be modified. This file can be used as a reference
+ * to all configuration macros of the MQTT library for creating an mqtt_config.h
+ * file. The MQTT library requires an mqtt_config.h file to build.
  */
 
 #ifndef MQTT_CONFIG_DEFAULTS_H_
@@ -84,35 +88,77 @@
 #endif
 
 /**
- * @brief Definition of logging macros to map logging calls within MQTT
- * library to application-specific logging implementation.
+ * @brief Macro that is called in the MQTT library for logging "Error" level
+ * messages.
  *
- * Each logging macro represents a different logging level. Only define those
- * logging macros whose corresponding logging level messages are desired in
- * the MQTT library build.
+ * To enable error logging from MQTT library, this macro should be mapped to the
+ * application-specific logging implementation that supports error logging.
  *
- * @note The logging macros are called in MQTT library with parameters wrapped in
+ * @note The logging macro is called in MQTT library with parameters wrapped in
  * double parentheses to be ISO C89/C90 standard compliant. For an example
  * implementation of the logging macros, refer to mqtt_config.h files of MQTT demo folder
  * and the reference implementation of logging-stack in demos.
  *
- * #define LogError( X )           MyErrorLoggingFunction( X )
- * #define LogWarn( X )            MyWarnLoggingFunction( X )
- * #define LogInfo( X )            MyInfoLoggingFunction( X )
- * #define LogDebug( X )           MyDebugLoggingFunction( X )
- *
- * <b>Default values</b>: Logging is turned off, and no code is generated for logging calls
- * in the MQTT library on compilation.
+ * <b>Default value</b>: Error logging is turned off, and no code is generated for calls
+ * to the macro in the MQTT library on compilation.
  */
 #ifndef LogError
     #define LogError( message )
 #endif
+
+/**
+ * @brief Macro that is called in the MQTT library for logging "Warning" level
+ * messages.
+ *
+ * To enable logging of warning messages, this macro should be mapped to the
+ * application-specific logging implementation that supports warning logging.
+ *
+ * @note The logging macro is called in MQTT library with parameters wrapped in
+ * double parentheses to be ISO C89/C90 standard compliant. For an example
+ * implementation of the logging macros, refer to mqtt_config.h files of MQTT demo folder
+ * and the reference implementation of logging-stack in demos.
+ *
+ * <b>Default value</b>: Warning logs are turned off, and no code is generated for calls
+ * to the macro in the MQTT library on compilation.
+ */
 #ifndef LogWarn
     #define LogWarn( message )
 #endif
+
+/**
+ * @brief Macro that is called in the MQTT library for logging "Info" level
+ * messages.
+ *
+ * To enable info level logging from MQTT library, this macro should be mapped to the
+ * application-specific logging implementation that supports info logging.
+ *
+ * @note The logging macro is called in MQTT library with parameters wrapped in
+ * double parentheses to be ISO C89/C90 standard compliant. For an example
+ * implementation of the logging macros, refer to mqtt_config.h files of MQTT demo folder
+ * and the reference implementation of logging-stack in demos.
+ *
+ * <b>Default value</b>: Info logging is turned off, and no code is generated for calls
+ * to the macro in the MQTT library on compilation.
+ */
 #ifndef LogInfo
     #define LogInfo( message )
 #endif
+
+/**
+ * @brief Macro that is called in the MQTT library for logging "Debug" level
+ * messages.
+ *
+ * To enable debug level logging from MQTT library, this macro should be mapped to the
+ * application-specific logging implementation that supports debug logging.
+ *
+ * @note The logging macro is called in MQTT library with parameters wrapped in
+ * double parentheses to be ISO C89/C90 standard compliant. For an example
+ * implementation of the logging macros, refer to mqtt_config.h files of MQTT demo folder
+ * and the reference implementation of logging-stack in demos.
+ *
+ * <b>Default value</b>: Debug logging is turned off, and no code is generated for calls
+ * to the macro in the MQTT library on compilation.
+ */
 #ifndef LogDebug
     #define LogDebug( message )
 #endif
