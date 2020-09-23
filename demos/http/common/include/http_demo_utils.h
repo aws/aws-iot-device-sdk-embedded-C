c0a838fcdbe95d22d46d31007eb92cf57a88d596
Oscar Michael Abrina <abrinao@amazon.com>
2020-09-23 16:45:20 -0700

    Add reconnection logic to remaining HTTP demos (#1280)
    This adds the same reconnection logic added in PR #1253 to the remaining HTTP demos. Along with this, the function connectToServerWithBackoffRetries is refactored into a common place under demos/http/common. The ports are also renamed differently for all the demos so that they can be passed as CFLAGS, and the dependency on httpbin.org is removed.

---
+++ demos/http/common/include/http_demo_utils.h
@@ -0,0 +1,51 @@
+/*
+ * AWS IoT Device SDK for Embedded C V202009.00
+ * Copyright (C) 2020 Amazon.com, Inc. or its affiliates.  All Rights Reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+/* Standard includes. */
+#include <stdlib.h>
+
+/* Transport interface include. */
+#include "transport_interface.h"
+
+/**
+ * @brief Function pointer for establishing connection to a server.
+ *
+ * @param[out] pNetworkContext Implementation-defined network context.
+ *
+ * @return EXIT_FAILURE on failure; EXIT_SUCCESS on successful connection.
+ */
+typedef int32_t ( * TransportConnect_t )( NetworkContext_t * pNetworkContext );
+
+/**
+ * @brief Connect to a server with reconnection retries.
+ *
+ * If connection fails, retry is attempted after a timeout.
+ * Timeout value will exponentially increase until maximum
+ * timeout value is reached or the number of attempts are exhausted.
+ *
+ * @param[in] connectFunction Function pointer for establishing connection to a server.
+ * @param[out] pNetworkContext Implementation-defined network context.
+ *
+ * @return EXIT_FAILURE on failure; EXIT_SUCCESS on successful connection.
+ */
+int32_t connectToServerWithBackoffRetries( TransportConnect_t connectFunction,
+                                           NetworkContext_t * pNetworkContext );
---
+++ demos/http/common/src/http_demo_utils.c
@@ -0,0 +1,69 @@
+/*
+ * AWS IoT Device SDK for Embedded C V202009.00
+ * Copyright (C) 2020 Amazon.com, Inc. or its affiliates.  All Rights Reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+/* Standard includes. */
+#include <assert.h>
+
+/* Include Demo Config as the first non-system header. */
+#include "demo_config.h"
+
+#include "http_demo_utils.h"
+
+/* Retry utilities. */
+#include "retry_utils.h"
+
+int32_t connectToServerWithBackoffRetries( TransportConnect_t connectFunction,
+                                           NetworkContext_t * pNetworkContext )
+{
+    int32_t returnStatus = EXIT_FAILURE;
+    /* Status returned by the retry utilities. */
+    RetryUtilsStatus_t retryUtilsStatus = RetryUtilsSuccess;
+    /* Struct containing the next backoff time. */
+    RetryUtilsParams_t reconnectParams;
+
+    assert( connectFunction != NULL );
+
+    /* Initialize reconnect attempts and interval */
+    RetryUtils_ParamsReset( &reconnectParams );
+
+    /* Attempt to connect to HTTP server. If connection fails, retry after
+     * a timeout. Timeout value will exponentially increase until maximum
+     * attempts are reached. */
+    do
+    {
+        returnStatus = connectFunction( pNetworkContext );
+
+        if( returnStatus != EXIT_SUCCESS )
+        {
+            LogWarn( ( "Connection to the HTTP server failed. "
+                       "Retrying connection with backoff and jitter." ) );
+            retryUtilsStatus = RetryUtils_BackoffAndSleep( &reconnectParams );
+        }
+    } while( ( returnStatus == EXIT_FAILURE ) && ( retryUtilsStatus == RetryUtilsSuccess ) );
+
+    if( returnStatus == EXIT_FAILURE )
+    {
+        LogError( ( "Connection to the server failed, all attempts exhausted." ) );
+    }
+
+    return returnStatus;
+}
