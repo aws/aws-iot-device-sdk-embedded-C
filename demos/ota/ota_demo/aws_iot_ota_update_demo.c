390fa9d480936a52938499a8929eff667f830791
pvyawaha <pvyawaha@amazon.com>
2020-10-11 22:11:11 -0700

    Update OTA demo loop for receivng notification

--- demos/ota/ota_demo/aws_iot_ota_update_demo.c
+++ demos/ota/ota_demo/aws_iot_ota_update_demo.c
@@ -745,6 +745,8 @@ void startOTADemo( MQTTContext_t * pMqttContext )
 {
     int ret = 0;
 
+    MQTTStatus_t mqttStatus;
+
     OtaEventMsg_t eventMsg = { 0 };
 
     /* MQTT susbsrciption manager parameters.*/
@@ -801,7 +803,21 @@ void startOTADemo( MQTTContext_t * pMqttContext )
                    OTA_GetPacketsProcessed(),
                    OTA_GetPacketsDropped() ) );
 
-        sleep( OTA_DEMO_TASK_DELAY_SECONDS );
+       if(state == OtaAgentStateWaitingForJob )
+       {
+            mqttStatus = MQTT_ProcessLoop( pMqttContext, 1000 );
+
+            if( mqttStatus != MQTTSuccess )
+            {
+                LogError( ( "MQTT_ProcessLoop returned with status = %u.",
+                        mqttStatus ) );
+            }
+       }
+       else
+       {
+           sleep( OTA_DEMO_TASK_DELAY_SECONDS );
+       }
+       
     }
 
 }
