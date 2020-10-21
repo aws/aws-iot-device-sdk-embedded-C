3a8d3e90fb53a4731cd6d22aa4b4a79eeb02c0e5
Sukhmani Minhas <50919130+sukhmanm@users.noreply.github.com>
2020-10-20 23:03:58 -0400

    Script formatting + add newline

--- demos/http/common/src/presigned_urls_gen.py
+++ demos/http/common/src/presigned_urls_gen.py
@@ -4,6 +4,7 @@ import boto3
 from botocore.client import Config
 import argparse
 
+
 def get_presigned_urls(bucket_name, key_name) -> None:
     """
     Prints the presigned GET and PUT URLs assigned to the demo specific C macros,
@@ -17,34 +18,37 @@ def get_presigned_urls(bucket_name, key_name) -> None:
     # Get the service client.
     # SigV2 is being deprecated. If the boto3 installation in the current Python environment has an older version of
     # the package, then this configuration forces the use of SigV4.
-    s3 = boto3.client('s3', config=Config(signature_version='s3v4'))
+    s3 = boto3.client("s3", config=Config(signature_version="s3v4"))
 
-    client_method_dict = {
-        'GET': 'get_object',
-        'PUT': 'put_object'
-    }
+    client_method_dict = {"GET": "get_object", "PUT": "put_object"}
 
     # Generate the URL to get 'key-name' from 'bucket-name'
     for method in client_method_dict.keys():
         url = s3.generate_presigned_url(
-            ClientMethod=client_method_dict[method],
-            Params={
-                'Bucket': bucket_name,
-                'Key': key_name
-            }
+            ClientMethod=client_method_dict[method], Params={"Bucket": bucket_name, "Key": key_name}
         )
-        print('#define S3_PRESIGNED_' + method + '_URL' + '    ' + '\"' + url + '\"')
+        print("#define S3_PRESIGNED_" + method + "_URL" + "    " + '"' + url + '"\n')
+
 
 def main():
     """
     Generate demo C macro strings, on the console, for the input S3 bucket and object key.
     """
-    parser = argparse.ArgumentParser(description='S3 Presigned URL Generator. See README.md')
-    parser.add_argument('--bucket', action='store', required=True, dest='bucket_name', help='The name of the S3 bucket of interest.')
-    parser.add_argument('--key', action='store', required=True, dest='key_name', help='The name of the S3 Object in the bucket. This is referred to as a \'key\'')
+    parser = argparse.ArgumentParser(description="S3 Presigned URL Generator. See README.md")
+    parser.add_argument(
+        "--bucket", action="store", required=True, dest="bucket_name", help="The name of the S3 bucket of interest."
+    )
+    parser.add_argument(
+        "--key",
+        action="store",
+        required=True,
+        dest="key_name",
+        help="The name of the S3 Object in the bucket. This is referred to as a 'key'",
+    )
     args = parser.parse_args()
 
     get_presigned_urls(args.bucket_name, args.key_name)
 
-if __name__ == '__main__':  # pragma: no cover
+
+if __name__ == "__main__":  # pragma: no cover
     main()
