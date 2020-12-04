7e2e098c0ae8d3708c6d54cac168422e859e8bba
Joshua Yan <52796499+yanjos-dev@users.noreply.github.com>
2020-12-03 18:52:48 -0800

    Mock OpenSSL and stdio func. for OTA unit tests

--- platform/posix/ota_pal/utest/mocks/stdio_api.h
+++ platform/posix/ota_pal/utest/mocks/stdio_api.h
@@ -44,6 +44,10 @@ extern _STDIO_FILE_TYPE * fopen( const char * __filename,
 /* Close STREAM. */
 extern int fclose( _STDIO_FILE_TYPE * __stream );
 
+extern int snprintf ( char * s, size_t n, const char * format, ... );
+
+extern size_t fread( void * ptr, size_t size, size_t count, FILE * stream );
+
 /* The "fseek" function needs to be mocked to test the OTA PAL. This function
    can't be directly mocked because it's required by the coverage tools. To get
    around this, the "fseek" function is defined as "fseek_alias" in the test
