96c3d4195544d4a5aaa4db8c309a0b3b5b345255
pvyawaha <pvyawaha@amazon.com>
2020-12-05 20:36:21 -0800

    Add topic parsing for message type callback

--- demos/ota/ota_demo_core_mqtt/ota_demo_core_mqtt.c
+++ demos/ota/ota_demo_core_mqtt/ota_demo_core_mqtt.c
@@ -207,6 +207,26 @@
     #define CLIENT_USERNAME_WITH_METRICS    CLIENT_USERNAME METRICS_STRING
 #endif
 
+/**
+ * @brief The common prefix for all OTA topics.
+ */
+#define OTA_TOPIC_PREFIX      "$aws/things/"
+
+/**
+ * @brief The string used for jobs topics.
+ */
+#define OTA_TOPIC_JOBS      "jobs"
+
+/**
+ * @brief The string used for streaming service topics.
+ */
+#define OTA_TOPIC_STREAM     "streams"
+
+/**
+ * @brief The length of #OTA_TOPIC_PREFIX
+ */
+#define OTA_TOPIC_PREFIX_LENGTH     ( ( uint16_t ) ( sizeof( OTA_TOPIC_PREFIX ) - 1U ) )
+
 /*-----------------------------------------------------------*/
 
 /* Linkage for error reporting. */
@@ -257,6 +277,16 @@ static pthread_mutex_t mqttMutex;
  * @brief Semaphore for synchronizing buffer operations.
  */
 static sem_t bufferSemaphore;
+
+/**
+ * @brief Enum for type of OTA messages received.
+ */
+typedef enum OtaMessageType
+{
+    OtaMessageTypeJob = 0,
+    OtaMessageTypeStream,
+    OtaNumOfMessageType
+}OtaMessageType_t;
 
 /**
  * @brief The network buffer must remain valid when OTA library task is running.
@@ -364,14 +394,11 @@ static OtaMqttStatus_t mqttPublish( const char * const pacTopic,
  *
  * @param[in] qos Quality of Service
  *
- * @param[in] callback Callback to be registered for handling an incoming PUBLISH message on the topic.
- *
  * @return OtaMqttSuccess if success , other error code on failure.
  */
 static OtaMqttStatus_t mqttSubscribe( const char * pTopicFilter,
                                       uint16_t topicFilterLength,
-                                      uint8_t qos,
-                                      OtaMqttCallback_t callback );
+                                      uint8_t qos );
 
 /**
  * @brief Unsubscribe to the Mqtt topics.
@@ -483,6 +510,18 @@ static void otaAppCallback( OtaJobEvent_t event,
                             const void * pData );
 
 /**
+ * @brief callback to use with the MQTT context to notify incoming packet events.
+ *
+ * @param[in] pMqttContext MQTT context which stores the connection.
+ * @param[in] pPacketInfo Parameters of the incoming packet.
+ * @param[in] pDeserializedInfo Deserialized packet information to be dispatched by
+ * the subscription manager to event callbacks.
+ */
+static void mqttEventCallback( MQTTContext_t * pMqttContext,
+                               MQTTPacketInfo_t * pPacketInfo,
+                               MQTTDeserializedInfo_t * pDeserializedInfo );
+
+/**
  * @brief Callback registered with the OTA library that notifies the OTA agent
  * of an incoming PUBLISH containing a job document.
  *
@@ -502,17 +541,8 @@ static void mqttJobCallback( MQTTContext_t * pContext,
 static void mqttDataCallback( MQTTContext_t * pContext,
                               MQTTPublishInfo_t * pPublishInfo );
 
-/**
- * @brief callback to use with the MQTT context to notify incoming packet events.
- *
- * @param[in] pMqttContext MQTT context which stores the connection.
- * @param[in] pPacketInfo Parameters of the incoming packet.
- * @param[in] pDeserializedInfo Deserialized packet information to be dispatched by
- * the subscription manager to event callbacks.
- */
-static void mqttEventCallback( MQTTContext_t * pMqttContext,
-                               MQTTPacketInfo_t * pPacketInfo,
-                               MQTTDeserializedInfo_t * pDeserializedInfo );
+static SubscriptionManagerCallback_t otaMessageCallback[ OtaNumOfMessageType ] = { mqttJobCallback, mqttDataCallback };
+
 /*-----------------------------------------------------------*/
 
 void otaEventBufferFree( OtaEventData_t * const pxBuffer )
@@ -1056,7 +1086,19 @@ static void disconnect( void )
     /* Disconnect from broker. */
     LogInfo( ( "Disconnecting the MQTT connection with %s.", AWS_IOT_ENDPOINT ) );
 
-    MQTT_Disconnect( &mqttContext );
+    if( pthread_mutex_lock( &mqttMutex ) == 0 )
+    {
+        /* OTA job is completed. so delete the network connection. */
+        MQTT_Disconnect( &mqttContext );
+
+        pthread_mutex_unlock( &mqttMutex );
+    }
+    else
+    {
+        LogError( ( "Failed to acquire mutex to execute MQTT_Disconnect"
+                    ",errno=%s",
+                    strerror( errno ) ) );
+    }
 
     /* End TLS session, then close TCP connection. */
     ( void ) Openssl_Disconnect( &networkContext );
@@ -1064,13 +1106,114 @@ static void disconnect( void )
 
 /*-----------------------------------------------------------*/
 
+static OtaMessageType_t getOtaMessageType( const char * pTopicFilter, 
+                                           uint16_t topicFilterLength )
+{
+    int retStatus = EXIT_FAILURE;
+
+    uint16_t stringIndex = 0U, fieldLength = 0U, i = 0U;
+    OtaMessageType_t retMesageType = OtaNumOfMessageType;
+
+    /* Lookup table for OTA message string. */
+    static const char * const pOtaMessageStrings[ OtaNumOfMessageType ] =
+    {
+        OTA_TOPIC_JOBS,
+        OTA_TOPIC_STREAM
+    };
+
+   /* Check topic prefix is valid.*/
+    if( strncmp( pTopicFilter, OTA_TOPIC_PREFIX, ( size_t ) OTA_TOPIC_PREFIX_LENGTH ) == 0 )
+    {
+        stringIndex = OTA_TOPIC_PREFIX_LENGTH;
+
+        retStatus = EXIT_SUCCESS;
+    }
+
+    /* Check if thing name is valid.*/
+    if( retStatus == EXIT_SUCCESS )
+    {
+        retStatus = EXIT_FAILURE;
+
+        /* Extract the thing name.*/
+        for( ; stringIndex < topicFilterLength; stringIndex++ )
+        {
+            if( pTopicFilter[ stringIndex ] == ( char ) '/' )
+            {
+                break;
+            }
+            else
+            {
+                fieldLength++;
+            }
+        }
+
+        if( fieldLength > 0 )
+        {
+            /* Check thing name.*/
+            if( strncmp( &pTopicFilter[stringIndex - fieldLength], 
+                         CLIENT_IDENTIFIER, 
+                         ( size_t ) ( fieldLength ) ) == 0 )
+            {
+                stringIndex++;
+
+                retStatus = EXIT_SUCCESS;
+            }
+        }
+    }
+
+    /* Check the message type from topic.*/
+    if( retStatus == EXIT_SUCCESS )
+    {
+        fieldLength = 0;
+
+        /* Extract the topic type.*/
+        for( ; stringIndex < topicFilterLength; stringIndex++ )
+        {
+            if( pTopicFilter[ stringIndex ] == ( char ) '/' )
+            {
+                break;
+            }
+            else
+            {
+                fieldLength++;
+            }
+            
+        }
+
+        if( fieldLength > 0 )
+        {
+
+            for( i = 0; i < OtaNumOfMessageType; i++ )
+            {
+                /* check thing name.*/
+                if( strncmp( &pTopicFilter[stringIndex - fieldLength], 
+                             pOtaMessageStrings[ i ], 
+                             ( size_t ) ( fieldLength ) ) == 0 )
+                {
+                    break;
+                }
+            }
+
+            if( i < OtaNumOfMessageType ) 
+            {
+                 retMesageType = i;
+            }
+                
+        }
+    }
+
+    return retMesageType;
+}
+
+/*-----------------------------------------------------------*/
+
 static OtaMqttStatus_t mqttSubscribe( const char * pTopicFilter,
                                       uint16_t topicFilterLength,
-                                      uint8_t qos,
-                                      OtaMqttCallback_t callback )
+                                      uint8_t qos )
 {
     OtaMqttStatus_t otaRet = OtaMqttSuccess;
     SubscriptionManagerStatus_t subscriptionStatus = SUBSCRIPTION_MANAGER_SUCCESS;
+    OtaMessageType_t otaMessageType;
 
     MQTTStatus_t mqttStatus;
     MQTTContext_t * pMqttContext = &mqttContext;
@@ -1119,8 +1262,14 @@ static OtaMqttStatus_t mqttSubscribe( const char * pTopicFilter,
                    pTopicFilter ) );
     }
 
+    otaMessageType = getOtaMessageType( pTopicFilter, topicFilterLength );
+
+    assert( ( otaMessageType >= 0 ) && ( otaMessageType < OtaNumOfMessageType ) );
+
     /* Register callback to subscription manager. */
-    subscriptionStatus = SubscriptionManager_RegisterCallback( pTopicFilter, topicFilterLength, callback );
+    subscriptionStatus = SubscriptionManager_RegisterCallback( pTopicFilter, 
+                                                               topicFilterLength, 
+                                                               otaMessageCallback[otaMessageType] );
 
     if( subscriptionStatus != SUBSCRIPTION_MANAGER_SUCCESS )
     {
@@ -1156,7 +1305,7 @@ static OtaMqttStatus_t mqttPublish( const char * const pacTopic,
     {
         mqttStatus = MQTT_Publish( pMqttContext,
                                    &publishInfo,
-                                   ( qos ) ? MQTT_GetPacketId( pMqttContext ) : 0U );
+                                   MQTT_GetPacketId( pMqttContext ) );
 
         pthread_mutex_unlock( &mqttMutex );
     }
@@ -1256,8 +1405,6 @@ static void setOtaInterfaces( OtaInterfaces_t * pOtaInterfaces )
     pOtaInterfaces->mqtt.subscribe = mqttSubscribe;
     pOtaInterfaces->mqtt.publish = mqttPublish;
     pOtaInterfaces->mqtt.unsubscribe = mqttUnsubscribe;
-    pOtaInterfaces->mqtt.jobCallback = mqttJobCallback;
-    pOtaInterfaces->mqtt.dataCallback = mqttDataCallback;
 
     /* Initialize the OTA library PAL Interface.*/
     pOtaInterfaces->pal.getPlatformImageState = otaPal_GetPlatformImageState;
@@ -1350,7 +1497,8 @@ static int startOTADemo( void )
 
     if( returnStatus == EXIT_SUCCESS )
     {
-        /* Until OTA library is stopped, log statistics for currently running OTA job. */
+        /* Wait till OTA library is stopped, output statistics for currently running
+         * OTA job */
         while( ( ( state = OTA_GetState() ) != OtaAgentStateStopped ) )
         {
             if( mqttSessionEstablished != true )
