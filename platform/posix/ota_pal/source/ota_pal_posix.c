05922a295aa4c5577d9d377065d4117d300a7a64
Prasad Vyawahare <pvyawaha@amazon.com>
2020-12-11 21:53:39 -0800

    Update platform/posix/ota_pal/source/ota_pal_posix.c
    Co-authored-by: Shubham Divekar <divekarshubham@gmail.com>

--- platform/posix/ota_pal/source/ota_pal_posix.c
+++ platform/posix/ota_pal/source/ota_pal_posix.c
@@ -1,5 +1,5 @@
 /*
- * OTA PAL for POSIX V2.0.0
+ * OTA PAL V2.0.0 (Release Candidate) for POSIX
  * Copyright (C) 2020 Amazon.com, Inc. or its affiliates.  All Rights Reserved.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of
