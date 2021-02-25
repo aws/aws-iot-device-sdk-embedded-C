b732203b7ce8dab99419e1c111adf06adb6a737d
yanjos-dev <52796499+yanjos-dev@users.noreply.github.com>
2021-02-25 03:34:34 +0000

    Increment the minor version of the OTA PAL
    * Update the minor version of the OTA PAL due to minor bug fixes
      since the last release.

--- platform/posix/ota_pal/source/ota_pal_posix.c
+++ platform/posix/ota_pal/source/ota_pal_posix.c
@@ -1,5 +1,5 @@
 /*
- * OTA PAL V2.0.0 (Release Candidate) for POSIX
+ * OTA PAL V2.1.0 for POSIX
  * Copyright (C) 2020 Amazon.com, Inc. or its affiliates.  All Rights Reserved.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of
