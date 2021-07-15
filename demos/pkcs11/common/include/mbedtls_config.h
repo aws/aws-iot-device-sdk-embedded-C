4400da18ff6ea68ee351e318ce7c84583757bece
Oscar Abrina <abrinao@amazon.com>
2021-07-14 19:24:40 -0700

    Define MBEDTLS_CMAC_C

--- demos/pkcs11/common/include/mbedtls_config.h
+++ demos/pkcs11/common/include/mbedtls_config.h
@@ -22,7 +22,7 @@
  *  not use this file except in compliance with the License.
  *  You may obtain a copy of the License at
  *
- *  https://www.apache.org/licenses/LICENSE-2.0
+ *  http://www.apache.org/licenses/LICENSE-2.0
  *
  *  Unless required by applicable law or agreed to in writing, software
  *  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
@@ -2179,7 +2179,7 @@
  * Requires: MBEDTLS_AES_C or MBEDTLS_DES_C
  *
  */
-/*#define MBEDTLS_CMAC_C */
+#define MBEDTLS_CMAC_C
 
 /**
  * \def MBEDTLS_CTR_DRBG_C
