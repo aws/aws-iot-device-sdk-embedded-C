9e5e2b75df2f353879f63a3e15ded7af3c2a7c35
Shivangi <gshvang@amazon.com>
2021-08-11 17:42:17 -0700

    Adding config file

--- demos/http/http_demo_s3_download/sigv4_config.h
+++ demos/http/http_demo_s3_download/sigv4_config.h
@@ -29,168 +29,43 @@
 #ifndef SIGV4_CONFIG_H_
 #define SIGV4_CONFIG_H_
 
-/**
- * @brief Macro defining the size of the internal buffer used for incremental
- * canonicalization and hashing.
- *
- * A buffer of this size in bytes is declared on the stack. It should be be
- * large enough for the digest output of the specified hash function.
- *
- * <b>Possible values:</b> Any positive 32 bit integer. <br>
- * <b>Default value:</b> `1024`
- */
 #ifndef SIGV4_PROCESSING_BUFFER_LENGTH
     #define SIGV4_PROCESSING_BUFFER_LENGTH    1024U
 #endif
 
-/**
- * @brief Macro defining the maximum number of headers in the request, used to
- * assist the library in sorting header fields during canonicalization.
- *
- * <b>Possible values:</b> Any positive 32 bit integer. <br>
- * <b>Default value:</b> `100`
- */
 #ifndef SIGV4_MAX_HTTP_HEADER_COUNT
     #define SIGV4_MAX_HTTP_HEADER_COUNT    100U
 #endif
 
-/**
- * @brief Macro defining the maximum number of query key/value pairs, used to
- * assist the library in sorting query keys during canonicalization.
- *
- * <b>Possible values:</b> Any positive 32 bit integer. <br>
- * <b>Default value:</b> `100`
- */
 #ifndef SIGV4_MAX_QUERY_PAIR_COUNT
     #define SIGV4_MAX_QUERY_PAIR_COUNT    100U
 #endif
 
-/**
- * @brief Macro indicating the largest block size of any hashing
- * algorithm used for SigV4 authentication i.e. the maximum of all
- * values specified for the hashBlockLen in #SigV4CryptoInterface_t.
- * For example, using SHA-512 would require this value to be at least 128.
- *
- * <b>Possible values:</b> Any positive 32 bit integer. <br>
- * <b>Default value:</b> `64`
- */
 #ifndef SIGV4_HASH_MAX_BLOCK_LENGTH
     #define SIGV4_HASH_MAX_BLOCK_LENGTH    64U
 #endif
 
-/**
- * @brief Macro defining the maximum digest length of the specified hash function,
- * used to determine the length of the output buffer.
- *
- * This macro should be updated if using a hashing algorithm other than SHA256
- * (32 byte digest length). For example, using SHA512 would require this
- * value to be at least 64.
- *
- * <b>Possible values:</b> Any positive 32 bit integer. <br>
- * <b>Default value:</b> `32`
- */
 #ifndef SIGV4_HASH_MAX_DIGEST_LENGTH
     #define SIGV4_HASH_MAX_DIGEST_LENGTH    32U
 #endif
 
-/**
- * @brief Macro to statically enable support for canonicalizing the URI,
- * headers, and query in this utility.
- *
- * Set this to one to enable the encoding functions used to create the canonical
- * request.
- *
- * <b>Possible values:</b> 0 or 1 <br>
- * <b>Default value:</b> `1`
- */
 #ifndef SIGV4_USE_CANONICAL_SUPPORT
     #define SIGV4_USE_CANONICAL_SUPPORT    1
 #endif
 
-/**
- * @brief Macro called by the SigV4 Utility library for logging "Error" level
- * messages.
- *
- * To enable error level logging in the SigV4 Utility library, this macro should
- * be mapped to the application-specific logging implementation that supports
- * error logging.
- *
- * @note This logging macro is called in the SigV4 Utility library with
- * parameters wrapped in double parentheses to be ISO C89/C90 standard
- * compliant. For a reference POSIX implementation of the logging macros, refer
- * to sigv4_config.h files, and the logging-stack in demos folder of the [AWS
- * IoT Embedded C SDK
- * repository](https://github.com/aws/aws-iot-device-sdk-embedded-C).
- *
- * <b>Default value</b>: Error logging is turned off, and no code is generated
- * for calls to the macro in the SigV4 Utility library on compilation.
- */
+/* Logging configuration for the SigV4 library. */
 #ifndef LogError
     #define LogError( message )
 #endif
 
-/**
- * @brief Macro called by the the SigV4 Utility library for logging "Warning"
- * level messages.
- *
- * To enable warning level logging in the SigV4 Utility library, this macro
- * should be mapped to the application-specific logging implementation that
- * supports warning logging.
- *
- * @note This logging macro is called in the SigV4 Utility library with
- * parameters wrapped in double parentheses to be ISO C89/C90 standard
- * compliant. For a reference POSIX implementation of the logging macros, refer
- * to sigv4_config.h files, and the logging-stack in demos folder of the [AWS
- * IoT Embedded C SDK
- * repository](https://github.com/aws/aws-iot-device-sdk-embedded-C).
- *
- * <b>Default value</b>: Warning logs are turned off, and no code is generated
- * for calls to the macro in the SigV4 Utility library on compilation.
- */
 #ifndef LogWarn
     #define LogWarn( message )
 #endif
 
-/**
- * @brief Macro called by the the SigV4 Utility library for logging "Info" level
- * messages.
- *
- * To enable info level logging in the SigV4 Utility library, this macro should
- * be mapped to the application-specific logging implementation that supports
- * info logging.
- *
- * @note This logging macro is called in the SigV4 Utility library with
- * parameters wrapped in double parentheses to be ISO C89/C90 standard
- * compliant. For a reference POSIX implementation of the logging macros, refer
- * to sigv4_config.h files, and the logging-stack in demos folder of the [AWS
- * IoT Embedded C SDK
- * repository](https://github.com/aws/aws-iot-device-sdk-embedded-C).
- *
- * <b>Default value</b>: Info logging is turned off, and no code is generated
- * for calls to the macro in the SigV4 Utility library on compilation.
- */
 #ifndef LogInfo
     #define LogInfo( message )
 #endif
 
-/**
- * @brief Macro called by the the SigV4 Utility library for logging "Debug"
- * level messages.
- *
- * To enable debug level logging from SigV4 Utility library, this macro should
- * be mapped to the application-specific logging implementation that supports
- * debug logging.
- *
- * @note This logging macro is called in the SigV4 Utility library with
- * parameters wrapped in double parentheses to be ISO C89/C90 standard
- * compliant. For a reference POSIX implementation of the logging macros, refer
- * to sigv4_config.h files, and the logging-stack in demos folder of the [AWS
- * IoT Embedded C SDK
- * repository](https://github.com/aws/aws-iot-device-sdk-embedded-C).
- *
- * <b>Default value</b>: Debug logging is turned off, and no code is generated
- * for calls to the macro in the SigV4 Utility library on compilation.
- */
 #ifndef LogDebug
     #define LogDebug( message )
 #endif
