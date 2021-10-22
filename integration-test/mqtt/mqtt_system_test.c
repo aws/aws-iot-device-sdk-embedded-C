acb9b32171300f0add921091d3551e98abae16e7
John Hendrickson <johnrhen@amazon.com>
2021-10-22 15:45:40 -0700

    Create separate target for the AWS IoT integration test group

--- integration-test/mqtt/mqtt_system_test.c
+++ integration-test/mqtt/mqtt_system_test.c
@@ -78,13 +78,6 @@
     #include "custom_unity_runner.h"
 #endif
 
-/* If testing against IoT Core, a subset of tests which are compatible should be used */
-#if ( defined( TEST_AGAINST_IOT_CORE ) && TEST_AGAINST_IOT_CORE )
-    #define TEST_AGAINST_IOT_CORE    true
-#else
-    #define TEST_AGAINST_IOT_CORE    false
-#endif
-
 /**
  * @brief Length of MQTT server host name.
  */
