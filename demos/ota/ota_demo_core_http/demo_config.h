81b2240333d77343da47fb30b49d128f31b45915
bujain <bujain@amazon.com>
2021-12-08 00:03:47 +0000

    update comments

--- demos/ota/ota_demo_core_http/demo_config.h
+++ demos/ota/ota_demo_core_http/demo_config.h
@@ -103,9 +103,13 @@
  * @brief Path of the file containing the server's root CA certificate for TLS
  * authentication.
  *
- * The Baltimore Cybertrust Root CA Certificate is automatically downloaded to
- * the certificates directory using the CMake build system, from @ref
- * https://cacerts.digicert.com/BaltimoreCyberTrustRoot.crt.pem.
+ * This certificate is used to identify the AWS IoT server and is publicly
+ * available. Refer to the AWS documentation available in the link below
+ * https://docs.aws.amazon.com/iot/latest/developerguide/server-authentication.html#server-authentication-certs
+ *
+ * Amazon's root CA certificate is automatically downloaded to the certificates
+ * directory from @ref https://www.amazontrust.com/repository/AmazonRootCA1.pem
+ * using the CMake build system.
  *
  * @note This certificate should be PEM-encoded.
  */
