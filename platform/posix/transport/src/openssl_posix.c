d57f0a95ace427d682dc6576a62cdab5266f774e
ActoryOu <ousc@amazon.com>
2022-01-05 02:17:34 +0800

    return negative value when byteToSend/Recv is 0 (#1774)
    * check more input parameters
    
    * seperate network check function
    
    * fix formatting & coding style
    
    * Update isValidNetworkContext to start with failure
    
    Signed-off-by: Gaurav Aggarwal <aggarg@amazon.com>
    
    * return negative value if byteToSend/Recv is 0
    
    * fix formatting
    
    * separate error conditions
    
    * fix complexity
    
    Co-authored-by: Ubuntu <ubuntu@ip-172-31-34-254.ap-northeast-1.compute.internal>
    Co-authored-by: Gaurav Aggarwal <aggarg@amazon.com>

--- platform/posix/transport/src/openssl_posix.c
+++ platform/posix/transport/src/openssl_posix.c
@@ -752,13 +752,11 @@ int32_t Openssl_Recv( NetworkContext_t * pNetworkContext,
     OpensslParams_t * pOpensslParams = NULL;
     int32_t bytesReceived = 0;
 
-    if( bytesToRecv == 0 )
+    if( !isValidNetworkContext( pNetworkContext ) ||
+        ( pBuffer == NULL ) ||
+        ( bytesToRecv == 0 ) )
     {
-        LogDebug( ( "bytesToRecv is 0." ) );
-    }
-    else if( !isValidNetworkContext( pNetworkContext ) || ( pBuffer == NULL ) )
-    {
-        LogError( ( "Parameter check failed: invalid pNetworkContext or pBuffer is NULL." ) );
+        LogError( ( "Parameter check failed: invalid input, pNetworkContext is invalid or pBuffer = %p, bytesToRecv = %lu", pBuffer, bytesToRecv ) );
         bytesReceived = -1;
     }
     else
@@ -869,13 +867,11 @@ int32_t Openssl_Send( NetworkContext_t * pNetworkContext,
     OpensslParams_t * pOpensslParams = NULL;
     int32_t bytesSent = 0;
 
-    if( bytesToSend == 0 )
+    if( !isValidNetworkContext( pNetworkContext ) ||
+        ( pBuffer == NULL ) ||
+        ( bytesToSend == 0 ) )
     {
-        LogDebug( ( "bytesToSend is 0." ) );
-    }
-    else if( !isValidNetworkContext( pNetworkContext ) || ( pBuffer == NULL ) )
-    {
-        LogError( ( "Parameter check failed: invalid pNetworkContext or pBuffer is NULL." ) );
+        LogError( ( "Parameter check failed: invalid input, pNetworkContext is invalid or pBuffer = %p, bytesToSend = %lu", pBuffer, bytesToSend ) );
         bytesSent = -1;
     }
     else
