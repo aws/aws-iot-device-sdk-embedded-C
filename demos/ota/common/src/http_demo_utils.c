f1d1e7099279df969688db021a2ed0ec23c5d5f7
Muneeb Ahmed <ahmemune@amazon.com>
2022-02-17 10:58:57 -0800

    Apply same fix to OTA file

--- demos/ota/common/src/http_demo_utils.c
+++ demos/ota/common/src/http_demo_utils.c
@@ -20,11 +20,21 @@
  * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
+/* Standard includes. */
+#include <string.h>
+
 /* Demo utils header. */
 #include "http_demo_utils.h"
 
-/* Third party parser utilities. */
-#include "http_parser.h"
+/**
+ * @brief The separator between the "https" scheme and the host in a URL.
+ */
+#define SCHEME_SEPARATOR        "://"
+
+/**
+ * @brief The length of the "://" separator.
+ */
+#define SCHEME_SEPARATOR_LEN    ( sizeof( SCHEME_SEPARATOR ) - 1 )
 
 /*-----------------------------------------------------------*/
 
@@ -33,13 +43,10 @@ HTTPStatus_t getUrlPath( const char * pUrl,
                          const char ** pPath,
                          size_t * pPathLen )
 {
-    /* http-parser status. Initialized to 1 to signify failure. */
-    int parserStatus = 1;
-    struct http_parser_url urlParser;
     HTTPStatus_t httpStatus = HTTPSuccess;
-
-    /* Sets all members in urlParser to 0. */
-    http_parser_url_init( &urlParser );
+    const char * pHostStart = NULL;
+    const char * pPathStart = NULL;
+    size_t hostLen = 0, i = 0, pathStartIndex = 0, pathLen = 0;
 
     if( ( pUrl == NULL ) || ( pPath == NULL ) || ( pPathLen == NULL ) )
     {
@@ -49,31 +56,51 @@ HTTPStatus_t getUrlPath( const char * pUrl,
 
     if( httpStatus == HTTPSuccess )
     {
-        parserStatus = http_parser_parse_url( pUrl, urlLen, 0, &urlParser );
+        httpStatus = getUrlAddress( pUrl, urlLen, &pHostStart, &hostLen );
+    }
 
-        if( parserStatus != 0 )
+    if( httpStatus == HTTPSuccess )
+    {
+        /* Search for the start of the path. */
+        for( i = ( pHostStart - pUrl ) + hostLen; i < urlLen; i++ )
         {
-            LogError( ( "Error parsing the input URL %.*s. Error code: %d.",
+            if( pUrl[ i ] == '/' )
+            {
+                pPathStart = &pUrl[ i ];
+                pathStartIndex = i;
+                break;
+            }
+        }
+
+        if( pPathStart != NULL )
+        {
+            /* The end of the path will be either the start of the query,
+             * start of the fragment, or end of the URL. If this is an S3
+             * presigned URL, then there must be a query. */
+            for( i = pathStartIndex; i < urlLen; i++ )
+            {
+                if( pUrl[ i ] == '?' )
+                {
+                    break;
+                }
+            }
+
+            pathLen = i - pathStartIndex;
+        }
+
+        if( pathLen == 0 )
+        {
+            LogError( ( "Could not parse path from input URL %.*s",
                         ( int32_t ) urlLen,
-                        pUrl,
-                        parserStatus ) );
-            httpStatus = HTTPParserInternalError;
+                        pUrl ) );
+            httpStatus = HTTPNoResponse;
         }
     }
 
     if( httpStatus == HTTPSuccess )
     {
-        *pPathLen = ( size_t ) ( urlParser.field_data[ UF_PATH ].len );
-
-        if( *pPathLen == 0 )
-        {
-            httpStatus = HTTPNoResponse;
-            *pPath = NULL;
-        }
-        else
-        {
-            *pPath = &pUrl[ urlParser.field_data[ UF_PATH ].off ];
-        }
+        *pPathLen = pathLen;
+        *pPath = pPathStart;
     }
 
     if( httpStatus != HTTPSuccess )
@@ -93,13 +120,10 @@ HTTPStatus_t getUrlAddress( const char * pUrl,
                             const char ** pAddress,
                             size_t * pAddressLen )
 {
-    /* http-parser status. Initialized to 1 to signify failure. */
-    int parserStatus = 1;
-    struct http_parser_url urlParser;
     HTTPStatus_t httpStatus = HTTPSuccess;
-
-    /* Sets all members in urlParser to 0. */
-    http_parser_url_init( &urlParser );
+    const char * pHostStart = NULL;
+    const char * pHostEnd = NULL;
+    size_t i = 0, hostLen = 0;
 
     if( ( pUrl == NULL ) || ( pAddress == NULL ) || ( pAddressLen == NULL ) )
     {
@@ -109,30 +133,54 @@ HTTPStatus_t getUrlAddress( const char * pUrl,
 
     if( httpStatus == HTTPSuccess )
     {
-        parserStatus = http_parser_parse_url( pUrl, urlLen, 0, &urlParser );
-
-        if( parserStatus != 0 )
+        /* Search for the start of the hostname using the "://" separator. */
+        for( i = 0; i < ( urlLen - SCHEME_SEPARATOR_LEN ); i++ )
         {
-            LogError( ( "Error parsing the input URL %.*s. Error code: %d.",
+            if( strncmp( &( pUrl[ i ] ), SCHEME_SEPARATOR, SCHEME_SEPARATOR_LEN ) == 0 )
+            {
+                pHostStart = pUrl + i + SCHEME_SEPARATOR_LEN;
+                break;
+            }
+        }
+
+        if( pHostStart == NULL )
+        {
+            LogError( ( "Could not find \"://\" scheme separator in input URL %.*s",
                         ( int32_t ) urlLen,
-                        pUrl,
-                        parserStatus ) );
+                        pUrl ) );
             httpStatus = HTTPParserInternalError;
+        }
+        else
+        {
+            /* Search for the end of the hostname assuming that the object path
+             * is next. Assume that there is no port number as this is used for
+             * S3 presigned URLs. */
+            for( pHostEnd = pHostStart; pHostEnd < ( pUrl + urlLen ); pHostEnd++ )
+            {
+                if( *pHostEnd == '/' )
+                {
+                    hostLen = ( size_t ) ( pHostEnd - pHostStart );
+                    break;
+                }
+            }
         }
     }
 
     if( httpStatus == HTTPSuccess )
     {
-        *pAddressLen = ( size_t ) ( urlParser.field_data[ UF_HOST ].len );
+        *pAddressLen = hostLen;
 
-        if( *pAddressLen == 0 )
+        if( hostLen == 0 )
         {
+            LogError( ( "Could not find end of host in input URL %.*s",
+                        ( int32_t ) urlLen,
+                        pUrl ) );
             httpStatus = HTTPNoResponse;
             *pAddress = NULL;
         }
         else
         {
-            *pAddress = &pUrl[ urlParser.field_data[ UF_HOST ].off ];
+            *pAddress = pHostStart;
         }
     }
 
